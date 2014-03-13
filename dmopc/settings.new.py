6bb68ed33bba9b6a3eee945f39fb8d5ea4b38da1
Xiaomao Chen <xiaomao5@live.com>
2014-03-13 13:25:01 -0400

    Real static file handling.

--- dmopc/settings.new.py
+++ dmopc/settings.new.py
@@ -98,4 +98,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'resources'),
+]
 STATIC_URL = '/static/'
