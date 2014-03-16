47bd53e02cb6243e66eaf34def7320189da1a9b9
Tudor Brinedus <tbrindus@gmail.com>
2014-03-16 13:47:51 -0400

    Attempt to add problem submissions

--- judge/views/problem.py
+++ judge/views/problem.py
@@ -3,7 +3,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from judge.models import Problem
+from judge.forms import ProblemSubmitForm
+from judge.models import Problem, Profile
 
 
 def problem(request, code):
@@ -17,4 +18,18 @@ def problem(request, code):
 
 def problems(request):
     return render_to_response('problems.html', {'problems': Problem.objects.all(), 'title': 'Problems'},
+                              context_instance=RequestContext(request))
+
+
+@login_required
+def problem_submit(request):
+    profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = ProblemSubmitForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path)
+    else:
+        form = ProblemSubmitForm(instance=profile)
+    return render_to_response('problem_submit.html', {'form': form, 'title': 'Submit'},
                               context_instance=RequestContext(request))
