e04779b057873ce4608b135c792cf76af50cc29a
Xiaomao Chen <xiaomao5@live.com>
2014-03-16 19:21:58 -0400

    Honestly hope this will put an end to no profile errors.

--- judge/signals.py
+++ judge/signals.py
@@ -1,8 +1,9 @@
 from django.dispatch import receiver
-from registration.signals import user_activated
-from .models import Profile
+from registration.signals import user_registered
+from .models import Profile, Language
 
 
-@receiver(user_activated)
+@receiver(user_registered)
 def make_profile(sender, user, **kwargs):
-    Profile.objects.get_or_create(user=user)
+    Profile.objects.get_or_create(user=user,
+                                  language=Language.objects.get_or_create(key='PY2', name='Python 2')[0])
