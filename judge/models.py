d3e695a88c5e404bc9ebd121f1fd09e1a3180e13
Tudor Brinedus <tbrindus@gmail.com>
2014-03-17 08:06:11 -0400

    Shorter name for problem

--- judge/models.py
+++ judge/models.py
@@ -84,8 +84,7 @@ class Problem(models.Model):
         return map(attrgetter('full_name'), self.types.all())
 
     def __unicode__(self):
-        return '%s (%s), %s%s points created by %s' % (self.name, self.code, self.points, 'p' if self.partial else '',
-                                                       self.user)
+        return self.name
 
 
 class ProblemAdmin(admin.ModelAdmin):
