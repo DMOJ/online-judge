bbacf826118fe0afdfaf33bd89794ef78eecdeab
Tudor Brinedus <tbrindus@gmail.com>
2014-03-21 18:02:43 -0400

    Add format prototype

---
+++ resources/common.js
@@ -0,0 +1,8 @@
+String.prototype.format = function() {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+        formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+};
