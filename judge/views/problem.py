19ab88d9ec70d9ab424c38ab0d62d899c24251a2
Tudor Brinedus <tbrindus@gmail.com>
2014-03-21 16:38:52 -0400

    On submit, move to submission status page

--- judge/views/problem.py
+++ judge/views/problem.py
@@ -33,7 +33,7 @@ def problems(request):
 
 
 @login_required
-def problem_submit(request, problem=None):
+def problem_submit(request, problem_code=None):
     if request.method == 'POST':
         form = ProblemSubmitForm(request.POST, instance=Submission(user=request.user.profile))
         if form.is_valid():
@@ -42,11 +42,16 @@ def problem_submit(request, problem=None):
             return HttpResponseRedirect(request.path)
     else:
         initial = {'language': request.user.profile.language}
-        if problem is not None:
+        problem = None
+        if problem_code is not None:
             try:
-                initial['problem'] = Problem.objects.get(code=problem)
+                problem = Problem.objects.get(code=problem_code)
+                initial['problem'] = problem
             except ObjectDoesNotExist:
                 return Http404()
+        else:
+            return Http404()
+
         form = ProblemSubmitForm(initial=initial)
-    return render_to_response('problem_submit.html', {'form': form, 'title': 'Submit'},
+    return render_to_response('problem_submit.html', {'form': form, 'title': 'Submit', 'problem': problem},
                               context_instance=RequestContext(request))
