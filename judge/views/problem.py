ea0c99cc7ecae638f590ddedb20971fddc44bd2c
Xiaomao Chen <xiaomao5@live.com>
2014-03-24 17:39:43 -0400

    IE and CE are also finished submissions

--- judge/views/problem.py
+++ judge/views/problem.py
@@ -48,7 +48,7 @@ def problem_submit(request, problem=None):
     if request.method == 'POST':
         form = ProblemSubmitForm(request.POST, instance=Submission(user=request.user.profile))
         if form.is_valid():
-            if Submission.objects.filter(user=request.user.profile).exclude(status='D').count() > 2:
+            if Submission.objects.filter(user=request.user.profile).exclude(status__in=['D', 'IE', 'CE']).count() > 2:
                 return HttpResponse('<h1>You submitted too many submissions.</h1>', status=503)
             model = form.save()
             model.judge()
