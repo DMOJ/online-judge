08e6718595f9c400a439cca3c86d4630a88a08d7
Tudor Brinedus <tbrindus@gmail.com>
2014-03-25 13:06:40 -0400

    Make login fields visible

--- resources/navbar.js
+++ resources/navbar.js
@@ -3,11 +3,11 @@ $(function () {
     $(window).scroll(function () {
         $('#navigation').css('position', ($(window).scrollTop() - nav_offset > 0) ? 'fixed' : 'relative');
     });
-    var float_bar = $('.info-float');
-    var info_offset = float_bar.offset().top;
-    var info_right = float_bar.offset().right;
-    $(window).scroll(function () {
-        float_bar.css('position', ($(window).scrollTop() - info_offset > 0) ? 'fixed' : 'relative');
-        float_bar.css('right', ($(window).scrollTop() - info_offset > 0) ? info_right : 'auto');
-    });
+//    var float_bar = $('.info-float');
+//    var info_offset = float_bar.offset().top;
+//    var info_right = float_bar.offset().right;
+//    $(window).scroll(function () {
+//        float_bar.css('position', ($(window).scrollTop() - info_offset > 0) ? 'fixed' : 'relative');
+//        float_bar.css('right', ($(window).scrollTop() - info_offset > 0) ? info_right : 'auto');
+//    });
 });
