6b95334567797c82d0edc21325e8add1c875502f
Tudor Brinedus <tbrindus@gmail.com>
2014-03-25 12:23:07 -0400

    Make status table float

--- resources/navbar.js
+++ resources/navbar.js
@@ -1,8 +1,10 @@
-$(function() {
-    (function() {
-        var nav_offset = $('#navigation').offset().top;
-        $(window).scroll(function() {
-            $('#navigation').css('position', ($(window).scrollTop() - nav_offset > 0) ? 'fixed' : 'relative');
-        });
-    })();
+$(function () {
+    var nav_offset = $('#navigation').offset().top;
+    $(window).scroll(function () {
+        $('#navigation').css('position', ($(window).scrollTop() - nav_offset > 0) ? 'fixed' : 'relative');
+    });
+    var info_offset = $('.info-float').offset().top;
+    $(window).scroll(function () {
+        $('.info-float').css('position', ($(window).scrollTop() - info_offset > 0) ? 'fixed' : 'relative');
+    });
 });
