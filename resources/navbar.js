7707eabf067fb70a898379301e64233e395ced48
Tudor Brinedus <tbrindus@gmail.com>
2014-03-25 12:27:21 -0400

    Fix status table right align (attempt)

--- resources/navbar.js
+++ resources/navbar.js
@@ -3,8 +3,11 @@ $(function () {
     $(window).scroll(function () {
         $('#navigation').css('position', ($(window).scrollTop() - nav_offset > 0) ? 'fixed' : 'relative');
     });
-    var info_offset = $('.info-float').offset().top;
+    var float = $('.info-float');
+    var info_offset = float.offset().top;
+    var info_right = float.offset().right;
     $(window).scroll(function () {
-        $('.info-float').css('position', ($(window).scrollTop() - info_offset > 0) ? 'fixed' : 'relative');
+        float.css('position', ($(window).scrollTop() - info_offset > 0) ? 'fixed' : 'relative');
+        float.css('right', ($(window).scrollTop() - info_offset > 0) ? 0 : 'auto');
     });
 });
