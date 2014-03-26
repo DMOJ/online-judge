755ccc88594ecfa6ac718d18a1b3a0bef27dcb21
FatalEagle <li.yikuan@gmail.com>
2014-03-26 17:06:09 -0400

    Update djangoserver.py

--- judge/bridge/djangoserver.py
+++ judge/bridge/djangoserver.py
@@ -7,3 +7,11 @@ class DjangoServer(SocketServer.ThreadingTCPServer):
     def __init__(self, judges, *args, **kwargs):
         SocketServer.ThreadingTCPServer.__init__(self, *args, **kwargs)
         self.judges = judges
+
+
+    def ping(self):
+        while True:
+            for judge in self.judges:
+                judge._send({'name': 'ping'})
+            import time
+            time.sleep(0.2)
