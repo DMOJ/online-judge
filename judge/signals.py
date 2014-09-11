4f2a644172e94805ce41e013a861f35fdcf89d71
Xiaomao Chen <xiaomao5@live.com>
2014-09-11 17:27:36 -0400

    Added display name to registration

--- judge/signals.py
+++
@@ -1,9 +0,0 @@
-from django.dispatch import receiver
-from registration.signals import user_registered
-from .models import Profile, Language
-
-
-@receiver(user_registered)
-def make_profile(sender, user, **kwargs):
-    Profile.objects.get_or_create(user=user,
-                                  language=Language.objects.get_or_create(key='PY2', name='Python 2')[0])
--- judge/views/register.py
+++ judge/views/register.py
@@ -1,16 +1,35 @@
-from registration.backends.default.views import RegistrationView, ActivationView
+from django.forms import CharField
+from registration.backends.default.views import\
+    RegistrationView as OldRegistrationView,\
+    ActivationView as OldActivationView
+from registration.forms import RegistrationForm
+from judge.models import Profile, Language
 
 
-class RegistrationView(RegistrationView):
+class CustomRegistrationForm(RegistrationForm):
+    display_name = CharField(max_length=50, required=False)
+
+
+class RegistrationView(OldRegistrationView):
     title = 'Registration'
+    form_class = CustomRegistrationForm
 
     def get_context_data(self, **kwargs):
         if 'title' not in kwargs:
             kwargs['title'] = self.title
         return super(RegistrationView, self).get_context_data(**kwargs)
 
+    def register(self, request, **cleaned_data):
+        user = super(RegistrationView, self).register(request, **cleaned_data)
+        profile, _ = Profile.objects.get_or_create(user=user, defaults={
+            'language': Language.objects.get_or_create(key='PY2', name='Python 2')[0]
+        })
+        profile.name = cleaned_data['display_name']
+        profile.save()
+        return user
 
-class ActivationView(ActivationView):
+
+class ActivationView(OldActivationView):
     title = 'Registration'
 
     def get_context_data(self, **kwargs):
