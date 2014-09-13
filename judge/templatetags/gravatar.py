5dbf65e63d942b78fd1fc28133ba0bc19c429d23
Tudor <tbrindus@gmail.com>
2014-09-13 11:18:24 -0400

    Use Gravatar default icon

--- judge/templatetags/gravatar.py
+++ judge/templatetags/gravatar.py
@@ -25,9 +25,9 @@ class GravatarUrlNode(template.Node):
  
         default = "http://example.com/static/images/defaultavatar.jpg"
         size = 40
- 
-        gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-        gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+
+        gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest()#+ "?"
+        #gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
  
         return gravatar_url
  
