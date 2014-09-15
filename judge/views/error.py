d4c555929dfc6524eb8637e6ecb14b0e41202d1e
Tudor <tbrindus@gmail.com>
2014-09-14 23:06:45 -0400

    Fancy error view

--- judge/views/error.py
+++ judge/views/error.py
@@ -4,7 +4,7 @@ from django.shortcuts import *
 def error404(request):
     return render_to_response('error.html',
                               {'id': 'page_out_of_bounds',
-                               'description': 'bad page "%s"' % request.path,
+                               'description': 'bad page %s' % request.path,
                                'code': 404},
                               context_instance=RequestContext(request))
 
@@ -12,7 +12,7 @@ def error404(request):
 def error403(request):
     return render_to_response('error.html',
                               {'id': 'unauthorized_access',
-                               'description': 'no permission for "%s"' % request.path,
+                               'description': 'no permission for %s' % request.path,
                                'code': 403},
                               context_instance=RequestContext(request))
 
@@ -20,6 +20,6 @@ def error403(request):
 def error500(request):
     return render_to_response('error.html',
                               {'id': 'invalid_state',
-                               'description': 'corrupt page "%s"' % request.path,
+                               'description': 'corrupt page %s' % request.path,
                                'code': 500},
                               context_instance=RequestContext(request))
