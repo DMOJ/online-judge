67a25285be7ea1663f5a54e3fec5b99abdfc80f4
Xiaomao Chen <xiaomao5@live.com>
2014-09-15 22:06:55 -0400

    Returned proper response code from error handlers

--- judge/views/error.py
+++ judge/views/error.py
@@ -1,25 +1,24 @@
 from django.shortcuts import *
 
 
+def error(request, context, status):
+    template = loader.get_template('error.html')
+    return HttpResponse(template.render(RequestContext(request, context)), status=status)
+
+
 def error404(request):
-    return render_to_response('error.html',
-                              {'id': 'page_out_of_bounds',
-                               'description': 'bad page %s' % request.path,
-                               'code': 404},
-                              context_instance=RequestContext(request))
+    return error(request, {'id': 'page_out_of_bounds',
+                           'description': 'bad page %s' % request.path,
+                           'code': 404}, 404)
 
 
 def error403(request):
-    return render_to_response('error.html',
-                              {'id': 'unauthorized_access',
-                               'description': 'no permission for %s' % request.path,
-                               'code': 403},
-                              context_instance=RequestContext(request))
+    return error(request, {'id': 'unauthorized_access',
+                           'description': 'no permission for %s' % request.path,
+                           'code': 403}, 403)
 
 
 def error500(request):
-    return render_to_response('error.html',
-                              {'id': 'invalid_state',
-                               'description': 'corrupt page %s' % request.path,
-                               'code': 500},
-                              context_instance=RequestContext(request))
+    return error(request, {'id': 'invalid_state',
+                           'description': 'corrupt page %s' % request.path,
+                           'code': 500}, 500)
