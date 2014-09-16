d53f69c5d5d55ebb67efa2e7a2e38dc2ce57e908
Xiaomao Chen <xiaomao5@live.com>
2014-09-16 17:27:09 -0400

    Made channels virtual and require no creation

--- websocket/daemon.js
+++ websocket/daemon.js
@@ -3,40 +3,69 @@ var queue = require('qu');
 var WebSocketServer = require('ws').Server;
 var wss_receiver = new WebSocketServer({port: 17001});
 var wss_sender = new WebSocketServer({port: 17002});
-var channels = {};
-var max_channel = 10;
+var messages = new queue();
+var followers = new set();
+var max_queue = 50;
 var message_id = Date.now();
+
+messages.catch_up = function (client) {
+    this.each(function (message) {
+        if (message.id > client.last_msg)
+            client.got_message(message);
+    });
+};
+
+messages.post = function (channel, message) {
+    message = {
+        id: ++message_id,
+        channel: channel,
+        message: message
+    };
+    this.push(message);
+    if (this.length > max_queue)
+        this.shift();
+    followers.each(function (client) {
+        client.got_message(message);
+    });
+    return message.id;
+},
+
+messages.last = function () {
+    return this.tail().id;
+}
 
 wss_receiver.on('connection', function (socket) {
     socket.channel = null;
-    socket.last_msg = null;
+    socket.last_msg = 0;
 
     var commands = {
         start_msg: function (request) {
             socket.last_msg = request.start;
         },
-        set_channel: function (request) {
-            if (request.channel in channels) {
-                if (socket.channel != null)
-                    socket.channel.followers.remove(socket);
-                socket.channel = channels[request.channel];
-                socket.channel.followers.add(socket);
-                socket.send(JSON.stringify({
-                    status: 'success',
-                    message: 'channel set to ' + request.channel
-                }));
-                socket.channel.catch_up(socket);
-            } else
+        set_filter: function (request) {
+            var filter = {};
+            if (Array.isArray(request.filter) && request.filter.every(function (channel, index, array) {
+                if (typeof channel != 'string')
+                    return false;
+                filter[channel] = true;
+                return true;
+            })) {
+                socket.filter = request.filter.length == 0 ? true : filter;
+                followers.add(socket);
+                messages.catch_up(socket);
+            } else {
                 socket.send(JSON.stringify({
                     status: 'error',
-                    code: 'no-channel',
-                    message: 'channel no exist: ' + request.channel
+                    code: 'invalid-filter',
+                    message: 'invalid filter: ' + request.filter
                 }));
+            }
         },
     };
 
     socket.got_message = function (message) {
-        socket.send(JSON.stringify(message));
+        if (socket.filter === true || message.channel in socket.filter)
+            socket.send(JSON.stringify(message));
         socket.last_msg = message.id;
     };
 
@@ -61,84 +90,46 @@ wss_receiver.on('connection', function (socket) {
                 message: 'bad command: ' + request.command
             }));
     });
-    
+
     socket.on('close', function(code, message) {
-        if (socket.channel != null)
-            socket.channel.followers.remove(socket);
+        followers.remove(socket);
     });
 });
 
 wss_sender.on('connection', function (socket) {
     var commands = {
-        new_channel: function (request) {
-            if (request.channel in channels) {
-                socket.send(JSON.stringify({
-                    status: 'error',
-                    code: 'channel-exists',
-                    message: 'channel exists: ' + request.channel
-                }));
-                return;
-            }
-            channels[request.channel] = {
-                messages: new queue(),
-                followers: new set(),
-                catch_up: function (client) {
-                    this.messages.each(function (message) {
-                        if (message.id > client.last_msg)
-                            client.got_message(message);
-                    });
-                },
-                add: function (message) {
-                    message = {
-                        id: message_id++,
-                        message: message
-                    };
-                    this.messages.push(message);
-                    if (this.messages.length > max_channel)
-                        this.messages.shift();
-                    this.followers.each(function (client) {
-                        client.got_message(message);
-                    });
-                    return message.id;
-                },
-                last: function () {
-                    return this.messages.tail().id;
-                }
-            };
-            socket.send(JSON.stringify({
-                status: 'success'
-            }));
-        },
         post: function (request) {
-            if (request.channel in channels) {
-                socket.send(JSON.stringify({
-                    status: 'success',
-                    id: channels[request.channel].add(request.message)
-                }));
-            } else
-                socket.send(JSON.stringify({
+            if (typeof request.channel != 'string')
+                return {
                     status: 'error',
-                    code: 'no-channel'
-                }));
+                    code: 'invalid-channel'
+                };
+            return {
+                status: 'success',
+                id: messages.post(request.channel, request.message)
+            };
         },
         last_msg: function (request) {
-            if (request.channel in channels) {
-                socket.send(JSON.stringify({
-                    status: 'success',
-                    id: channels[request.channel].last()
-                }));
-            } else
-                socket.send(JSON.stringify({
-                    status: 'error',
-                    code: 'no-channel'
-                }));
+            return {
+                status: 'success',
+                id: message_id,
+            };
         }
     };
     socket.on('message', function (request) {
-        request = JSON.parse(request);
+        try {
+            request = JSON.parse(request);
+        } catch (err) {
+            socket.send(JSON.stringify({
+                status: 'error',
+                code: 'syntax-error',
+                message: err.message
+            }));
+            return;
+        }
         request.command = request.command.replace(/-/g, '_');
         if (request.command in commands)
-            commands[request.command](request);
+            socket.send(JSON.stringify(commands[request.command](request)));
         else
             socket.send(JSON.stringify({
                 status: 'error',
