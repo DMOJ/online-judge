b6164250671fbe4366fb273a4af9b19fa122a2a7
Xiaomao Chen <xiaomao5@live.com>
2014-09-27 13:30:23 -0400

    Evaluates flatpages as template

---
+++ judge/templatetags/evaluate.py
@@ -0,0 +1,27 @@
+from django import template
+
+register = template.Library()
+
+@register.tag(name='evaluate')
+def do_evaluate(parser, token):
+    """
+    tag usage {% evaluate object.textfield %}
+    """
+    try:
+        tag_name, variable = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, '%r tag requires a single argument' % token.contents.split()[0]
+    return EvaluateNode(variable)
+
+
+class EvaluateNode(template.Node):
+    def __init__(self, variable):
+        self.variable = template.Variable(variable)
+
+    def render(self, context):
+        try:
+            content = self.variable.resolve(context)
+            t = template.Template(content)
+            return t.render(context)
+        except template.VariableDoesNotExist, template.TemplateSyntaxError:
+            return 'Error rendering', self.variable
