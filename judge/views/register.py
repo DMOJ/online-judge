7804c0699cb4c582876ce800bef5808db14e456d
Xiaomao Chen <xiaomao5@live.com>
2014-09-28 20:02:19 -0400

    Can you finally stop generating the empty field value now?

--- judge/views/register.py
+++ judge/views/register.py
@@ -9,7 +9,7 @@ from judge.models import Profile, Language, TIMEZONE
 class CustomRegistrationForm(RegistrationForm):
     display_name = CharField(max_length=50, required=False, label='Display name (optional)')
     timezone = ChoiceField(choices=TIMEZONE)
-    language = ModelChoiceField(queryset=Language.objects.all(), label='Default language', required=True)
+    language = ModelChoiceField(queryset=Language.objects.all(), label='Default language', empty_label=None)
 
 
 class RegistrationView(OldRegistrationView):
