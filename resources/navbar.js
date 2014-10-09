4110d05a36cb807d7f33d118ab0f0ad32b7d152d
Quantum <quantum2048@gmail.com>
2014-10-08 22:34:48 -0400

    Isolated fix_div

--- resources/navbar.js
+++ resources/navbar.js
@@ -20,3 +20,25 @@ $(function () {
         ($(window).scrollTop() - nav_offset > 0) ? fix() : moving();
     });
 });
+
+function fix_div(div, height, right) {
+    var div_offset = div.offset().top;
+    var is_moving = false;
+    var moving = function () {
+        if (!is_moving) {
+            div.css('position', 'absolute').css('top', div_offset);
+            is_moving = true;
+        }
+    };
+    var fix = function () {
+        if (is_moving) {
+            div.css('position', 'fixed').css('top', height);
+            is_moving = false;
+        }
+    };
+    if (right) div.css('right', $(window).width() - div.offset().left - div.outerWidth());
+    moving();
+    $(window).scroll(function () {
+        ($(window).scrollTop() - div_offset > -height) ? fix() : moving();
+    });
+}
