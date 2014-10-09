cb4669e829ad475cae091b6d1ea0ff5f5ce8da55
Quantum <quantum2048@gmail.com>
2014-10-09 14:36:39 -0400

    Ok need an order.

--- judge/sql/navigationbar.sql
+++ judge/sql/navigationbar.sql
@@ -1,5 +1,5 @@
-INSERT INTO judge_navigationbar (`key`, label, path, regex) VALUES ('home', 'Home', '/', '^/$');
-INSERT INTO judge_navigationbar (`key`, label, path, regex) VALUES ('problem', 'Problems', '/problems/', '^/problem');
-INSERT INTO judge_navigationbar (`key`, label, path, regex) VALUES ('submit', 'Problems', '/submissions/', '^/(?:submi|src/)');
-INSERT INTO judge_navigationbar (`key`, label, path, regex) VALUES ('user', 'Users', '/user/', '^/user');
-INSERT INTO judge_navigationbar (`key`, label, path, regex) VALUES ('status', 'Status', '/status/', '^/status/$');
+INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (0, 'home', 'Home', '/', '^/$');
+INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (1, 'problem', 'Problems', '/problems/', '^/problem');
+INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (2, 'submit', 'Problems', '/submissions/', '^/(?:submi|src/)');
+INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (3, 'user', 'Users', '/user/', '^/user');
+INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (4, 'status', 'Status', '/status/', '^/status/$');
