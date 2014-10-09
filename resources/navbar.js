fedb1f998951945462f734964d6fb4af09b1405e
Quantum <quantum2048@gmail.com>
2014-10-08 22:30:37 -0400

    Damn, moving doesn't activate first time before.

--- resources/navbar.js
+++ resources/navbar.js
@@ -2,7 +2,7 @@ $(function () {
     var nav = $('#navigation');
     var nav_offset = nav.offset().top;
     $('<div/>', {id: 'fake-nav'}).css('height', nav.height()).prependTo('#nav-head');
-    var is_moving = true;
+    var is_moving = false;
     var moving = function () {
         if (!is_moving) {
             nav.css('position', 'absolute').css('top', nav_offset);
