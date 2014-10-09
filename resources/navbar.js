9196fe894673957c6bf07462770d24a2aef2aeb3
Quantum <quantum2048@gmail.com>
2014-10-08 22:27:45 -0400

    Makes it faster?

--- resources/navbar.js
+++ resources/navbar.js
@@ -2,11 +2,18 @@ $(function () {
     var nav = $('#navigation');
     var nav_offset = nav.offset().top;
     $('<div/>', {id: 'fake-nav'}).css('height', nav.height()).prependTo('#nav-head');
+    var is_moving = true;
     var moving = function () {
-        nav.css('position', 'absolute').css('top', nav_offset);
+        if (!is_moving) {
+            nav.css('position', 'absolute').css('top', nav_offset);
+            is_moving = true;
+        }
     };
     var fix = function () {
-        nav.css('position', 'fixed').css('top', 0);
+        if (is_moving) {
+            nav.css('position', 'fixed').css('top', 0);
+            is_moving = false;
+        }
     };
     moving();
     $(window).scroll(function () {
