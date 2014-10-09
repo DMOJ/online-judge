bb4b67f67bf4a42e914e352dcc871f9956d8f4d7
Quantum <quantum2048@gmail.com>
2014-10-09 15:28:45 -0400

    Simplified callback.

--- resources/navbar.js
+++ resources/navbar.js
@@ -1,12 +1,8 @@
 window.fix_div = function (div, height, right, fake_gen) {
     var div_offset = div.offset().top;
     var is_moving = false;
-    if (typeof fake_gen !== 'undefined') {
-        var fake_info = fake_gen(div);
-        var fake = $('<div/>', {id: fake_info.id});
-        delete fake_info.id;
-        fake.css(fake_info);
-    }
+    if (typeof fake_gen !== 'undefined')
+        fake_gen(div);
     var moving = function () {
         if (!is_moving) {
             div.css('position', 'absolute').css('top', div_offset);
@@ -24,16 +20,10 @@ window.fix_div = function (div, height, right, fake_gen) {
     $(window).scroll(function () {
         ($(window).scrollTop() - div_offset > -height) ? fix() : moving();
     });
-    if (typeof fake_gen !== 'undefined') {
-        return fake;
-    }
 };
 
 $(function () {
     fix_div($('#navigation'), 0, false, function (nav) {
-        return {
-            height: nav.height(),
-            id: 'fake-nav'
-        };
-    }).prependTo('#nav-head');
+        $('<div/>', {id: 'fake-nav'}).css('height', nav.height()).prependTo('#nav-head');;
+    })
 });
