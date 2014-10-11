2d6c795c51f82cf63a37aa750a9a9cc0ba86dd5d
Quantum <quantum2048@gmail.com>
2014-10-11 00:05:04 -0400

    Markdown self-description.

--- dmopc/settings.py
+++ dmopc/settings.py
@@ -143,6 +143,19 @@ MARKDOWN_TROIS_STYLES = {
         },
         'safe_mode': 'escape',
     },
+    'self-description': {
+        'extras': {
+            'code-friendly': None,
+            'pyshell': None,
+            'fenced-code-blocks': None,
+            'demote-headers': 3,
+            'nofollow': None,
+            'html-classes': {'pre': 'code'},
+            'cuddled-lists': None,
+            'header-ids': None,
+        },
+        'safe_mode': 'escape',
+    },
     'problem': {
         'extras': {
             'pyshell': None,
