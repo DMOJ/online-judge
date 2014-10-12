27a6a4754162351ab230a8588b74e7ab710e9e83
Quantum <quantum2048@gmail.com>
2014-10-11 21:25:27 -0400

    Contest cache should auto clear now

--- judge/signals.py
+++ judge/signals.py
@@ -2,8 +2,14 @@ from django.core.cache.utils import make_template_fragment_key
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.cache import cache
-from .models import Problem
+from .models import Problem, Contest
+
 
 @receiver(post_save, sender=Problem)
 def problem_update(sender, instance, **kwargs):
     cache.delete(make_template_fragment_key('problem_html', (instance.id,)))
+
+
+@receiver(post_save, sender=Contest)
+def problem_update(sender, instance, **kwargs):
+    cache.delete(make_template_fragment_key('contest_html', (instance.id,)))
