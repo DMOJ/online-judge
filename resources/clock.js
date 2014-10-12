e00ca4899e1101fffe0c9128d135775a0844f148
Quantum <quantum2048@gmail.com>
2014-10-12 09:43:28 -0400

    Some parsers don't like the name int

--- resources/clock.js
+++ resources/clock.js
@@ -2,8 +2,8 @@ function count_down(label) {
     var match = /(\d+):(\d+):(\d+)/.exec(label.text());
     var time = parseInt(match[1]) * 3600 + parseInt(match[2]) * 60 + parseInt(match[3]);
 
-    function format(int) {
-        var s = "0" + int;
+    function format(num) {
+        var s = "0" + num;
         return s.substr(s.length - 2);
     }
 
