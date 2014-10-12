27c858c5a7ca3f2532fa255861f3326ac22d7adf
Quantum <quantum2048@gmail.com>
2014-10-12 09:42:22 -0400

    Made time remaining count down on contest page.

---
+++ resources/clock.js
@@ -0,0 +1,19 @@
+function count_down(label) {
+    var match = /(\d+):(\d+):(\d+)/.exec(label.text());
+    var time = parseInt(match[1]) * 3600 + parseInt(match[2]) * 60 + parseInt(match[3]);
+
+    function format(int) {
+        var s = "0" + int;
+        return s.substr(s.length - 2);
+    }
+
+    var timer = setInterval(function () {
+        --time;
+        if (time <= 0)
+            clearInterval(timer);
+        var h = Math.floor(time / 3600);
+        var m = Math.floor(time % 3600 / 60);
+        var s = time % 60;
+        label.text(format(h) + ":" + format(m) + ":" + format(s));
+    }, 1000);
+}
