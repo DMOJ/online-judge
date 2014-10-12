28b1409a82ad6748570ff6da2db412ae69eba923
Quantum <quantum2048@gmail.com>
2014-10-11 21:52:12 -0400

    No urlconf for judge.views.join_contest.

--- dmopc/urls.py
+++ dmopc/urls.py
@@ -122,6 +122,7 @@ urlpatterns = patterns('',
     url(r'^contests/$', 'judge.views.contest_list'),
     url(r'^contest/(\w+)/$', 'judge.views.contest'),
     url(r'^contest/(\w+)/ranking/$', 'judge.views.contest_ranking'),
+    url(r'^contest/(\w+)/join/$', 'judge.views.join_contest'),
 
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {
         'problem': ProblemSitemap,
