bc28c37043b60cf426908c8f6ac3c0e71e1595a0
Quantum <quantum2048@gmail.com>
2014-10-13 11:34:31 -0400

    Let's not update IP when it's null or whatever.

--- judge/user_log.py
+++ judge/user_log.py
@@ -5,6 +5,8 @@ from django.utils.timezone import now
 class LogUserAccessMiddleware(object):
     def process_request(self, request):
         if hasattr(request, 'user') and request.user.is_authenticated():
+            updates = {'last_access': now()}
             # Decided on using REMOTE_ADDR as nginx will translate it to the external IP that hits it.
-            Profile.objects.filter(user_id=request.user.pk).update(last_access=now(),
-                                                                   ip=request.META.get('REMOTE_ADDR') or '')
+            if request.META.get('REMOTE_ADDR'):
+                updates['ip'] = request.META.get('REMOTE_ADDR')
+            Profile.objects.filter(user_id=request.user.pk).update(**updates)
