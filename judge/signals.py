063141026f34c23a6af734a0c19b02da3ba0a2f2
Quantum <quantum2048@gmail.com>
2014-10-12 21:32:37 -0400

    Too many variables for caching

--- judge/signals.py
+++ judge/signals.py
@@ -2,7 +2,7 @@ from django.core.cache.utils import make_template_fragment_key
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.cache import cache
-from .models import Problem, Contest, Submission, SubmissionTestCase
+from .models import Problem, Contest
 
 
 @receiver(post_save, sender=Problem)
@@ -13,13 +13,3 @@ def problem_update(sender, instance, **kwargs):
 @receiver(post_save, sender=Contest)
 def contest_update(sender, instance, **kwargs):
     cache.delete(make_template_fragment_key('contest_html', (instance.id,)))
-
-
-@receiver(post_save, sender=Submission)
-def submission_update(sender, instance, **kwargs):
-    cache.delete(make_template_fragment_key('submission_row', (instance.id,)))
-
-
-@receiver(post_save, sender=SubmissionTestCase)
-def submission_case_update(sender, instance, **kwargs):
-    cache.delete(make_template_fragment_key('submission_row', (instance.submission_id,)))
