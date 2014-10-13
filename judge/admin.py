6f54c6d724c0593130b91fb80939977fa71db0fd
Quantum <quantum2048@gmail.com>
2014-10-12 22:47:43 -0400

    Attempted to fix ProblemGroupAdmin.

--- judge/admin.py
+++ judge/admin.py
@@ -241,9 +241,8 @@ class ProblemGroupAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         super(ProblemGroupAdmin, self).save_model(request, obj, form, change)
-        obj.problem_set.clear()
-        for problem in form.cleaned_data['problems']:
-            obj.problem_set.add(problem)
+        obj.problem_set = form.cleaned_data['problems']
+        obj.save()
 
     def get_form(self, request, obj=None, **kwargs):
         self.form.base_fields['problems'].initial = [o.pk for o in obj.problem_set.all()] if obj else []
