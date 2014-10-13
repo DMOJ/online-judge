5d7fc586cb0e755dcce1a20b597d25d72a8dbdb1
Quantum <quantum2048@gmail.com>
2014-10-13 18:54:25 -0400

    Demoted problems.

--- dmopc/settings.py
+++ dmopc/settings.py
@@ -207,6 +207,7 @@ MARKDOWN_TROIS_STYLES = {
             'cuddled-lists': None,
             'footnotes': None,
             'header-ids': None,
+            'demote-headers': 1,
             'tables': None,
         },
         'safe_mode': False,
