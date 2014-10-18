3dbcb69f6f6a0590bb27dc0a34d987d82a92ab5c
Quantum <quantum2048@gmail.com>
2014-10-18 14:16:14 -0400

    @Xyene, THIS IS WTF LEVEL CODE.
    YOU PASSED THE BUILTIN FUNCTION id. YOU DIDN'T CHECK PERMISSIONS. YOU DIDN'T DO ANYTHING RIGHT.

--- judge/views/widgets.py
+++ judge/views/widgets.py
@@ -8,12 +8,15 @@ __all__ = ['rejudge_submission']
 
 @login_required
 def rejudge_submission(request):
-    if request.method != 'POST':
+    if request.method != 'POST' or not request.user.has_perm('judge.rejudge_submission'):
         return HttpResponseForbidden()
 
-    if 'id' not in request.POST:
+    if 'id' not in request.POST or not request.POST['id'].isdigit():
         return HttpResponseBadRequest()
 
-    Submission.objects.get(id=id).judge()
+    try:
+        Submission.objects.get(id=request.POST['id']).judge()
+    except Submission.DoesNotExist:
+        return HttpResponseBadRequest()
 
     return HttpResponse('success', mimetype='text/plain')
