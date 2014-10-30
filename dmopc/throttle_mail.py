5f79765b774299a74993af6018764b117a701292
Quantum <quantum2048@gmail.com>
2014-10-30 19:16:01 -0400

    Added a throttled email handler.

---
+++ dmopc/throttle_mail.py
@@ -0,0 +1,21 @@
+import traceback
+from django.utils.log import AdminEmailHandler
+from django.core.cache import cache
+
+
+def new_email():
+    cache.add('error_email_throttle', 0, 60)
+    return cache.incr('error_email_throttle')
+
+
+class ThrottledEmailHandler(AdminEmailHandler):
+    def emit(self, record):
+        try:
+            count = new_email()
+        except Exception:
+            traceback.print_exc()
+            pass
+        else:
+            if count >= 10:
+                return
+        super(ThrottledEmailHandler, self).emit(record)
