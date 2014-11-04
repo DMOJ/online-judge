22a193400074b715341ce79fdbbd765da368a673
Quantum <quantum2048@gmail.com>
2014-11-04 17:35:32 -0500

    Dynamically modifies a class on body to mark page visibility.

--- resources/common.js
+++ resources/common.js
@@ -31,6 +31,44 @@ if (!Array.indexOf) {
     }
 }
 
+// http://stackoverflow.com/a/1060034/1090657
+(function () {
+    var hidden = 'hidden';
+
+    // Standards:
+    if (hidden in document)
+        document.addEventListener('visibilitychange', onchange);
+    else if ((hidden = 'mozHidden') in document)
+        document.addEventListener('mozvisibilitychange', onchange);
+    else if ((hidden = 'webkitHidden') in document)
+        document.addEventListener('webkitvisibilitychange', onchange);
+    else if ((hidden = 'msHidden') in document)
+        document.addEventListener('msvisibilitychange', onchange);
+    // IE 9 and lower:
+    else if ('onfocusin' in document)
+        document.onfocusin = document.onfocusout = onchange;
+    // All others:
+    else
+        window.onpageshow = window.onpagehide
+            = window.onfocus = window.onblur = onchange;
+
+    function onchange(evt) {
+        var v = 'visible', h = 'hidden', evtMap = {
+            focus: v, focusin: v, pageshow: v, blur: h, focusout: h, pagehide: h
+        };
+
+        evt = evt || window.event;
+        if (evt.type in evtMap)
+            document.body.className = evtMap[evt.type];
+        else
+            document.body.className = this[hidden] ? 'window-hidden' : 'window-visible';
+    }
+
+    // set the initial state (but only if browser supports the Page Visibility API)
+    if (document[hidden] !== undefined)
+        onchange({type: document[hidden] ? 'blur' : 'focus'});
+})();
+
 $(function () {
     $('.toggle').click(function () {
         var link = $(this);
