69e08d980f7159bcd9d5374d4927ccb0f9c9b33b
Quantum <quantum2048@gmail.com>
2014-11-05 19:11:15 -0500

    Attempt to avoid language query per submission

---
+++ judge/templatetags/fast_data_query.py
@@ -0,0 +1,26 @@
+from django import template
+from django.core.cache import cache
+from judge.models import Language
+
+register = template.Library()
+
+
+class LanguageShortDisplayNode(template.Node):
+    def __init__(self, language):
+        self.language_id = language
+ 
+    def render(self, context):
+        key = 'lang_sdn:%d' % self.language_id
+        result = cache.get(key)
+        if result is not None:
+            return result
+        result = Language.objects.get(id=self.language_id).short_display_name
+        cache.set(key, result, 86400)
+        return result
+ 
+@register.tag
+def language_short_display(parser, token):
+    try:
+        return LanguageShortDisplayNode(token.split_contents()[1])
+    except IndexError:
+        raise template.TemplateSyntaxError('%r tag requires language id' % token.contents.split()[0])
