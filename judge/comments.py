8b2132b886ba0ab956b9898037f72f1e938fca02
Quantum <quantum2048@gmail.com>
2014-11-07 23:48:08 -0500

    ContestDetail as class.

--- judge/comments.py
+++ judge/comments.py
@@ -1,4 +1,45 @@
+from django.http import HttpResponseForbidden, HttpResponseRedirect
 from .models import Comment, Problem
+from judge.forms import CommentForm
+
+
+class CommentMixin(object):
+    def get_page_id(self):
+        raise NotImplementedError()
+
+    def get_success_url(self):
+        return self.request.path
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.author = self.request.user.profile
+        comment.page = self.get_page_id()
+        comment.save()
+        return super(CommentMixin, self).form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return HttpResponseForbidden()
+
+        self.comment_form = form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user.profile
+            comment.page = self.get_page_id()
+            comment.save()
+            return HttpResponseRedirect(request.path)
+
+    def get_context_data(self, **kwargs):
+        context = super(CommentMixin, self).get_context_data(**kwargs)
+        context['comment_list'] = Comment.objects.filter(page=self.get_page_id(), parent=None)
+        context['comment_form'] = self.comment_form
+        return context
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests and instantiates a blank version of the form.
+        """
+        self.comment_form = CommentForm(initial={'page': self.get_page_id(), 'parent': None})
 
 
 def problem_comments(problem):
@@ -10,7 +51,6 @@ def contest_comments(contest):
 
 
 def comment_form(request, page_id):
-    from judge.forms import CommentForm
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
--- judge/views/contests.py
+++ judge/views/contests.py
@@ -9,15 +9,16 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils import timezone
 from django.utils.functional import SimpleLazyObject
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
 
-from judge.comments import comment_form, contest_comments
+from judge.comments import comment_form, contest_comments, CommentMixin
 from judge.models import Contest, ContestParticipation, ContestProblem, Profile
 from judge.utils.ranker import ranker
 from judge.utils.views import TitleMixin
 
 
-__all__ = ['ContestList', 'contest', 'contest_ranking', 'join_contest', 'leave_contest']
+__all__ = ['ContestList', 'ContestDetail', 'contest_ranking', 'join_contest', 'leave_contest']
 
 
 def _find_contest(request, key, private_check=True):
@@ -46,39 +47,40 @@ class ContestList(TitleMixin, ListView):
             return Contest.objects.filter(is_public=True)
 
 
-def contest(request, key):
-    contest, exists = _find_contest(request, key)
-    if not exists:
-        return contest
+class ContestDetail(CommentMixin, DetailView):
+    model = Contest
+    context_object_name = 'contest'
+    template_name = 'contest.jade'
 
-    form = comment_form(request, 'c:' + key)
-    if form is None:
-        return HttpResponseRedirect(request.path)
+    def get_title(self):
+        return self.object.name
 
-    if request.user.is_authenticated():
-        contest_profile = request.user.profile.contest
-        try:
-            participation = contest_profile.history.get(contest=contest)
-        except ContestParticipation.DoesNotExist:
+    def get_page_id(self):
+        return 'c:%s' % self.object.key
+
+    def get_context_data(self, **kwargs):
+        context = super(ContestDetail, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated():
+            contest_profile = self.request.user.profile.contest
+            try:
+                participation = contest_profile.history.get(contest=self.object)
+            except ContestParticipation.DoesNotExist:
+                participating = False
+                participation = None
+            else:
+                participating = True
+            in_contest = contest_profile.current is not None and contest_profile.current.contest == self.object
+        else:
             participating = False
             participation = None
-        else:
-            participating = True
-        in_contest = contest_profile.current is not None and contest_profile.current.contest == contest
-    else:
-        participating = False
-        participation = None
-        in_contest = False
-    return render_to_response('contest.jade', {
-        'contest': contest,
-        'title': contest.name,
-        'comment_list': contest_comments(contest),
-        'comment_form': form,
-        'in_contest': in_contest,
-        'participating': participating,
-        'participation': participation,
-        'now': timezone.now(),
-    }, context_instance=RequestContext(request))
+            in_contest = False
+        context.update(
+            in_contest=in_contest,
+            participation=participation,
+            participating=participating,
+            now=timezone.now()
+        )
+        return context
 
 
 @login_required
