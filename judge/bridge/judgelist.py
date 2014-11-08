9fe3de1b9c3a4cf11f0519bc651603ca8dba57d4
Quantum <quantum2048@gmail.com>
2014-11-08 00:20:02 -0500

    Let's lock judgelist and see if it still races

--- judge/bridge/judgelist.py
+++ judge/bridge/judgelist.py
@@ -1,5 +1,6 @@
 import logging
 from operator import attrgetter
+from threading import RLock
 import traceback
 
 logger = logging.getLogger('judge.bridge')
@@ -10,6 +11,7 @@ class JudgeList(object):
         self.queue = []
         self.judges = set()
         self.submission_map = {}
+        self.lock = RLock()
 
     def _handle_free_judge(self, judge):
         for i, elem in enumerate(self.queue):
@@ -27,50 +29,56 @@ class JudgeList(object):
                 break
 
     def register(self, judge):
-        self.judges.add(judge)
-        self._handle_free_judge(judge)
+        with self.lock:
+            self.judges.add(judge)
+            self._handle_free_judge(judge)
 
     def update_problems(self, judge):
-        self._handle_free_judge(judge)
+        with self.lock:
+            self._handle_free_judge(judge)
 
     def remove(self, judge):
-        sub = judge.get_current_submission()
-        if sub is not None:
-            try:
-                del self.submission_map[judge.working]
-            except KeyError:
-                pass
-        self.judges.discard(judge)
+        with self.lock:
+            sub = judge.get_current_submission()
+            if sub is not None:
+                try:
+                    del self.submission_map[judge.working]
+                except KeyError:
+                    pass
+            self.judges.discard(judge)
 
     def __iter__(self):
         return iter(self.judges)
 
     def on_judge_free(self, judge, submission):
-        logger.info('Judge available after grading %d: %s', submission, judge.name)
-        del self.submission_map[submission]
-        self._handle_free_judge(judge)
+        with self.lock:
+            logger.info('Judge available after grading %d: %s', submission, judge.name)
+            del self.submission_map[submission]
+            self._handle_free_judge(judge)
 
     def abort(self, submission):
-        logger.info('Abort request: %d', submission)
-        self.submission_map[submission].abort()
+        with self.lock:
+            logger.info('Abort request: %d', submission)
+            self.submission_map[submission].abort()
 
     def judge(self, id, problem, language, source):
-        if id in self.submission_map:
-            logger.warning('Already judging? %d', id)
-            return
+        with self.lock:
+            if id in self.submission_map:
+                logger.warning('Already judging? %d', id)
+                return
 
-        candidates = [judge for judge in self.judges if not judge.working and judge.can_judge(problem, language)]
-        logger.info('Free judges: %d', len(candidates))
-        if candidates:
-            judge = min(candidates, key=attrgetter('load'))
-            logger.info('Dispatched submission %d to: %s', id, judge.name)
-            self.submission_map[id] = judge
-            try:
-                judge.submit(id, problem, language, source)
-            except Exception:
-                logger.exception('Failed to dispatch %d (%s, %s) to %s', id, problem, language, judge.name)
-                self.judges.discard(judge)
-                return self.judge(id, problem, language, source)
-        else:
-            self.queue.append((id, problem, language, source))
-            logger.info('Queued submission: %d', id)
+            candidates = [judge for judge in self.judges if not judge.working and judge.can_judge(problem, language)]
+            logger.info('Free judges: %d', len(candidates))
+            if candidates:
+                judge = min(candidates, key=attrgetter('load'))
+                logger.info('Dispatched submission %d to: %s', id, judge.name)
+                self.submission_map[id] = judge
+                try:
+                    judge.submit(id, problem, language, source)
+                except Exception:
+                    logger.exception('Failed to dispatch %d (%s, %s) to %s', id, problem, language, judge.name)
+                    self.judges.discard(judge)
+                    return self.judge(id, problem, language, source)
+            else:
+                self.queue.append((id, problem, language, source))
+                logger.info('Queued submission: %d', id)
