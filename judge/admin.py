5ba4e4046453c2de019aa11da56e0c17d43bb7e8
Quantum <quantum2048@gmail.com>
2014-11-14 13:35:36 -0500

    Trying this way of showing email

--- judge/admin.py
+++ judge/admin.py
@@ -22,13 +22,17 @@ except ImportError:
 
 class ProfileAdmin(admin.ModelAdmin):
     fields = ('user', 'name', 'about', 'organization', 'timezone', 'language', 'ace_theme', 'last_access', 'ip')
-    list_display = ('long_display_name', 'user__email', 'timezone_full', 'language', 'last_access', 'ip')
+    list_display = ('long_display_name', 'email', 'timezone_full', 'language', 'last_access', 'ip')
     ordering = ('user__username',)
     search_fields = ('user__username', 'name', 'ip')
     list_filter = ('language', 'timezone')
     actions = ('recalculate_points',)
     actions_on_top = True
     actions_on_bottom = True
+
+    def email(self, obj):
+        return obj.user.email
+    email.short_description = 'Email'
 
     def timezone_full(self, obj):
         return obj.timezone
