2521b6f6416ac3ce087ffc71dcc8be5c7623a7ae
Quantum <quantum2048@gmail.com>
2014-11-15 21:06:25 -0500

    Make problem authors easier to select

--- judge/admin.py
+++ judge/admin.py
@@ -67,6 +67,7 @@ class ProblemAdmin(admin.ModelAdmin):
         ('Language', {'fields': ('allowed_languages',)})
     )
     list_display = ['code', 'name', 'show_authors', 'points', 'is_public']
+    filter_horizontal = ['authors']
     ordering = ['code']
     search_fields = ('^code', 'name')
     actions = ['make_public', 'make_private']
