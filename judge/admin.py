9be21844d2359a3279548a4e95c3d2a5b071d59b
Quantum <quantum2048@gmail.com>
2014-11-20 16:41:11 -0500

    Better multiply by problem points

--- judge/admin.py
+++ judge/admin.py
@@ -194,7 +194,8 @@ class SubmissionAdmin(admin.ModelAdmin):
         submissions = list(queryset.select_related('problem').only('points', 'case_points', 'case_total',
                                                                    'problem__partial', 'problem__points'))
         for submission in submissions:
-            submission.points = round(submission.case_points / submission.case_total if submission.case_total else 0, 1)
+            submission.points = round(submission.case_points / submission.case_total * submission.problem.points
+                                      if submission.case_total else 0, 1)
             if not submission.problem.partial and submission.points < submission.problem.points:
                 submission.points = 0
             submission.save()
