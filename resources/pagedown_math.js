853496ac8500026b139b5acc92f663d523d77aac
Quantum <quantum2048@gmail.com>
2014-11-23 15:43:46 -0500

    Definitely not in the right JS mood today

--- resources/pagedown_math.js
+++ resources/pagedown_math.js
@@ -1,7 +1,8 @@
-('$' in window ? $ : django.jQuery).ready(function ($) {
+('$' in window ? $ : django.jQuery)(function ($) {
     if ('MathJax' in window) {
         $.each(window.editors, function (id, editor) {
             // var textarea = $('textarea.wmd-input#' + id);
+            console.log(id);
             var preview = $('div.wmd-preview#' + id + '_wmd_preview')[0];
             editor.hooks.chain('onPreviewRefresh', function () {
                 MathJax.Hub.Queue(["Typeset", MathJax.Hub, preview]);
