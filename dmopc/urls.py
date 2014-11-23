47e3bf879e13674368e33c503c3ef73be649429b
Quantum <quantum2048@gmail.com>
2014-11-23 18:56:00 -0500

    Need slug

--- dmopc/urls.py
+++ dmopc/urls.py
@@ -126,7 +126,7 @@ urlpatterns = patterns('',
     url(r'^comments/downvote/$', 'judge.views.downvote_comment'),
 
     url(r'^contests/$', views.ContestList.as_view(), name='contest_list'),
-    url(r'^contest/(\w+)$', views.ContestDetail.as_view(), name='contest_view'),
+    url(r'^contest/(?P<key>\w+)$', views.ContestDetail.as_view(), name='contest_view'),
     url(r'^contest/(\w+)/ranking/$', 'judge.views.contest_ranking'),
     url(r'^contest/(\w+)/ranking/ajax$', 'judge.views.contest_ranking_ajax'),
     url(r'^contest/(\w+)/join$', 'judge.views.join_contest'),
