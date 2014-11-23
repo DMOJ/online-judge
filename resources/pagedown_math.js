9f1b0b439bb17ce780638a0478f50fbc510800ab
Quantum <quantum2048@gmail.com>
2014-11-23 15:51:38 -0500

    Completed live updating math preview for markdown

--- resources/pagedown_math.js
+++ resources/pagedown_math.js
@@ -1,12 +1,12 @@
-('$' in window ? $ : django.jQuery)(function ($) {
+(function () {
     if ('MathJax' in window) {
-        $.each(window.editors, function (id, editor) {
-            // var textarea = $('textarea.wmd-input#' + id);
-            console.log(id);
-            var preview = $('div.wmd-preview#' + id + '_wmd_preview')[0];
-            editor.hooks.chain('onPreviewRefresh', function () {
-                MathJax.Hub.Queue(["Typeset", MathJax.Hub, preview]);
+        $(window).load(function () {
+            $.each(window.editors, function (id, editor) {
+                var preview = $('div.wmd-preview#' + id + '_wmd_preview')[0];
+                editor.hooks.chain('onPreviewRefresh', function () {
+                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, preview]);
+                });
             });
         });
     }
-});
+})('$' in window ? $ : django.jQuery);
