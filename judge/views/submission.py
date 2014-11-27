508ad03f8aa1a321a9cdb02a3a0a3db3806a6494
FatalEagle <li.yikuan@gmail.com>
2014-11-26 21:36:50 -0500

    Attempted fix

--- judge/views/submission.py
+++ judge/views/submission.py
@@ -265,7 +265,8 @@ class ForceContestMixin(object):
         return self._contest
 
     def access_check(self, request):
-        if not request.user.has_perm('judge.see_private_contest') and not self.contest.is_public:
+        if not request.user.has_perm('judge.see_private_contest') and not (self.contest.is_public or \
+                        self.contest.start_time > timezone.now()):
             raise Http404()
 
     def get(self, request, *args, **kwargs):
