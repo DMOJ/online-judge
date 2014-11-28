532ce74d28443a94de031e3a069a41ba93460792
Tudor <tbrindus@gmail.com>
2014-11-28 16:32:33 -0500

    Encode target name in GET

--- judge/views/messages.py
+++ judge/views/messages.py
@@ -10,7 +10,7 @@ from django.views.generic import CreateView, DetailView, ListView, View, UpdateV
 from django.views.generic.detail import SingleObjectMixin
 
 from judge.forms import EditOrganizationForm, NewOrganizationForm, NewMessageForm
-from judge.models import Organization, PrivateMessage
+from judge.models import Organization, PrivateMessage, Profile
 from judge.utils.ranker import ranker
 from judge.utils.views import generic_message, TitleMixin, LoginRequiredMixin
 
@@ -21,9 +21,19 @@ class NewMessage(LoginRequiredMixin, TitleMixin, CreateView):
     form_class = NewMessageForm
     title = 'New Message'
 
+    def get_context_data(self, **kwargs):
+        context = super(NewMessage, self).get_context_data(**kwargs)
+        profile = Profile.objects.filter(user__username=self.target)
+        context['target'] = profile
+        return context
+
     def form_valid(self, form):
         form.instance.registrant = self.request.user.profile
         return super(NewMessage, self).form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        self.target = request.GET.get('target') if 'target' in request.GET else None
+        return super(NewMessage, self).get(request, *args, **kwargs)
 
 
 class MessageList(LoginRequiredMixin, TitleMixin, ListView):
