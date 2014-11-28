aa6a82bf2565355d56e4bb299bdab674d2321a75
Tudor <tbrindus@gmail.com>
2014-11-28 16:27:42 -0500

    Add skeleton message views/templates

---
+++ judge/views/messages.py
@@ -0,0 +1,39 @@
+from itertools import chain
+from django.core.cache import cache
+from django.core.cache.utils import make_template_fragment_key
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
+from django.db.models import Count, Max
+from django.http import HttpResponseRedirect, Http404
+from django.utils import timezone
+from django.views.generic import CreateView, DetailView, ListView, View, UpdateView
+from django.views.generic.detail import SingleObjectMixin
+
+from judge.forms import EditOrganizationForm, NewOrganizationForm, NewMessageForm
+from judge.models import Organization, PrivateMessage
+from judge.utils.ranker import ranker
+from judge.utils.views import generic_message, TitleMixin, LoginRequiredMixin
+
+
+class NewMessage(LoginRequiredMixin, TitleMixin, CreateView):
+    template_name = 'messages/new.jade'
+    model = PrivateMessage
+    form_class = NewMessageForm
+    title = 'New Message'
+
+    def form_valid(self, form):
+        form.instance.registrant = self.request.user.profile
+        return super(NewMessage, self).form_valid(form)
+
+
+class MessageList(LoginRequiredMixin, TitleMixin, ListView):
+    model = PrivateMessage
+    template_name = 'messages/list.jade'
+    title = 'Inbox'
+
+    def get_context_data(self, **kwargs):
+        context = super(MessageList, self).get_context_data(**kwargs)
+        profile = self.request.user.user.profile
+        context['outgoing'] = PrivateMessage.objects.filter(sender=profile).order_by('-key')
+        context['incoming'] = PrivateMessage.objects.filter(target=profile).order_by('-key')
+        return context
