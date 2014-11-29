bad07bfb5c87d312ba44c372f23ce3891f9bc702
Quantum <quantum2048@gmail.com>
2014-11-28 20:42:03 -0500

    @Xyene why a method to cache the queryset?
    Also made the line wrap work. &nbsp; breaks it.

--- judge/models.py
+++ judge/models.py
@@ -222,10 +222,6 @@ class Problem(models.Model):
         return reverse('problem_detail', args=(self.code,))
 
     @cached_property
-    def authors_list(self):
-        return self.authors.all()
-
-    @cached_property
     def author_ids(self):
         return self.authors.values_list('id', flat=True)
 
