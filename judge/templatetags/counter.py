f2388afc5e0ecc6298a4dfb49825781f6183f518
Quantum <quantum2048@gmail.com>
2015-01-02 09:47:30 -0500

    Used a counter to number lone test cases so they don't reset after a batch.

---
+++ judge/templatetags/counter.py
@@ -0,0 +1,23 @@
+from django import template
+from itertools import count
+
+register = template.Library()
+
+
+class GetCounterNode(template.Node):
+    def __init__(self, var_name, start=1, step=1):
+        self.var_name = var_name
+        self.start = start
+        self.step = step
+
+    def render(self, context):
+        context[self.var_name] = count(self.start, self.step).next
+        return ''
+
+
+@register.tag
+def get_counter(parser, token):
+    try:
+        return GetCounterNode(*token.contents.split()[1:])
+    except ValueError:
+        raise template.TemplateSyntaxError, '%r tag requires arguments' % token.contents.split()[0]
