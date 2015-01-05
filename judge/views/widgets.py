7205e01b7557cf348c14f37cc68273a134fdd7e1
Quantum <quantum2048@gmail.com>
2015-01-05 18:33:40 -0500

    Don't let people rejudge stuff through the ajax view without permission.

--- judge/views/widgets.py
+++ judge/views/widgets.py
@@ -8,15 +8,21 @@ __all__ = ['rejudge_submission']
 
 @login_required
 def rejudge_submission(request):
-    if request.method != 'POST' or not request.user.has_perm('judge.rejudge_submission'):
+    if request.method != 'POST' or not request.user.has_perm('judge.rejudge_submission') or \
+            not request.user.has_perm('judge.edit_own_problem'):
         return HttpResponseForbidden()
 
     if 'id' not in request.POST or not request.POST['id'].isdigit():
         return HttpResponseBadRequest()
 
     try:
-        Submission.objects.get(id=request.POST['id']).judge()
+        submission = Submission.objects.get(id=request.POST['id'])
     except Submission.DoesNotExist:
         return HttpResponseBadRequest()
 
+    if not request.user.has_perm('judge.edit_all_problem') and \
+            not submission.problem.authors.filter(id=request.user.profile.id).exists():
+        return HttpResponseForbidden()
+
+    submission.judge()
     return HttpResponse('success', mimetype='text/plain')
