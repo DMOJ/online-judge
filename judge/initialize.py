2b60336a11058ce6e2457f244b0a7697441333f8
Quantum <quantum2048@gmail.com>
2015-01-15 19:05:56 -0500

    Language.get_python2() exists for a reason

--- judge/initialize.py
+++ judge/initialize.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class InitializationMiddleware(object):
     def __init__(self):
-        lang = Language.objects.get_or_create(key='PY2', name='Python 2')[0]  # Create a default language
+        lang = Language.get_python2()
         for user in User.objects.filter(profile=None):
             # These poor profileless users
             profile = Profile(user=user, language=lang)
