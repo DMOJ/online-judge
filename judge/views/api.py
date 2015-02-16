3c75121c0674bf930692fda8d48ce1690f3e5453
Quantum <quantum2048@gmail.com>
2015-02-16 17:40:54 -0500

    Stringify timedelta

--- judge/views/api.py
+++ judge/views/api.py
@@ -2,6 +2,7 @@ from django.shortcuts import *
 import traceback
 import json
 from judge.models import Contest
+from judge.templatetags.timedelta import nice_repr
 
 
 def api_contest_list(request):
@@ -10,9 +11,9 @@ def api_contest_list(request):
         js[c.key] = {
             'name': c.name,
             'free_start': c.free_start,
-            "start_time": c.start_time,
-            "time_limit": c.time_limit,
-            "ongoing": c.ongoing
+            'start_time': c.start_time,
+            'time_limit': nice_repr(c.time_limit, 'concise'),
+            'ongoing': c.ongoing
         }
     jso = json.dumps(js)
     return HttpResponse(jso, mimetype='application/json')
