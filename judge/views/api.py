e9b55057f7a8dd8002c1ba7bb7ad841e1c868968
FatalEagle <li.yikuan@gmail.com>
2015-02-16 17:37:45 -0500

    Probably works

--- judge/views/api.py
+++ judge/views/api.py
@@ -7,8 +7,11 @@ from judge.models import Contest
 def api_contest_list(request):
     js = {}
     for c in Contest.objects.filter(is_public=True):
-        js[c.name] = {
-            "description": c.description,
+        js[c.key] = {
+            'name': c.name,
+            'free_start': c.free_start,
+            "start_time": c.start_time,
+            "time_limit": c.time_limit,
             "ongoing": c.ongoing
         }
     jso = json.dumps(js)
