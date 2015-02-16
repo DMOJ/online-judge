a1f9f256391156b4cfc9a86229fcd5ec8fac7a60
Quantum <quantum2048@gmail.com>
2015-02-16 17:42:41 -0500

    ISO 8601 date.

--- judge/views/api.py
+++ judge/views/api.py
@@ -1,5 +1,5 @@
 from django.shortcuts import *
-import traceback
+
 import json
 from judge.models import Contest
 from judge.templatetags.timedelta import nice_repr
@@ -11,7 +11,7 @@ def api_contest_list(request):
         js[c.key] = {
             'name': c.name,
             'free_start': c.free_start,
-            'start_time': c.start_time,
+            'start_time': c.start_time.isoformat(),
             'time_limit': nice_repr(c.time_limit, 'concise'),
             'ongoing': c.ongoing
         }
