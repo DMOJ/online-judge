9ed450c906a7c14bc5043be1cb15a32bbfb10b10
Tudor Brinedus <tbrindus@gmail.com>
2015-02-17 00:02:03 -0500

    Give Mathjax 10 seconds

--- judge/pdf_problems.py
+++ judge/pdf_problems.py
@@ -222,7 +222,7 @@ class WebKitPdfMaker(object):
     def make(self):
         self.proc = subprocess.Popen([
             getattr(settings, 'XVFB_RUN', 'xvfb-run'), '-a',
-            getattr(settings, 'WKHTMLTOPDF', 'wkhtmltopdf'), '--disable-javascript', self.url,
+            getattr(settings, 'WKHTMLTOPDF', 'wkhtmltopdf'), '--enable-javascript', '--javascript-delay', '10000', self.url,
             self.path
         ], stdout=subprocess.PIPE)
         self.log = self.proc.communicate()[0]
