ce053f3012a5ee29afbc11c87110b61bad9228b1
Tudor <tbrindus@gmail.com>
2015-02-16 21:06:16 -0500

    Very raw

--- dmopc/urls.py
+++ dmopc/urls.py
@@ -161,7 +161,7 @@ urlpatterns = patterns('',
 
     url(r'^api/contest/list$', 'judge.views.api_contest_list'),
     url(r'^api/problem/list$', 'judge.views.api_problem_list'),
-    url(r'^api/problem/info/(\\w+)', 'judge.views.api_problem_info'),
+    url(r'^api/problem/info/(\w+)', 'judge.views.api_problem_info'),
 
 
     url(r'^blog/$', blog.PostList.as_view(), name='blog_post_list'),
