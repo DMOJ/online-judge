ab77219c12e441954d24ab7c8ab0e03c58a9c987
Quantum <quantum2048@gmail.com>
2015-03-21 17:30:35 -0400

    Stuff can be None! #13

--- judge/templatetags/rating.py
+++ judge/templatetags/rating.py
@@ -6,9 +6,9 @@ from judge.ratings import rating_class, rating_name
 
 @register.filter(name='rating_class')
 def get_class(rating):
-    return rating_class(int(rating))
+    return '' if rating is None else rating_class(int(rating))
 
 
 @register.filter(name='rating_name')
 def get_name(rating):
-    return rating_name(int(rating))
+    return '' if rating is None else rating_name(int(rating))
