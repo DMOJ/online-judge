d50d35eb3ffc0ab8e3b145a925985b683dec8e26
Quantum <quantum2048@gmail.com>
2015-03-22 13:33:30 -0400

    Totally forgot about select_related.

--- judge/views/user.py
+++ judge/views/user.py
@@ -114,7 +114,8 @@ class UserRating(TitleMixin, UserMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(UserRating, self).get_context_data(**kwargs)
-        ratings = context['ratings'] = self.object.ratings.order_by('contest__end_time').defer('contest__description')
+        ratings = context['ratings'] = self.object.ratings.order_by('contest__end_time').select_related('contest') \
+                                           .defer('contest__description')
         if ratings:
             user_data = self.object.ratings.aggregate(Min('rating'), Max('rating'))
             global_data = Rating.objects.aggregate(Min('rating'), Max('rating'))
