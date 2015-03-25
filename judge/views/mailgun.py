1667002cdc7fb1dbf9f9419681f2c11ec3dccd35
Quantum <quantum2048@gmail.com>
2015-03-25 18:45:34 -0400

    Logging to assist in debugging. #51

--- judge/views/mailgun.py
+++ judge/views/mailgun.py
@@ -1,5 +1,6 @@
 import hashlib
 import hmac
+import logging
 from email.utils import parseaddr
 
 from django.conf import settings
@@ -10,6 +11,9 @@ from django.views.generic import View
 from registration.models import RegistrationProfile
 
 
+logger = logging.getLogger('judge.mail.activate')
+
+
 if hasattr(settings, 'MAILGUN_ACCESS_KEY'):
     class MailgunActivationView(View):
         def post(self, request, *args, **kwargs):
@@ -18,29 +22,38 @@ if hasattr(settings, 'MAILGUN_ACCESS_KEY'):
             token = params.get('token', '')
             signature = params.get('signature', '')
 
-            print request.POST
+            logger.debug('Received request: %s', params)
 
             if signature != hmac.new(key=settings.MAILGUN_ACCESS_KEY, msg='%s%s' % (timestamp, token),
                                      digestmod=hashlib.sha256).hexdigest():
+                logger.info('Rejected request: signature: %s, timestamp: %s, token: %s', signature, timestamp, token)
                 raise PermissionDenied()
             _, sender = parseaddr(params.get('from'))
             if not sender:
+                logger.info('Rejected invalid sender: %s', params.get('from'))
                 return HttpResponse(status=406)
             try:
                 user = User.objects.get(email__iexact=sender)
             except (User.DoesNotExist, User.MultipleObjectsReturned):
+                logger.info('Rejected unknown sender: %s: %s', sender, params.get('from'))
                 return HttpResponse(status=406)
             try:
                 registration = RegistrationProfile.objects.get(user=user)
             except RegistrationProfile.DoesNotExist:
+                logger.info('Rejected sender without RegistrationProfile: %s: %s', sender, params.get('from'))
                 return HttpResponse(status=406)
             key = registration.activation_key
             if key == RegistrationProfile.ACTIVATED:
+                logger.info('Rejected activated sender: %s: %s', sender, params.get('from'))
                 return HttpResponse(status=406)
 
             if key in params.get('body-plain', '') or key in params.get('body-html', ''):
                 if RegistrationProfile.objects.activate_user(key):
+                    logger.info('Activated sender: %s: %s', sender, params.get('from'))
                     return HttpResponse('Activated', status=200)
+                logger.info('Failed to activate sender: %s: %s', sender, params.get('from'))
+            else:
+                logger.info('Activation key not found: %s: %s', sender, params.get('from'))
             return HttpResponse(status=406)
 else:
     class MailgunActivationView(View):
