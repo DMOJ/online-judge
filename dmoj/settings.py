2301ff69ae3e207db56e3ca28b262b4b16e6f4e7
Quantum <quantum2048@gmail.com>
2015-04-03 18:44:29 -0400

    StackOverflow. #50

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -301,6 +301,7 @@ AUTHENTICATION_BACKENDS = (
     'social.backends.google.GoogleOAuth2',
     'social.backends.dropbox.DropboxOAuth2',
     'social.backends.facebook.FacebookOAuth2',
+    'social.backends.stackoverflow.StackoverflowOAuth2',
     'judge.social_auth.GitHubSecureEmailOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
