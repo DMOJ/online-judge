2fcb4e8e9898f7778769a3a3f9adf84872664803
Quantum <quantum2048@gmail.com>
2015-04-03 16:08:42 -0400

    Do we really need all this debugging? #50

--- judge/social_pipeline.py
+++ judge/social_pipeline.py
@@ -27,43 +27,35 @@ class GitHubSecureEmailOAuth2(GithubOAuth2):
         except (HTTPError, ValueError, TypeError):
             emails = []
 
-        logger.info('Got emails from GitHub: %s', emails)
         emails = [(e.get('email'), e.get('primary'), 0) for e in emails if isinstance(e, dict) and e.get('verified')]
         emails.sort(key=itemgetter(1), reverse=True)
         emails = map(itemgetter(0), emails)
-        logger.info('Usable emails: %s', emails)
 
         if emails:
             data['email'] = emails[0]
         else:
             data['email'] = None
-        logger.info('Setting email: %s', data['email'])
+
         return data
 
 
 def verify_email(backend, details, *args, **kwargs):
     if not details['email']:
-        logger.info('Denying: %s', details)
         raise InvalidEmail(backend)
-    else:
-        logger.info('Allowing: %s', details)
 
 
 @partial
 def make_profile(backend, user, response, is_new=False, *args, **kwargs):
-    logger.info('Info from %s: %s', backend.name, response)
-
     if is_new:
         if not hasattr(user, 'profile'):
             profile = Profile(user=user)
             profile.language = Language.get_python2()
-            logger.info('Creating profile for %s', user.username)
             if backend.name == 'google-oauth2':
-                logger.info('Using display name from %s: %s', backend.name, response['displayName'])
                 profile.name = response['displayName']
             elif backend.name == 'github' and 'name' in response:
-                logger.info('Using display name from %s: %s', backend.name, response['name'])
                 profile.name = response['name']
+            else:
+                logger.info('Info from %s: %s', backend.name, response)
             profile.save()
             form = ProfileForm(instance=profile)
         else:
