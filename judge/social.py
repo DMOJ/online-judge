3536bbc30f265c9ae7d57db6df3703aa263db37e
Quantum <quantum2048@gmail.com>
2015-04-03 14:19:18 -0400

    Even more logging. #50
    I really need to know the code path.

--- judge/social.py
+++ judge/social.py
@@ -10,7 +10,10 @@ def make_profile(backend, user, response, *args, **kwargs):
     if not hasattr(user, 'profile'):
         profile = Profile(user=user)
         profile.language = Language.get_python2()
+        logger.info('Creating profile for %s', user.username)
         if backend.name == 'google-oauth2':
             logging.info('Using display name from %s: %s', backend.name, response['displayName'])
             profile.display_name = response['displayName']
         profile.save()
+    else:
+        logger.info('Already have profile: %s', user.profile)
