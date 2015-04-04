bbf78407fc7f10a0db0836773db77baf7207e4fd
Quantum <quantum2048@gmail.com>
2015-04-04 15:57:29 -0400

    Merged clock.js, common.js, and navbar.js. #112

--- resources/common.js
+++ resources/common.js
@@ -1,24 +1,3 @@
-String.prototype.format = function () {
-    var formatted = this;
-    for (var i = 0; i < arguments.length; i++) {
-        var regexp = new RegExp('\\{' + i + '\\}', 'gi');
-        formatted = formatted.replace(regexp, arguments[i]);
-    }
-    return formatted;
-};
-
-String.prototype.replaceAll = function (find, replace) {
-    var str = this;
-    return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
-};
-
-Array.prototype.removeAll = function (what) {
-    var array = this;
-    for (var i = array.length - 1; i--;) {
-        if (array[i] === what) array.splice(i, 1);
-    }
-};
-
 // IE 8
 if (!Array.indexOf) {
     Array.prototype.indexOf = function (obj) {
@@ -89,3 +68,96 @@ $(function register_all_toggles() {
         register_toggle($(this));
     });
 });
+
+function featureTest(property, value, noPrefixes) {
+    var prop = property + ':',
+        el = document.createElement('test'),
+        mStyle = el.style;
+
+    if (!noPrefixes) {
+        mStyle.cssText = prop + ['-webkit-', '-moz-', '-ms-', '-o-', ''].join(value + ';' + prop) + value + ';';
+    } else {
+        mStyle.cssText = prop + value;
+    }
+    return !!mStyle[property];
+}
+
+window.fix_div = function (div, height, right, fake_gen) {
+    var div_offset = div.offset().top;
+    if (right)
+        var div_right = $(window).width() - div.offset().left - div.outerWidth();
+    var is_moving;
+    if (typeof fake_gen !== 'undefined')
+        var fake = fake_gen(div);
+    var moving = function () {
+        div.css('position', 'absolute').css('top', div_offset);
+        is_moving = true;
+    };
+    var fix = function () {
+        div.css('position', 'fixed').css('top', height);
+        is_moving = false;
+    };
+    if (right)
+        div.css('right', div_right);
+    if (typeof fake != 'undefined') {
+        div.css('left', fake.offset().left).css('right', undefined);
+        $(window).resize(function () {
+            div.css('left', fake.offset().left);
+        });
+    }
+    ($(window).scrollTop() - div_offset > -height) ? fix() : moving();
+    $(window).scroll(function () {
+        if (($(window).scrollTop() - div_offset > -height) == is_moving)
+            is_moving ? fix() : moving();
+    });
+};
+
+$(function () {
+    if (!featureTest('position', 'sticky'))
+        fix_div($('#navigation'), 0, false, function (nav) {
+            $('<div/>', {id: 'fake-nav'}).css('height', nav.height()).prependTo('#nav-head');
+        });
+
+    var $nav_list = $('#nav-list');
+    $('#navicon').click(function (event) {
+        event.stopPropagation();
+        $nav_list.show();
+        $nav_list.click(function (event) {
+            event.stopPropagation();
+        });
+        $('html').click(function () {
+            $nav_list.hide();
+        });
+    });
+});
+
+if (!Date.now) {
+    Date.now = function () {
+        return new Date().getTime();
+    };
+}
+
+function count_down(label) {
+    var match = /(?:(\d+)\s+days?\s+)?(\d+):(\d+):(\d+)/.exec(label.text());
+    var initial = parseInt(match[2]) * 3600 + parseInt(match[3]) * 60 + parseInt(match[4]);
+    if (typeof match[1] != 'undefined')
+        initial += parseInt(match[1]) * 86400;
+    var start = Date.now();
+
+    function format(num) {
+        var s = "0" + num;
+        return s.substr(s.length - 2);
+    }
+
+    var timer = setInterval(function () {
+        var time = Math.round(initial - (Date.now() - start) / 1000);
+        if (time <= 0)
+            clearInterval(timer);
+        var d = Math.floor(time / 86400);
+        var h = Math.floor(time % 86400 / 3600);
+        var m = Math.floor(time % 3600 / 60);
+        var s = time % 60;
+        var days = d > 0 ? d + ' day' + 's '.substr(d == 1) : '';
+        label.text(days + format(h) + ":" + format(m) + ":" + format(s));
+    }, 1000);
+}
