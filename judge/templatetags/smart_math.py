62d9be2bbd392c4ea56a9dac492d45a9b2a0d65a
Tudor Brinedus <tbrindus@gmail.com>
2015-04-10 00:00:30 -0400

    Use "new" math delimiters
    The old ones have been bothering me in Facebook previews for too long.
    
    @quantum5 @FatalEagle

--- judge/templatetags/smart_math.py
+++ judge/templatetags/smart_math.py
@@ -10,22 +10,22 @@ class MathJaxTexFallbackMath(MathHTMLParser):
     def inline_math(self, math):
         return ('<span class="inline-math">'
                     r'<img class="tex-image" src="%s?\textstyle %s" alt="%s"/>'
-                    r'<span class="tex-text" style="display:none">\(%s\)</span>'
+                    r'<span class="tex-text" style="display:none">~%s~</span>'
                 '</span>') % (MATHTEX_CGI, urlquote(math), math, math)
 
     def display_math(self, math):
         return ('<span class="display-math">'
                    r'<img class="tex-image" src="%s?\displaystyle %s" alt="%s"/>'
-                   r'<span class="tex-text" style="display:none">\[%s\]</span>'
+                   r'<span class="tex-text" style="display:none">$$%s$$</span>'
                 '</span>') % (MATHTEX_CGI, urlquote(math), math, math)
 
 
 class MathJaxTexOnlyMath(MathHTMLParser):
     def inline_math(self, math):
-        return '\(%s\)' % math
+        return '~%s~' % math
 
     def display_math(self, math):
-        return '\[%s\]' % math
+        return '$$%s$$' % math
 
 
 @register.filter(name='smart_math', is_safe=True)
