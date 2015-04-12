59466ed5882519fd9a70c5e7c6adf71cc29b9540
Quantum <quantum2048@gmail.com>
2015-04-12 17:36:48 -0400

    Better not let content-description.css disappear.

--- make_style.sh
+++ make_style.sh
@@ -2,3 +2,4 @@
 cd `dirname $0`
 sass --update resources:sass_processed
 pleeease compile sass_processed/style.css -t resources/style.css
+mv sass_processed/content-description.css resources/content-description.css
