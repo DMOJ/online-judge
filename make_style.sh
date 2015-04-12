217da7146be55ac03ae471d1a8ce61fef6a9bc5a
Quantum <quantum2048@gmail.com>
2015-04-12 17:36:00 -0400

    Just compile all files in one run instead of keeping running ruby. #39

--- make_style.sh
+++ make_style.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd `dirname $0`
-sass resources/style.sass 'resources/style$.css'
-sass resources/content-description.sass resources/content-description.css
-pleeease compile 'resources/style$.css' -t resources/style.css
-rm 'resources/style$.css' 'resources/style$.css.map'
+sass --update resources:sass_processed
+pleeease compile sass_processed/style.css -t resources/style.css
