5a7d813028df4b5bead704e085db1f87cc2dba43
Quantum <quantum2048@gmail.com>
2015-04-12 17:24:30 -0400

    Dis au revoirs à content-description.css

--- make_style.sh
+++ make_style.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
 sass resources/style.sass 'resources/style$.css'
+sass resources/content-description.sass resources/content-description.css
 pleeease compile 'resources/style$.css' -t resources/style.css
 rm 'resources/style$.css' 'resources/style$.css.map'
