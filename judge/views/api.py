a9ab0d955fb741b1118a718e87bae311590fe316
Quantum <quantum2048@gmail.com>
2015-08-27 01:11:28 -0400

    There is JsonResponse #62

--- judge/views/api.py
+++ judge/views/api.py
@@ -1,7 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import *
+from django.http import JsonResponse, Http404
 
-import json
 from judge.models import Contest, Problem, Profile, Submission
 
 
@@ -13,90 +12,89 @@ def sane_time_repr(delta):
 
 
 def api_contest_list(request):
-    js = {}
+    contests = {}
     for c in Contest.objects.filter(is_public=True, is_private=False):
-        js[c.key] = {
+        contests[c.key] = {
             'name': c.name,
             'start_time': c.start_time.isoformat(),
             'end_time':  c.end_time.isoformat(),
             'time_limit': c.time_limit and sane_time_repr(c.time_limit),
         }
-    return HttpResponse(json.dumps(js), content_type='application/json')
+    return JsonResponse(contests)
 
 
 def api_problem_list(request):
-    js = {}
+    problems = {}
     for p in Problem.objects.filter(is_public=True):
-        js[p.code] = {
+        problems[p.code] = {
             'points': p.points,
             'partial': p.partial,
             'name': p.name,
             'group': p.group.full_name
         }
-    return HttpResponse(json.dumps(js), content_type='application/json')
+    return JsonResponse(problems)
 
 
 def api_problem_info(request, problem):
     try:
         p = Problem.objects.get(code=problem)
-        js = {
-            'name': p.name,
-            'authors': list(p.authors.values_list('user__username', flat=True)),
-            'types': list(p.types.values_list('full_name', flat=True)),
-            'group': p.group.full_name,
-            'time_limit': p.time_limit,
-            'memory_limit': p.memory_limit,
-            'points': p.points,
-            'partial': p.partial,
-            'languages': list(p.allowed_languages.values_list('key', flat=True)),
-        }
     except ObjectDoesNotExist:
         raise Http404()
-    return HttpResponse(json.dumps(js), content_type='application/json')
+    return JsonResponse({
+        'name': p.name,
+        'authors': list(p.authors.values_list('user__username', flat=True)),
+        'types': list(p.types.values_list('full_name', flat=True)),
+        'group': p.group.full_name,
+        'time_limit': p.time_limit,
+        'memory_limit': p.memory_limit,
+        'points': p.points,
+        'partial': p.partial,
+        'languages': list(p.allowed_languages.values_list('key', flat=True)),
+    })
 
 
 def api_user_list(request):
-    js = {}
-    for p in Profile.objects.select_related('user'):
-        js[p.user.username] = {
+    users = {}
+    for p in Profile.objects.select_related('user').only('user__username', 'name', 'points', 'display_rank'):
+        users[p.user.username] = {
             'display_name': p.name,
             'points': p.points,
             'rank': p.display_rank
         }
-    return HttpResponse(json.dumps(js), content_type='application/json')
+    return JsonResponse(users)
 
 
 def api_user_info(request, user):
     try:
         p = Profile.objects.get(user__username=user)
-        js = {
-            'display_name': p.name,
-            'points': p.points,
-            'rank': p.display_rank,
-            'solved_problems': [],  # TODO
-        }
     except ObjectDoesNotExist:
         raise Http404()
-    return HttpResponse(json.dumps(js), content_type='application/json')
+    return JsonResponse({
+        'display_name': p.name,
+        'points': p.points,
+        'rank': p.display_rank,
+        'solved_problems': [],  # TODO
+    })
 
 
 def api_user_submissions(request, user):
     try:
         p = Profile.objects.get(user__username=user)
-        subs = Submission.objects.filter(user=p, problem__is_public=True).select_related('problem', 'language') \
-            .only('id', 'problem__code', 'time', 'memory', 'points', 'language__key', 'status', 'result')
-        js = {}
-
-        for s in subs:
-            js[s.id] = {
-                'problem': s.problem.code,
-                'time': s.time,
-                'memory': s.memory,
-                'points': s.points,
-                'language': s.language.key,
-                'status': s.status,
-                'result': s.result
-            }
     except ObjectDoesNotExist:
         raise Http404()
-    return HttpResponse(json.dumps(js), content_type='application/json')
+
+    subs = Submission.objects.filter(user=p, problem__is_public=True).select_related('problem', 'language') \
+        .only('id', 'problem__code', 'time', 'memory', 'points', 'language__key', 'status', 'result')
+    data = {}
+
+    for s in subs:
+        data[s.id] = {
+            'problem': s.problem.code,
+            'time': s.time,
+            'memory': s.memory,
+            'points': s.points,
+            'language': s.language.key,
+            'status': s.status,
+            'result': s.result
+        }
+    return JsonResponse(data)
