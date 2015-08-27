1d7734f3daefae2df704b15088273e6c9045652f
Quantum <quantum2048@gmail.com>
2015-08-27 14:18:32 -0400

    Method to initialize all the queues and exchanges for #183

--- judge/rabbitmq/connection.py
+++ judge/rabbitmq/connection.py
@@ -4,7 +4,24 @@ from django.conf import settings
 URL = settings.JUDGE_AMQP_PATH
 params = pika.URLParameters(URL)
 vhost = params.virtual_host
+conn = None
 
 
 def connect():
-    return pika.BlockingConnection(params)
+    global conn, chan
+    conn = pika.BlockingConnection(params)
+    return conn
+
+
+def initialize():
+    if conn is None:
+        raise SystemError('Not connected')
+    chan = conn.channel()
+    chan.queue_declare(queue='submission', durable=True)
+    chan.queue_declare(queue='judge-response', durable=True)
+    chan.queue_declare(queue='judge-ping', durable=True)
+    chan.exchange_declare(exchange='broadcast', exchange_type='fanout', durable=True)
+    chan.exchange_declare(exchange='submission-response', exchange_type='fanout', durable=True)
+    chan.exchange_declare(exchange='ping', exchange_type='fanout', durable=True)
+    chan.queue_bind(queue='judge-response', exchange='submission-response')
+    chan.queue_bind(queue='judge-ping', exchange='ping')
