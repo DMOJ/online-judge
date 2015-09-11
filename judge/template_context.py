6c03f38f3fc1471bd9105880ab8316d74c7aa373
Tudor <tbrindus@gmail.com>
2015-09-10 20:13:17 -0400

    Add SITE_ADMIN_EMAIL; #223

--- judge/template_context.py
+++ judge/template_context.py
@@ -96,4 +96,5 @@ def contest(request):
 
 
 def site_name(request):
-    return {'SITE_NAME': getattr(settings, 'SITE_NAME', 'DMOJ')}
+    return {'SITE_NAME': getattr(settings, 'SITE_NAME', 'DMOJ'),
+            'SITE_ADMIN_EMAIL': getattr(settings, 'SITE_ADMIN_EMAIL', False)};
