15a12e74046f0c354a81a9e439714cbeed8aeb8b
Tudor <tbrindus@gmail.com>
2015-09-14 22:42:52 -0400

    Trailing semicolon

--- judge/template_context.py
+++ judge/template_context.py
@@ -98,4 +98,4 @@ def contest(request):
 def site_name(request):
     return {'SITE_NAME': getattr(settings, 'SITE_NAME', 'DMOJ'),
             'SITE_LONG_NAME': getattr(settings, 'SITE_LONG_NAME', getattr(settings, 'SITE_NAME', 'DMOJ')),
-            'SITE_ADMIN_EMAIL': getattr(settings, 'SITE_ADMIN_EMAIL', False)};
+            'SITE_ADMIN_EMAIL': getattr(settings, 'SITE_ADMIN_EMAIL', False)}
