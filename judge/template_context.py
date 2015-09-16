abf2bcfc948ed3628f48a7ab4f0b7aebc6cf31e5
Tudor Brindus <tbrindus@gmail.com>
2015-09-15 22:59:12 -0400

    Template context for PYGMENT_THEME; #240

--- judge/template_context.py
+++ judge/template_context.py
@@ -22,6 +22,7 @@ def get_resource(request):
     else:
         scheme = 'http'
     return {
+        'PYGMENT_THEME': getattr(settings, 'PYGMENT_THEME', None),
         'INLINE_JQUERY': getattr(settings, 'INLINE_JQUERY', True),
         'INLINE_FONTAWESOME': getattr(settings, 'INLINE_FONTAWESOME', True),
         'JQUERY_JS': getattr(settings, 'JQUERY_JS', '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'),
