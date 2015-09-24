8cc3504102fff7f2f9face409ad920e10cb87b30
Quantum <quantum2048@gmail.com>
2015-09-24 15:29:20 -0400

    Improve HTMLTreeString #274

--- judge/lxml_tree.py
+++ judge/lxml_tree.py
@@ -3,10 +3,18 @@ from lxml import html
 
 class HTMLTreeString(object):
     def __init__(self, str):
-        self.tree = html.fromstring(str)
+        self._tree = html.fromstring(str)
 
     def __getattr__(self, attr):
-        return getattr(self.tree, attr)
+        return getattr(self._tree, attr)
+
+    def __setattr__(self, key, value):
+        if key[0] == '_':
+            super(HTMLTreeString, self).__setattr__(key, value)
+        setattr(self._tree, key, value)
+
+    def __str__(self):
+        return html.tostring(self._tree)
 
     def __unicode__(self):
-        return html.tostring(self.tree)
+        return html.tostring(self._tree, encoding='unicode')
