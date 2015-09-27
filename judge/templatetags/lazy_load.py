cd77aae266b235c37ff8f8af1b19a43593013918
Quantum <quantum2048@gmail.com>
2015-09-27 10:48:43 -0400

    Some user uses data: urls... #239

--- judge/templatetags/lazy_load.py
+++ judge/templatetags/lazy_load.py
@@ -15,10 +15,13 @@ register = template.Library()
 def lazy_load(text):
     tree = lxml_tree.fromstring(text)
     for img in tree.xpath('.//img'):
+        src = img.get('src')
+        if src.startswith('data'):
+            continue
         noscript = html.Element('noscript')
         noscript.append(deepcopy(img))
         img.addprevious(noscript)
-        img.set('data-src', img.get('src'))
+        img.set('data-src', src)
         img.set('src', blank)
         img.set('class', img.get('class') + ' unveil' if img.get('class') else 'unveil')
     return tree
