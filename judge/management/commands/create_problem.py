8fdbd34618514ca3e0a13377c33f51a847e5f448
Quantum <quantum2048@gmail.com>
2015-11-15 11:39:07 -0500

    `create_problem` now uses argparse per #311

--- judge/management/commands/create_problem.py
+++ judge/management/commands/create_problem.py
@@ -1,19 +1,23 @@
 from django.core.management.base import BaseCommand
+
 from judge.models import Problem, ProblemGroup, ProblemType
 
 
 class Command(BaseCommand):
-    args = '<code> <name> <body> <type> <group>'
     help = 'create an empty problem'
 
+    def add_arguments(self, parser):
+        parser.add_argument('code', help='problem code')
+        parser.add_argument('name', help='problem title')
+        parser.add_argument('body', help='problem description')
+        parser.add_argument('type', help='problem type')
+        parser.add_argument('group', help='problem group')
+
     def handle(self, *args, **options):
-        if len(args) != 5:
-            self.usage('create_problem')
-        code, name, body, type, group = args
         problem = Problem()
-        problem.code = code
-        problem.name = name
-        problem.description = body
-        problem.group = ProblemGroup.objects.get(name=group)
-        problem.types = [ProblemType.objects.get(name=type)]
+        problem.code = options['code']
+        problem.name = options['name']
+        problem.description = options['body']
+        problem.group = ProblemGroup.objects.get(name=options['group'])
+        problem.types = [ProblemType.objects.get(name=options['type'])]
         problem.save()
