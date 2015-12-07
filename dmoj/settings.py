652f09049f212d746a256ada781573f39c9fb7d4
Quantum <quantum2048@gmail.com>
2015-12-06 23:06:09 -0500

    Use jQuery 1.8.3 for django-suit for lack of #308

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -154,7 +154,8 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             # This is to make django-suit use our own jQuery.
-            os.path.join(BASE_DIR, 'dmoj', 'suit_template_patch'),
+            # But we currently don't because we are still using select2 3.x.
+            # os.path.join(BASE_DIR, 'dmoj', 'suit_template_patch'),
         ],
         'OPTIONS': {
             'context_processors': [
