43a52189c773423500edd0e4eb32e579c10d1a2c
Quantum <quantum2048@gmail.com>
2015-12-12 20:21:50 -0500

    Remove all traces of media_extract #345

--- judge/templatetags/media_extract.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.utils.safestring import mark_safe
-
-register = template.Library()
-
-
-@register.filter(name='media_type')
-def media_type(media, type):
-    return mark_safe('\n'.join(getattr(media, 'render_' + type)()))
