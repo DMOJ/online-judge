e49eab47edb3180a2b68b6840fc9c477443b3c10
Tudor Brindus <tbrindus@gmail.com>
2015-12-19 13:07:12 -0500

    Actually import HttpResponseRedirect; #354

--- judge/views/widgets.py
+++ judge/views/widgets.py
@@ -5,7 +5,7 @@ import urllib2
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ImproperlyConfigured
-from django.http import HttpResponseForbidden, HttpResponseBadRequest, HttpResponse, Http404
+from django.http import HttpResponseForbidden, HttpResponseBadRequest, HttpResponse, Http404, HttpResponseRedirect
 from django.views.generic import View
 
 from judge.models import Submission
