f7e40f6419645bf52f399e851ecf3e834d8ba674
Quantum <quantum2048@gmail.com>
2015-12-21 14:13:53 -0500

    Ensure returned connection is actually connected. #183

--- judge/rabbitmq/connection.py
+++ judge/rabbitmq/connection.py
@@ -1,4 +1,5 @@
 import pika
+from pika.exceptions import ConnectionClosed
 from django.conf import settings
 
 URL = settings.JUDGE_AMQP_PATH
@@ -9,12 +10,18 @@ conn = None
 
 def connect():
     global conn
-    if conn is None:
-        conn = pika.BlockingConnection(params)
-        chan = conn.channel()
-        chan.queue_declare(queue='submission', durable=True)
-        chan.queue_declare(queue='submission-id', durable=True)
-        chan.queue_declare(queue='judge-ping', durable=True)
-        chan.queue_declare(queue='latency', durable=True)
-        chan.exchange_declare(exchange='broadcast', exchange_type='fanout', durable=True)
+    if conn is not None:
+        try:
+            conn.process_data_events()
+        except ConnectionClosed:
+            pass
+        else:
+            return conn
+    conn = pika.BlockingConnection(params)
+    chan = conn.channel()
+    chan.queue_declare(queue='submission', durable=True)
+    chan.queue_declare(queue='submission-id', durable=True)
+    chan.queue_declare(queue='judge-ping', durable=True)
+    chan.queue_declare(queue='latency', durable=True)
+    chan.exchange_declare(exchange='broadcast', exchange_type='fanout', durable=True)
     return conn
