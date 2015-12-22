31e162c6294eb83fde4919ae356e78763e955172
Quantum <quantum2048@gmail.com>
2015-12-22 01:26:12 -0500

    Translate status table codes #337

--- judge/utils/problems.py
+++ judge/utils/problems.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
+
 from django.core.cache import cache
 from django.db.models import F, Count
+from django.utils.translation import ugettext as _
+
 from judge.models import Submission
 
 __all__ = ['contest_completed_ids', 'user_completed_ids']
@@ -30,15 +33,15 @@ def get_result_table(*args, **kwargs):
     if args:
         submissions = args[0]
         if kwargs:
-            raise ValueError("Can't pass both queryset and keyword filters")
+            raise ValueError(_("Can't pass both queryset and keyword filters"))
     else:
         submissions = Submission.objects.filter(**kwargs) if kwargs is not None else Submission.objects
     raw = submissions.values('result').annotate(count=Count('result')).values_list('result', 'count')
     results = defaultdict(int, raw)
-    return [('Accepted', 'AC', results['AC']),
-            ('Wrong Answer', 'WA', results['WA']),
-            ('Compile Error', 'CE', results['CE']),
-            ('Time Limit Exceed', 'TLE', results['TLE']),
-            ('Memory Limit Exceed', 'MLE', results['MLE']),
-            ('Invalid Return', 'IR', results['IR']),
-            ('Total', 'TOT', sum(results.values()))]
+    return [(_('Accepted'), 'AC', results['AC']),
+            (_('Wrong Answer'), 'WA', results['WA']),
+            (_('Compile Error'), 'CE', results['CE']),
+            (_('Time Limit Exceed'), 'TLE', results['TLE']),
+            (_('Memory Limit Exceed'), 'MLE', results['MLE']),
+            (_('Invalid Return'), 'IR', results['IR']),
+            (_('Total'), 'TOT', sum(results.values()))]
