03a6e43c72afe51765516b30968791e4ef0406d7
Quantum <quantum2048@gmail.com>
2015-12-22 15:44:02 -0500

    Problem type localization #337

--- judge/management/commands/makedmojmessages.py
+++ judge/management/commands/makedmojmessages.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.core.management import CommandError
 from django.core.management.commands.makemessages import Command as MakeMessagesCommand, check_programs
 
-from judge.models import NavigationBar
+from judge.models import NavigationBar, ProblemType
 
 
 class Command(MakeMessagesCommand):
@@ -121,3 +121,10 @@ msgstr ""
                 if self.verbosity > 2:
                     self.stdout.write('processing navigation item label "%s"\n' % label)
                 self._emit_message(potfile, label)
+
+            if self.verbosity > 1:
+                self.stdout.write('processing problem types')
+            for name in ProblemType.objects.values_list('full_name', flat=True):
+                if self.verbosity > 2:
+                    self.stdout.write('processing problem type name "%s"\n' % name)
+                self._emit_message(potfile, name)
