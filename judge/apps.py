9087e9e610b34021f8ec45ae3a92bfe431ad47ae
Quantum <quantum2048@gmail.com>
2015-12-25 15:23:27 -0500

    Localize judge application name #337

--- judge/apps.py
+++ judge/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
 
 
 class JudgeAppConfig(AppConfig):
     name = 'judge'
+    verbose_name = ugettext_lazy('Online Judge')
 
     def ready(self):
         from . import signals
