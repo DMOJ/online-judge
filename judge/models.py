5f000546074831d078b740624569b0ffd026542f
Quantum <quantum2048@gmail.com>
2015-12-26 13:56:12 -0500

    Aggregate doesn't actually work #237 #242

--- judge/models.py
+++ judge/models.py
@@ -12,8 +12,7 @@ from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.core.validators import RegexValidator
 from django.db import models
-from django.db.models import Max
-from django.db.models.sql.aggregates import Aggregate as SQLAggregate
+from django.db.models import Max, Lookup
 from django.utils import timezone
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
@@ -51,24 +50,6 @@ del make_timezones
 
 def fix_unicode(string, unsafe=tuple(u'\u202a\u202b\u202d\u202e')):
     return string + (sum(k in unsafe for k in string) - string.count(u'\u202c')) * u'\u202c'
-
-
-class SQLCountIf(SQLAggregate):
-    is_ordinal = True
-    sql_function = 'COUNT'
-    sql_template = '%(function)s(CASE %(condition)s WHEN true THEN 1 ELSE NULL END)'
-
-
-class CountIf(models.Aggregate):
-    name = 'COUNT'
-
-    def add_to_query(self, query, alias, col, source, is_summary):
-        sql, params = query.model._default_manager.filter(**self.extra['condition']).query.where.as_sql()
-        for search, replace in self.extra.get('sql_replace', {}).items():
-            sql = sql.replace(search, replace)
-        self.extra['condition'] = sql % tuple(params)
-        aggregate = SQLCountIf(col, source=source, is_summary=is_summary, **self.extra)
-        query.aggregates[alias] = aggregate
 
 
 class Language(models.Model):
