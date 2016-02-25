8369e4b3aeeceb4add6b3d0f1796ae0b4cc42440
Quantum <quantum2048@gmail.com>
2016-02-25 17:07:57 -0500

    Consistent quote style.

--- dmoj/wsgi.py
+++ dmoj/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmoj.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
