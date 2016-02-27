9cb69def6a9d3c30b22be9d762a4aad4c539a5a6
Quantum <quantum2048@gmail.com>
2016-02-26 22:04:48 -0500

    SQL files no longer works for #226

--- judge/sql/navigationbar.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (0, 'home', 'Home', '/', '^/$');
-INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (1, 'problem', 'Problems', '/problems/', '^/problem');
-INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (2, 'submit', 'Submissions', '/submissions/', '^/(?:submi|src/)');
-INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (3, 'user', 'Users', '/users/', '^/user');
-INSERT INTO judge_navigationbar (`order`, `key`, label, path, regex) VALUES (4, 'status', 'Status', '/status/', '^/status/$');
