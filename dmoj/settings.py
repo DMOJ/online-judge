9b16c4f82e67e6b76c0e8425c4e5091048e16301
Quantum <quantum2048@gmail.com>
2016-04-24 16:14:21 -0400

    Did I miss a 1.6 again?

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -349,7 +349,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 DMOJ_RESOURCES = os.path.join(BASE_DIR, 'resources')
 STATICFILES_FINDERS = (
