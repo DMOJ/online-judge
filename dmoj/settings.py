0d668655e20d7fc70baa165c2fdce7a4f5e66418
Quantum <quantum2048@gmail.com>
2016-05-02 01:02:49 -0400

    Another 1.6 link?

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -18,7 +18,7 @@ from markdown_trois.conf.settings import MARKDOWN_TROIS_DEFAULT_STYLE
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5*9f5q57mqmlz2#f$x1h76&jxy#yortjl1v+l*6hd18$d*yx#0'
