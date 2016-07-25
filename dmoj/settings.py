d042d3074b813359fd2b4d2be963452bc8b3aa40
Quantum <quantum2048@gmail.com>
2016-07-24 22:22:41 -0400

    TEMPLATE_DEBUG is deprecated in 1.8; #139

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = '5*9f5q57mqmlz2#f$x1h76&jxy#yortjl1v+l*6hd18$d*yx#0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
