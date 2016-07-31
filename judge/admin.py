52b4e1a2c5397672afdfc69242c1417edd163cf8
Quantum <quantum2048@gmail.com>
2016-07-31 16:02:49 -0400

    More missed references; #428

--- judge/admin.py
+++ judge/admin.py
@@ -785,7 +785,7 @@ class ContestForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(ContestForm, self).__init__(*args, **kwargs)
         if 'rate_exclude' in self.fields:
-            self.fields['rate_exclude'].queryset = Profile.objects.filter(contest__history__contest=self.instance)
+            self.fields['rate_exclude'].queryset = Profile.objects.filter(contest_history__contest=self.instance)
 
     class Meta:
         if use_select2:
