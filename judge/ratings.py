72f8acc7cb728a770b9096d801493aa4a5d22e9b
Tudor <tbrindus@gmail.com>
2016-08-01 23:21:04 -0400

    Such indent; #405

--- judge/ratings.py
+++ judge/ratings.py
@@ -117,7 +117,7 @@ def rate_contest(contest):
 
     users = contest.users.order_by('-score', 'cumtime').annotate(submissions=Count('submission')) \
                    .exclude(user_id__in=contest.rate_exclude.all(), spectate=True)\
-                    .values_list('id', 'user_id', 'score', 'cumtime')
+                   .values_list('id', 'user_id', 'score', 'cumtime')
     if not contest.rate_all:
         users = users.filter(submissions__gt=0)
     users = list(tie_ranker(users, key=itemgetter(2, 3)))
