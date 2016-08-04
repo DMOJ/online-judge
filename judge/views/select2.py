0997c4afc1c430685fb6dc05c10ecff425b21cee
Tudor <tbrindus@gmail.com>
2016-08-03 20:13:23 -0400

    Add UserSearchSelect2View for searches; #35

--- judge/views/select2.py
+++ judge/views/select2.py
@@ -4,6 +4,7 @@ from django.utils.encoding import smart_text
 from django.views.generic.list import BaseListView
 
 from judge.models import Profile, Organization, Problem, Comment, Contest
+from judge.templatetags.gravatar import get_gravatar_url
 
 
 class Select2View(BaseListView):
@@ -66,3 +67,31 @@ class ContestSelect2View(Select2View):
 class CommentSelect2View(Select2View):
     def get_queryset(self):
         return Comment.objects.filter(Q(title__icontains=self.term) | Q(page__icontains=self.term))
+
+
+class UserSearchSelect2View(BaseListView):
+    paginate_by = 20
+
+    def get(self, request, *args, **kwargs):
+        self.request = request
+        self.term = kwargs.get('term', request.GET.get('term', ''))
+        self.gravatar_size = request.GET.get('gravatar_size', 128)
+        self.gravatar_default = request.GET.get('gravatar_default', None)
+
+        self.object_list = Profile.objects.filter(Q(user__username__icontains=(self.term)) |
+                                                  Q(name__icontains=(self.term))).select_related('user')
+
+        context = self.get_context_data()
+
+        return JsonResponse({
+            'results': [
+                {
+                    'text': obj.user.username,
+                    'gravatar_url': get_gravatar_url(obj.user.email, self.gravatar_size, self.gravatar_default),
+                    'id': obj.pk,
+                } for obj in context['object_list']],
+            'more': context['page_obj'].has_next(),
+        })
+
+    def get_name(self, obj):
+        return unicode(obj)
