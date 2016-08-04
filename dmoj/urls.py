23a294dc0d1aa34d28b7f244c3f53175944479e1
Quantum <quantum2048@gmail.com>
2016-08-03 20:22:43 -0400

    I find your lack of name disturbing; #35

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -228,7 +228,7 @@ urlpatterns = [
         url(r'^submission_testcases$', submission.SubmissionTestCaseQuery.as_view(), name='submission_testcases_query'),
         url(r'^detect_timezone$', widgets.DetectTimezone.as_view(), name='detect_timezone'),
         url(r'^status-table$', status.status_table, name='status_table'),
-        url(r'^user_search_select2$', UserSearchSelect2View.as_view(), 'user_search_select2_ajax'),
+        url(r'^user_search_select2$', UserSearchSelect2View.as_view(), name='user_search_select2_ajax'),
     ])),
 
     url(r'^feed/', include([
