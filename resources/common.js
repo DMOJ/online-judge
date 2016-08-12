caf25fcc955e9611e7e06949142e50a33c8f1dc3
Tudor <tbrindus@gmail.com>
2016-08-12 17:01:07 -0400

    Remove non-DMOJ resources from main repo

--- resources/common.js
+++ resources/common.js
@@ -187,3 +187,29 @@ window.register_update_relative = function (get_times, show_relative, interval) 
 
     return update_relative_time;
 };
+
+// Placeholder polyfill
+$('input[placeholder]').each(function() {
+    if ($(this).val() == '') {
+
+        $(this).val($(this).attr('placeholder'));
+        $(this).focus(function() {
+            if ($(this).val() == $(this).attr('placeholder')) $(this).val('');
+
+        });
+        $(this).blur(function() {
+            if ($(this).val() == '') {
+                $(this).val($(this).attr('placeholder'));
+            }
+        });
+    }
+});
+
+$('form').submit(function(evt) {
+    $('input[placeholder]').each(function() {
+        if ($(this).attr('placeholder') == $(this).val()) {
+            $(this).val('');
+        }
+    });
+    evt.preventDefault();
+});
