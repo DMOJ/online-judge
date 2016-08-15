72784a4c7189493a36a4957457bf1f0c5bc678c5
Quantum <quantum2048@gmail.com>
2016-08-15 13:51:03 -0400

    Add Can I use data support for #440

---
+++ judge/utils/caniuse.py
@@ -0,0 +1,147 @@
+import json
+import urllib2
+from contextlib import closing
+
+from ua_parser import user_agent_parser
+
+with closing(urllib2.urlopen('https://raw.githubusercontent.com/Fyrd/caniuse/master/data.json')) as f:
+    _SUPPORT_DATA = json.load(f)['data']
+
+SUPPORT = 'y'
+PARTIAL_SUPPORT = 'a'
+UNSUPPORTED = 'n'
+POLYFILL = 'p'
+UNKNOWN = 'u'
+PREFIX = 'x'
+DISABLED = 'd'
+
+
+def safe_int(string):
+    try:
+        return int(string)
+    except (ValueError, TypeError):
+        return 0
+
+
+class BrowserFamily(object):
+    def __init__(self, data):
+        self._data = data
+        self._ranges = ranges = []
+        self._versions = versions = {}
+        max_version = ()
+        max_support = UNKNOWN
+
+        for version, support in data.iteritems():
+            if version == 'all':
+                self.max_support = support
+            elif '-' in version:
+                start, end = version.split('-')
+                start = tuple(map(int, start.split('.')))
+                end = tuple(map(int, end.split('.'))) + (1e3000,)
+                ranges.append((start, end, support))
+                if end > max_version:
+                    max_version = end
+                    max_support = support
+            else:
+                try:
+                    version = tuple(map(int, version.split('.')))
+                except ValueError:
+                    pass
+                else:
+                    if version > max_version:
+                        max_version = version
+                        max_support = support
+                versions[version] = support
+
+        self.max_version = max_version
+        self.max_support = max_support
+
+    def check(self, major, minor, patch):
+        int_major, int_minor, int_patch = map(safe_int, (major, minor, patch))
+
+        version = (int_major, int_minor, int_patch)
+        if version > self.max_version:
+            return self.max_support
+
+        for key in ((int_major, int_minor, int_patch), (int_major, int_minor), (int_major,), major):
+            try:
+                return self._versions[key]
+            except KeyError:
+                pass
+
+        for start, end, support in self._ranges:
+            if start <= version < end:
+                return support
+
+        return UNKNOWN
+
+
+class Feat(object):
+    def __init__(self, data):
+        self._data = data
+        self._family = {name: BrowserFamily(data) for name, data in data['stats'].iteritems()}
+
+    def __getitem__(self, item):
+        return self._family[item]
+
+
+class Database(object):
+    def __init__(self, data):
+        self._data = data
+        self._feats = {feat: Feat(data) for feat, data in data.iteritems()}
+
+    def __getitem__(self, item):
+        return self._feats[item]
+
+
+database = Database(_SUPPORT_DATA)
+
+
+class CanIUse(object):
+    def __init__(self, ua):
+        self._agent = user_agent_parser.Parse(ua)
+
+        os_family = self._agent['os']['family']
+        browser_family = self._agent['user_agent']['family']
+
+        family = None
+
+        if os_family == 'Android':
+            if 'Firefox' in browser_family:
+                family = 'and_ff'
+            elif 'Chrome' in browser_family:
+                family = 'and_chr'
+            elif 'Android' in browser_family:
+                family = 'android'
+        else:
+            if 'Edge' in browser_family:
+                family = 'edge'
+            elif 'Firefox' in browser_family:
+                family = 'firefox'
+            elif 'Chrome' in browser_family:
+                family = 'chrome'
+            elif 'IE' in browser_family:
+                family = 'ie'
+            elif 'Opera' in browser_family:
+                family = 'opera'
+            elif 'Safari' in browser_family:
+                family = 'safari'
+
+        self._family = family
+
+    def __getattr__(self, attr):
+        try:
+            feat = database[attr.replace('_', '-')]
+        except KeyError:
+            raise AttributeError(attr)
+        else:
+            if not self._family:
+                return UNKNOWN
+
+            try:
+                stats = feat[self._family]
+            except KeyError:
+                return UNKNOWN
+            else:
+                ua = self._agent['user_agent']
+                return stats.check(ua['major'], ua['minor'], ua['patch'])[0]
