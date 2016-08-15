627b6aff8401afd663ec689641ea8673e7fc3008
Tudor <tbrindus@gmail.com>
2016-08-15 16:11:11 -0400

    Implement basic Texoid support

--- judge/templatetags/latex_math.py
+++ judge/templatetags/latex_math.py
@@ -2,7 +2,7 @@ from django import template
 from lxml import html
 
 from judge import lxml_tree
-from judge.utils.texoid import get_texoid_url
+from judge.utils.texoid import get_result
 
 register = template.Library()
 
@@ -12,8 +12,8 @@ def latex_math(text):
     tree = lxml_tree.fromstring(text)
     for latex in tree.xpath('.//latex'):
         img = html.Element('img')
-        svg, png = get_texoid_url(latex.text)
-        img.set('src', svg)
-        img.set('onerror', "this.src='%s'" % png)
+        result = get_result(latex.text)
+        img.set('src', result['svg'])
+        img.set('onerror', "this.src='%s'" % result['png'])
         latex.getParent().replace(latex, img)
     return tree
--- judge/utils/texoid.py
+++ judge/utils/texoid.py
@@ -1,2 +1,82 @@
-def get_texoid_url(latex):
-    pass
+import hashlib
+import json
+import os
+import urllib2
+from contextlib import closing
+from urllib import urlencode
+from urlparse import urljoin
+
+import errno
+
+from dmoj import settings
+from judge.judgeapi import logger
+
+
+def cache_data(hash, file, data, url=True):
+    with open(os.path.join(settings.TEXOID_CACHE_DIR, hash, file), 'wb') as f:
+        f.write(data)
+    if url:
+        return urljoin(settings.TEXOID_CACHE_URL, '%s/%s' % (hash, file))
+
+
+def query_mathoid(formula, hash):
+    try:
+        os.makedirs(os.path.join(settings.TEXOID_CACHE_DIR, hash))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    try:
+        request = urllib2.urlopen(settings.TEXOID_URL, urlencode({
+            'q': formula
+        }))
+    except urllib2.HTTPError as e:
+        if e.code == 400:
+            logger.error('Texoid failed to render: %s\n%s', formula, e.read())
+        logger.exception('Failed to connect to texoid for: %s' % formula)
+        return
+    except Exception:
+        logger.exception('Failed to connect to texoid for: %s' % formula)
+        return
+    with closing(request) as f:
+        data = f.read()
+        try:
+            data = json.loads(data)
+        except ValueError:
+            logger.exception('Invalid texoid response for: %s\n%s', formula, data)
+            return
+
+    if not data['success']:
+        logger.error('Texoid failure for: %s\n%s', formula, data)
+        return
+
+    result = {
+        'png': cache_data(hash, 'png', data['png'].decode('base64')),
+        'svg': cache_data(hash, 'svg', data['svg'].encode('utf-8')),
+    }
+    return result
+
+
+def cache_complete(hash):
+    return os.path.isfile(os.path.join(settings.TEXOID_CACHE_DIR, hash, 'css'))
+
+
+def query_cache(hash):
+    result = {
+        'svg': urljoin(settings.TEXOID_CACHE_URL, '%s/svg' % hash),
+        'png': urljoin(settings.TEXOID_CACHE_URL, '%s/png' % hash),
+    }
+    return result
+
+
+def get_result(formula):
+    hash = hashlib.sha1(formula).hexdigest()
+    if cache_complete(hash):
+        result = query_cache(hash)
+    else:
+        result = query_mathoid(formula, hash)
+
+    if not result:
+        return None
+
+    return result
