329f06a9726337d99c9e74992b44263070a604db
Quantum <quantum2048@gmail.com>
2016-08-15 01:14:01 -0400

    Do not think one should register a handler; #440

--- resources/pagedown_math.js
+++ resources/pagedown_math.js
@@ -1,17 +1,17 @@
 function mathjax_pagedown($) {
     if ('MathJax' in window) {
-        $(window).load(function () {
-            $.each(window.editors, function (id, editor) {
-                var preview = $('div.wmd-preview#' + id + '_wmd_preview')[0];
-                editor.hooks.chain('onPreviewRefresh', function () {
-                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, preview]);
-                });
+        $.each(window.editors, function (id, editor) {
+            var preview = $('div.wmd-preview#' + id + '_wmd_preview')[0];
+            editor.hooks.chain('onPreviewRefresh', function () {
                 MathJax.Hub.Queue(["Typeset", MathJax.Hub, preview]);
             });
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, preview]);
         });
     }
-};
+}
 
 window.mathjax_pagedown = mathjax_pagedown;
 
-(mathjax_pagedown)('$' in window ? $ : django.jQuery);
+$(window).load(function () {
+    (mathjax_pagedown)('$' in window ? $ : django.jQuery);
+});
