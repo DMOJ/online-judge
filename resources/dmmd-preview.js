97637a805bce6488c94ee41d65a7a616e4c6b641
Quantum <quantum2048@gmail.com>
2016-08-17 00:54:28 -0400

    #444 is not present in every formset

--- resources/dmmd-preview.js
+++ resources/dmmd-preview.js
@@ -28,9 +28,11 @@ $(function () {
     if ('django' in window && 'jQuery' in window.django)
         django.jQuery(document).on('formset:added', function(event, $row) {
             var $preview = $row.find('.dmmd-preview');
-            var id = $row.attr('id');
-            id = id.substr(id.lastIndexOf('-') + 1);
-            $preview.attr('data-textarea-id', $preview.attr('data-textarea-id').replace('__prefix__', id));
-            register_dmmd_preview($preview);
+            if ($preview.length) {
+                var id = $row.attr('id');
+                id = id.substr(id.lastIndexOf('-') + 1);
+                $preview.attr('data-textarea-id', $preview.attr('data-textarea-id').replace('__prefix__', id));
+                register_dmmd_preview($preview);
+            }
         });
 });
