65d4ca0127ce1be3e9a1d0775b5a68d4f8c95417
Quantum <quantum2048@gmail.com>
2016-08-16 21:29:50 -0400

    Make the preview widget work; #444

--- resources/dmmd-preview.js
+++ resources/dmmd-preview.js
@@ -1,3 +1,21 @@
 $(function () {
-    
+    window.register_dmmd_preview = function ($preview) {
+        var $update = $preview.find('.dmmd-preview-update');
+        var $content = $preview.find('.dmmd-preview-content');
+        var preview_url = $preview.attr('data-preview-url');
+
+        $update.click(function () {
+            var $textarea = $('#' + $preview.attr('data-textarea-id'));
+            $.post(preview_url, {
+                preview: $textarea.val(),
+                csrfmiddlewaretoken: $.cookie('csrftoken')
+            }, function (result) {
+                $content.html(result);
+            });
+        }).click();
+    };
+
+    $('.dmmd-preview').each(function () {
+        register_dmmd_preview($(this));
+    });
 });
