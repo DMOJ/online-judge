9b48652b472cc4d26351f20f939d0409679069a9
Tudor <tbrindus@gmail.com>
2016-08-20 13:17:54 -0400

    Minor UX changes

--- judge/views/register.py
+++ judge/views/register.py
@@ -4,7 +4,7 @@ import re
 from django import forms
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.forms import CharField, ChoiceField, ModelChoiceField, TextInput
+from django.forms import CharField, ChoiceField, ModelChoiceField, TextInput, PasswordInput
 from django.shortcuts import render
 from django.utils.translation import ugettext, ugettext_lazy as _
 from registration.backends.default.views import (RegistrationView as OldRegistrationView,
@@ -38,8 +38,8 @@ class CustomRegistrationForm(RegistrationForm):
         super(CustomRegistrationForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget = TextInput(attrs={'placeholder': _('john_doe')})
         self.fields['email'].widget = TextInput(attrs={'placeholder': _('john.doe@company.com')})
-        self.fields['password1'].widget = TextInput(attrs={'placeholder': _(u'•••••••••••')})
-        self.fields['password2'].widget = TextInput(attrs={'placeholder': _(u'•••••••••••')})
+        self.fields['password1'].widget = PasswordInput(attrs={'placeholder': _(u'•••••••••••')})
+        self.fields['password2'].widget = PasswordInput(attrs={'placeholder': _(u'•••••••••••')})
 
     def clean_email(self):
         if User.objects.filter(email=self.cleaned_data['email']).exists():
