69799dd4be192308a7e5417a5e3a8ad45d9224c8
Tudor Brindus <tbrindus@gmail.com>
2016-09-12 20:07:40 -0400

    Why not me?

--- judge/admin.py
+++ judge/admin.py
@@ -1041,7 +1041,7 @@ class BlogPostAdmin(VersionAdmin):
         }
 
     def has_change_permission(self, request, obj=None):
-        return (request.user.has_perm('judge.see_hidden_post') and
+        return request.user.is_superuser or (request.user.has_perm('judge.see_hidden_post') and
                 (obj is None or obj.authors.filter(id=request.user.profile.id).exists()))
 
 
