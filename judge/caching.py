3804be0730a541b6d89f4a98d539fecdf0206b02
Tudor <tbrindus@gmail.com>
2016-09-14 18:42:12 -0400

    Clear cache on finished_submission; #463

--- judge/caching.py
+++ judge/caching.py
@@ -2,8 +2,9 @@ from django.core.cache import cache
 
 
 def finished_submission(sub):
-    keys = ['user_complete:%d' % sub.user_id]
+    keys = ['user_complete:%d' % sub.user_id, 'user_attempted:%s' % sub.user_id]
     if hasattr(sub, 'contest'):
         participation = sub.contest.participation
         keys += ['contest_complete:%d' % participation.id]
+        keys += ['contest_attempted:%d' % participation.id]
     cache.delete_many(keys)
