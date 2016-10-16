581a5234bddd49eceaacdc257cd89d0a596225b5
Quantum <quantum2048@gmail.com>
2016-10-16 17:38:38 -0400

    Don't fall for case issues; #494

--- judge/views/register.py
+++ judge/views/register.py
@@ -42,7 +42,7 @@ class CustomRegistrationForm(RegistrationForm):
             raise forms.ValidationError(ugettext(u'The email address "%s" is already taken. Only one registration '
                                                  u'is allowed per address.') % self.cleaned_data['email'])
         if '@' in self.cleaned_data['email']:
-            domain = self.cleaned_data['email'].split('@')[-1]
+            domain = self.cleaned_data['email'].split('@')[-1].lower()
             if (domain in getattr(settings, 'BAD_MAIL_PROVIDERS', ())
                     or any(regex.match(domain) for regex in bad_mail_regex)):
                 raise forms.ValidationError(ugettext(u'Your email provider is not allowed due to history of abuse. '
