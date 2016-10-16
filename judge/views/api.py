cb7a5a612717deee6fc9381b838cec5a31a444ca
Quantum <quantum2048@gmail.com>
2016-10-16 01:44:11 -0400

    Properly formatted API code

--- judge/views/api.py
+++ judge/views/api.py
@@ -1,13 +1,12 @@
 from operator import attrgetter
-from collections import defaultdict
 
 from django.db.models import Prefetch, F
 from django.http import JsonResponse, Http404
 from django.shortcuts import get_object_or_404
 
 from dmoj import settings
-from judge.views.contests import *
 from judge.models import Contest, Problem, Profile, Submission, ContestTag
+from judge.views.contests import contest_access_check, base_contest_ranking_list
 
 
 def sane_time_repr(delta):
@@ -18,17 +17,17 @@ def sane_time_repr(delta):
 
 
 def api_contest_list(request):
-    contests = {}
-    for c in Contest.objects.filter(is_public=True, is_private=False).prefetch_related(
-            Prefetch('tags', queryset=ContestTag.objects.only('name'), to_attr='tag_list')).defer('description'):
-        contests[c.key] = {
-            'name': c.name,
-            'start_time': c.start_time.isoformat(),
-            'end_time': c.end_time.isoformat(),
-            'time_limit': c.time_limit and sane_time_repr(c.time_limit),
-            'labels': map(attrgetter('name'), c.tag_list),
-        }
-    return JsonResponse(contests)
+    queryset = Contest.objects.filter(is_public=True, is_private=False).prefetch_related(
+        Prefetch('tags', queryset=ContestTag.objects.only('name'), to_attr='tag_list')).defer('description')
+
+    return JsonResponse({c.key: {
+        'name': c.name,
+        'start_time': c.start_time.isoformat(),
+        'end_time': c.end_time.isoformat(),
+        'time_limit': c.time_limit and sane_time_repr(c.time_limit),
+        'labels': map(attrgetter('name'), c.tag_list),
+    } for c in queryset})
+
 
 def api_contest_detail(request, contest):
     contest = get_object_or_404(Contest, key=contest)
@@ -37,50 +36,50 @@ def api_contest_detail(request, contest):
     problems = list(contest.contest_problems.select_related('problem')
                     .defer('problem__description').order_by('order'))
     users = base_contest_ranking_list(contest, problems, contest.users.filter(virtual=0)
-                                     .prefetch_related('user__organizations')
-                                     .order_by('-score', 'cumtime'))
-    resp = {
-        'problems': [{
-                        'points': int(problem.points),
-                        'partial': problem.partial,
-                        'name': str(problem.problem.name),
-                        'code': str(problem.problem.code)
-                    } for problem in problems],
-        'rankings': [{
-                        'user': user.user.username,
-                        'points': user.points,
-                        'cumtime': user.cumtime,
-                        'solutions': [{
-                                        'points': int(sol.points),
-                                        'time': sol.time.total_seconds()
-                                    } if sol else None for sol in user.problems]
-                    } for user in users]
-    }
-    
-    return JsonResponse(resp)
+                                      .prefetch_related('user__organizations')
+                                      .order_by('-score', 'cumtime'))
+    return JsonResponse({
+        'problems': [
+            {
+                'points': int(problem.points),
+                'partial': problem.partial,
+                'name': str(problem.problem.name),
+                'code': str(problem.problem.code)
+            } for problem in problems],
+        'rankings': [
+            {
+                'user': user.user.username,
+                'points': user.points,
+                'cumtime': user.cumtime,
+                'solutions': [{
+                                  'points': int(sol.points),
+                                  'time': sol.time.total_seconds()
+                              } if sol else None for sol in user.problems]
+            } for user in users]
+    })
+
 
 def api_problem_list(request):
-    qs = Problem.objects.filter(is_public=True)
+    queryset = Problem.objects.filter(is_public=True)
     if settings.ENABLE_FTS and 'search' in request.GET:
         query = ' '.join(request.GET.getlist('search')).strip()
         if query:
-            qs = qs.search(query)
+            queryset = queryset.search(query)
+    queryset = queryset.values_list('code', 'points', 'partial', 'name', 'group__full_name')
 
-    problems = {}
-    for code, points, partial, name, group in qs.values_list('code', 'points', 'partial', 'name', 'group__full_name'):
-        problems[code] = {
-            'points': points,
-            'partial': partial,
-            'name': name,
-            'group': group
-        }
-    return JsonResponse(problems)
+    return JsonResponse({code: {
+        'points': points,
+        'partial': partial,
+        'name': name,
+        'group': group
+    } for code, points, partial, name, group in queryset})
 
 
 def api_problem_info(request, problem):
     p = get_object_or_404(Problem, code=problem)
     if not p.is_accessible_by(request.user):
         raise Http404()
+
     return JsonResponse({
         'name': p.name,
         'authors': list(p.authors.values_list('user__username', flat=True)),
@@ -95,43 +94,37 @@ def api_problem_info(request, problem):
 
 
 def api_user_list(request):
-    users = {}
-    for username, name, points, rank in Profile.objects.values_list('user__username', 'name', 'points', 'display_rank'):
-        users[username] = {
-            'display_name': name,
-            'points': points,
-            'rank': rank
-        }
-    return JsonResponse(users)
+    queryset = Profile.objects.values_list('user__username', 'name', 'points', 'display_rank')
+    return JsonResponse({username: {
+        'display_name': name,
+        'points': points,
+        'rank': rank
+    } for username, name, points, rank in queryset})
 
 
 def api_user_info(request, user):
-    p = get_object_or_404(Profile, user__username=user)
-    submissions = list(Submission.objects.filter(case_points=F('case_total'), user=p, problem__is_public=True)
+    profile = get_object_or_404(Profile, user__username=user)
+    submissions = list(Submission.objects.filter(case_points=F('case_total'), user=profile, problem__is_public=True)
                        .values('problem').distinct().values_list('problem__code', flat=True))
     return JsonResponse({
-        'display_name': p.name,
-        'points': p.points,
-        'rank': p.display_rank,
+        'display_name': profile.name,
+        'points': profile.points,
+        'rank': profile.display_rank,
         'solved_problems': submissions,
-        'organizations': list(p.organizations.values_list('key', flat=True)),
+        'organizations': list(profile.organizations.values_list('key', flat=True)),
     })
 
 
 def api_user_submissions(request, user):
-    p = get_object_or_404(Profile, user__username=user)
+    profile = get_object_or_404(Profile, user__username=user)
+    subs = Submission.objects.filter(user=profile, problem__is_public=True)
 
-    subs = Submission.objects.filter(user=p, problem__is_public=True)
-    data = {}
-
-    for s in subs.values('id', 'problem__code', 'time', 'memory', 'points', 'language__key', 'status', 'result'):
-        data[s['id']] = {
-            'problem': s['problem__code'],
-            'time': s['time'],
-            'memory': s['memory'],
-            'points': s['points'],
-            'language': s['language__key'],
-            'status': s['status'],
-            'result': s['result'],
-        }
-    return JsonResponse(data)
+    return JsonResponse({sub['id']: {
+        'problem': sub['problem__code'],
+        'time': sub['time'],
+        'memory': sub['memory'],
+        'points': sub['points'],
+        'language': sub['language__key'],
+        'status': sub['status'],
+        'result': sub['result'],
+    } for sub in subs.values('id', 'problem__code', 'time', 'memory', 'points', 'language__key', 'status', 'result')})
