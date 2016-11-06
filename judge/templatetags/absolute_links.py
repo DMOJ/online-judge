2a7b05d0952ed03f99fc57d77738af0f02d9d8ab
Quantum <quantum2048@gmail.com>
2016-11-06 15:33:31 -0500

    PDF should use absolute links

---
+++ judge/templatetags/absolute_links.py
@@ -0,0 +1,15 @@
+from django import template
+
+from judge import lxml_tree
+
+register = template.Library()
+
+
+@register.filter(is_safe=True, name='absolutify')
+def absolute_links(text, request):
+    tree = lxml_tree.fromstring(text)
+    for anchor in tree.xpath('.//a'):
+        href = anchor.get('href')
+        if href:
+            anchor.set('href', request.build_absolute_uri(href))
+    return tree
