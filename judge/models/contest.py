88c036890463bda9b2c77846f2642db98998eae9
Quantum <quantum2048@gmail.com>
2016-11-12 22:23:42 -0500

    Move contest models out; #493

---
+++ judge/models/contest.py
@@ -0,0 +1,262 @@
+from operator import itemgetter
+
+from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse
+from django.core.validators import RegexValidator
+from django.db import models
+from django.db.models import Max
+from django.utils import timezone
+from django.utils.functional import cached_property
+from django.utils.translation import ugettext_lazy as _, ugettext
+from timedelta import TimedeltaField
+
+from judge.models.problem import Problem
+from judge.models.profile import Profile, Organization
+from judge.models.submission import Submission
+
+__all__ = ['Contest', 'ContestTag', 'ContestParticipation', 'ContestProblem', 'ContestSubmission']
+
+
+class ContestTag(models.Model):
+    color_validator = RegexValidator('^#(?:[A-Fa-f0-9]{3}){1,2}$', _('Invalid colour.'))
+
+    name = models.CharField(max_length=20, verbose_name=_('tag name'), unique=True,
+                            validators=[RegexValidator(r'^[a-z-]+$', message=_('Lowercase letters and hyphens only.'))])
+    color = models.CharField(max_length=7, verbose_name=_('tag colour'), validators=[color_validator])
+    description = models.TextField(verbose_name=_('tag description'), blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('contest_tag', args=[self.name])
+
+    @property
+    def text_color(self, cache={}):
+        if self.color not in cache:
+            if len(self.color) == 4:
+                r, g, b = [ord((i * 2).decode('hex')) for i in self.color[1:]]
+            else:
+                r, g, b = [ord(i) for i in self.color[1:].decode('hex')]
+            cache[self.color] = '#000' if 299 * r + 587 * g + 144 * b > 140000 else '#fff'
+        return cache[self.color]
+
+    class Meta:
+        verbose_name = _('contest tag')
+        verbose_name_plural = _('contest tags')
+
+
+class Contest(models.Model):
+    key = models.CharField(max_length=20, verbose_name=_('contest id'), unique=True,
+                           validators=[RegexValidator('^[a-z0-9]+$', _('Contest id must be ^[a-z0-9]+$'))])
+    name = models.CharField(max_length=100, verbose_name=_('contest name'), db_index=True)
+    organizers = models.ManyToManyField(Profile, help_text=_('These people will be able to edit the contest.'),
+                                        related_name='organizers+')
+    description = models.TextField(blank=True)
+    problems = models.ManyToManyField(Problem, verbose_name=_('problems'), through='ContestProblem')
+    start_time = models.DateTimeField(db_index=True)
+    end_time = models.DateTimeField(db_index=True)
+    time_limit = TimedeltaField(verbose_name=_('time limit'), blank=True, null=True)
+    is_public = models.BooleanField(verbose_name=_('publicly visible'), default=False,
+                                    help_text=_('Should be set even for organization-private contests, where it '
+                                                'determines whether the contest is visible to members of the '
+                                                'specified organizations.'))
+    is_rated = models.BooleanField(verbose_name=_('contest rated'), help_text=_('Whether this contest can be rated.'),
+                                   default=False)
+    rate_all = models.BooleanField(verbose_name=_('rate all'), help_text=_('Rate all users who joined.'), default=False)
+    rate_exclude = models.ManyToManyField(Profile, verbose_name=_('exclude from ratings'), blank=True,
+                                          related_name='rate_exclude+')
+    is_private = models.BooleanField(verbose_name=_('private to organizations'), default=False)
+    hide_problem_tags = models.BooleanField(verbose_name=_('hide problem tags'),
+                                            help_text=_('Whether problem tags should be hidden by default.'),
+                                            default=False)
+    run_pretests_only = models.BooleanField(verbose_name=_('run pretests only'),
+                                            help_text=_('Whether judges should grade pretests only, versus all '
+                                                        'testcases. Commonly set during a contest, then unset '
+                                                        'prior to rejudging user submissions when the contest ends.'),
+                                            default=False)
+    organizations = models.ManyToManyField(Organization, blank=True, verbose_name=_('organizations'),
+                                           help_text=_('If private, only these organizations may see the contest'))
+    og_image = models.CharField(verbose_name=_('OpenGraph image'), default='', max_length=150, blank=True)
+    tags = models.ManyToManyField(ContestTag, verbose_name=_('contest tags'), blank=True, related_name='contests')
+    user_count = models.IntegerField(verbose_name=_('the amount of live participants'), default=0)
+    summary = models.TextField(blank=True, verbose_name=_('contest summary'),
+                               help_text=_('Plain-text, shown in meta description tag, e.g. for social media.'))
+
+    def clean(self):
+        if self.start_time >= self.end_time:
+            raise ValidationError('What is this? A contest that ended before it starts?')
+
+    @property
+    def contest_window_length(self):
+        return self.end_time - self.start_time
+
+    @cached_property
+    def _now(self):
+        # This ensures that all methods talk about the same now.
+        return timezone.now()
+
+    @cached_property
+    def can_join(self):
+        return self.start_time <= self._now
+
+    @property
+    def time_before_start(self):
+        if self.start_time >= self._now:
+            return self.start_time - self._now
+        else:
+            return None
+
+    @property
+    def time_before_end(self):
+        if self.end_time >= self._now:
+            return self.end_time - self._now
+        else:
+            return None
+
+    @cached_property
+    def ended(self):
+        return self.end_time < self._now
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('contest_view', args=(self.key,))
+
+    def update_user_count(self):
+        self.user_count = self.users.filter(virtual=0).count()
+        self.save()
+
+    update_user_count.alters_data = True
+
+    class Meta:
+        permissions = (
+            ('see_private_contest', 'See private contests'),
+            ('edit_own_contest', 'Edit own contests'),
+            ('edit_all_contest', 'Edit all contests'),
+            ('contest_rating', 'Rate contests'),
+        )
+        verbose_name = _('contest')
+        verbose_name_plural = _('contests')
+
+
+class ContestParticipation(models.Model):
+    contest = models.ForeignKey(Contest, verbose_name=_('associated contest'), related_name='users')
+    user = models.ForeignKey(Profile, verbose_name=_('user'), related_name='contest_history')
+    real_start = models.DateTimeField(verbose_name=_('start time'), default=timezone.now, db_column='start')
+    score = models.IntegerField(verbose_name=_('score'), default=0, db_index=True)
+    cumtime = models.PositiveIntegerField(verbose_name=_('cumulative time'), default=0)
+    virtual = models.IntegerField(verbose_name=_('virtual participation id'), default=0,
+                                  help_text=_('0 means non-virtual, otherwise the n-th virtual participation'))
+
+    def recalculate_score(self):
+        self.score = sum(map(itemgetter('points'),
+                             self.submissions.values('submission__problem').annotate(points=Max('points'))))
+        self.save()
+        return self.score
+
+    @property
+    def spectate(self):
+        return self.virtual == -1
+
+    @cached_property
+    def start(self):
+        contest = self.contest
+        return contest.start_time if contest.time_limit is None and not self.virtual > 0 else self.real_start
+
+    @cached_property
+    def end_time(self):
+        contest = self.contest
+        if self.spectate:
+            return contest.end_time
+        if self.virtual:
+            if contest.time_limit:
+                return self.real_start + contest.time_limit
+            else:
+                return self.real_start + (contest.end_time - contest.start_time)
+        return contest.end_time if contest.time_limit is None else \
+            min(self.real_start + contest.time_limit, contest.end_time)
+
+    @property
+    def ended(self):
+        return self.end_time is not None and self.end_time < timezone.now()
+
+    @property
+    def time_remaining(self):
+        now = timezone.now()
+        end = self.end_time
+        if end is not None and end >= now:
+            return end - now
+
+    def update_cumtime(self):
+        cumtime = 0
+        for problem in self.contest.contest_problems.all():
+            solution = problem.submissions.filter(participation=self, points__gt=0) \
+                .values('submission__user_id').annotate(time=Max('submission__date'))
+            if not solution:
+                continue
+            dt = solution[0]['time'] - self.start
+            cumtime += dt.days * 86400 + dt.seconds
+        self.cumtime = cumtime
+        self.save()
+
+    update_cumtime.alters_data = True
+
+    def __unicode__(self):
+        if self.spectate:
+            return ugettext('%s spectating in %s') % (self.user.long_display_name, self.contest.name)
+        if self.virtual:
+            return ugettext('%s in %s, v%d') % (self.user.long_display_name, self.contest.name, self.virtual)
+        return ugettext('%s in %s') % (self.user.long_display_name, self.contest.name)
+
+    class Meta:
+        verbose_name = _('contest participation')
+        verbose_name_plural = _('contest participations')
+
+        unique_together = ('contest', 'user', 'virtual')
+
+
+class ContestProblem(models.Model):
+    problem = models.ForeignKey(Problem, verbose_name=_('problem'), related_name='contests')
+    contest = models.ForeignKey(Contest, verbose_name=_('contest'), related_name='contest_problems')
+    points = models.IntegerField(verbose_name=_('points'))
+    partial = models.BooleanField(default=True, verbose_name=_('partial'))
+    order = models.PositiveIntegerField(db_index=True, verbose_name=_('order'))
+    output_prefix_override = models.IntegerField(verbose_name=_('output prefix length override'), null=True, blank=True)
+
+    class Meta:
+        unique_together = ('problem', 'contest')
+        verbose_name = _('contest problem')
+        verbose_name_plural = _('contest problems')
+
+
+class ContestSubmission(models.Model):
+    submission = models.OneToOneField(Submission, verbose_name=_('submission'), related_name='contest')
+    problem = models.ForeignKey(ContestProblem, verbose_name=_('problem'),
+                                related_name='submissions', related_query_name='submission')
+    participation = models.ForeignKey(ContestParticipation, verbose_name=_('participation'),
+                                      related_name='submissions', related_query_name='submission')
+    points = models.FloatField(default=0.0, verbose_name=_('points'))
+    is_pretest = models.BooleanField(verbose_name=_('is pretested'),
+                                     help_text=_('Whether this submission was ran only on pretests.'),
+                                     default=False)
+
+    class Meta:
+        verbose_name = _('contest submission')
+        verbose_name_plural = _('contest submissions')
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(Profile, verbose_name=_('user'), related_name='ratings')
+    contest = models.ForeignKey(Contest, verbose_name=_('contest'), related_name='ratings')
+    participation = models.OneToOneField(ContestParticipation, verbose_name=_('participation'), related_name='rating')
+    rank = models.IntegerField(verbose_name=_('rank'))
+    rating = models.IntegerField(verbose_name=_('rating'))
+    volatility = models.IntegerField(verbose_name=_('volatility'))
+    last_rated = models.DateTimeField(db_index=True, verbose_name=_('last rated'))
+
+    class Meta:
+        unique_together = ('user', 'contest')
+        verbose_name = _('contest rating')
+        verbose_name_plural = _('contest ratings')
