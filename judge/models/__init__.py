946228d8b63a4b6a7831d11105671aafd39b03da
Quantum <quantum2048@gmail.com>
2016-11-12 22:14:45 -0500

    Move site managed data models out; #493

--- judge/models/__init__.py
+++ judge/models/__init__.py
@@ -1,5 +1,4 @@
 import itertools
-import os
 import re
 from collections import OrderedDict
 from operator import itemgetter, attrgetter
@@ -25,73 +24,10 @@ from judge.judgeapi import judge_submission, abort_submission
 from judge.models.choices import TIMEZONE, ACE_THEMES, MATH_ENGINES_CHOICES, EFFECTIVE_MATH_ENGINES
 from judge.models.problem import ProblemGroup, ProblemType, Problem, ProblemTranslation, TranslatedProblemQuerySet, \
     TranslatedProblemForeignKeyQuerySet, License
+from judge.models.problem_data import problem_data_storage, problem_directory_file, ProblemData, ProblemTestCase, \
+    CHECKERS
 from judge.models.profile import Profile, Organization, OrganizationRequest
 from judge.models.runtimes import Language, RuntimeVersion
-from judge.utils.problem_data import ProblemDataStorage
-
-problem_data_storage = ProblemDataStorage()
-
-
-def problem_directory_file(data, filename):
-    return os.path.join(data.problem.code, os.path.basename(filename))
-
-
-CHECKERS = (
-    ('standard', _('Standard')),
-    ('floats', _('Floats')),
-    ('floatsabs', _('Floats (absolute)')),
-    ('floatsrel', _('Floats (relative)')),
-    ('rstripped', _('Non-trailing spaces')),
-    ('sorted', _('Unordered')),
-    ('identical', _('Byte identical')),
-)
-
-
-class ProblemData(models.Model):
-    problem = models.OneToOneField(Problem, verbose_name=_('problem'), related_name='data_files')
-    zipfile = models.FileField(verbose_name=_('data zip file'), storage=problem_data_storage, null=True, blank=True,
-                               upload_to=problem_directory_file)
-    generator = models.FileField(verbose_name=_('generator file'), storage=problem_data_storage, null=True, blank=True,
-                                 upload_to=problem_directory_file)
-    output_prefix = models.IntegerField(verbose_name=_('output prefix length'), blank=True, null=True)
-    output_limit = models.IntegerField(verbose_name=_('output limit length'), blank=True, null=True)
-    feedback = models.TextField(verbose_name=_('init.yml generation feedback'), blank=True)
-    checker = models.CharField(max_length=10, verbose_name=_('checker'), choices=CHECKERS, blank=True)
-    checker_args = models.TextField(verbose_name=_('checker arguments'), blank=True,
-                                    help_text=_('checker arguments as a JSON object'))
-
-    __original_zipfile = None
-
-    def __init__(self, *args, **kwargs):
-        super(ProblemData, self).__init__(*args, **kwargs)
-        self.__original_zipfile = self.zipfile
-
-    def save(self, *args, **kwargs):
-        if self.zipfile != self.__original_zipfile:
-            self.__original_zipfile.delete(save=False)
-        return super(ProblemData, self).save(*args, **kwargs)
-
-    def has_yml(self):
-        return problem_data_storage.exists('%s/init.yml' % self.problem.code)
-
-
-class ProblemTestCase(models.Model):
-    dataset = models.ForeignKey(Problem, verbose_name=_('problem data set'), related_name='cases')
-    order = models.IntegerField(verbose_name=_('case position'))
-    type = models.CharField(max_length=1, verbose_name=_('case type'),
-                            choices=(('C', _('Normal case')),
-                                     ('S', _('Batch start')),
-                                     ('E', _('Batch end'))))
-    input_file = models.CharField(max_length=100, verbose_name=_('input file name'), blank=True)
-    output_file = models.CharField(max_length=100, verbose_name=_('output file name'), blank=True)
-    generator_args = models.TextField(verbose_name=_('generator arguments'), blank=True)
-    points = models.IntegerField(verbose_name=_('point value'), blank=True, null=True)
-    is_pretest = models.BooleanField(verbose_name=_('case is pretest?'))
-    output_prefix = models.IntegerField(verbose_name=_('output prefix length'), blank=True, null=True)
-    output_limit = models.IntegerField(verbose_name=_('output limit length'), blank=True, null=True)
-    checker = models.CharField(max_length=10, verbose_name=_('checker'), choices=CHECKERS, blank=True)
-    checker_args = models.TextField(verbose_name=_('checker arguments'), blank=True,
-                                    help_text=_('checker arguments as a JSON object'))
 
 
 class LanguageLimit(models.Model):
---
+++ judge/models/problem_data.py
@@ -0,0 +1,72 @@
+import os
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from judge.utils.problem_data import ProblemDataStorage
+
+__all__ = ['problem_data_storage', 'problem_directory_file', 'ProblemData', 'ProblemTestCase', 'CHECKERS']
+
+problem_data_storage = ProblemDataStorage()
+
+
+def problem_directory_file(data, filename):
+    return os.path.join(data.problem.code, os.path.basename(filename))
+
+
+CHECKERS = (
+    ('standard', _('Standard')),
+    ('floats', _('Floats')),
+    ('floatsabs', _('Floats (absolute)')),
+    ('floatsrel', _('Floats (relative)')),
+    ('rstripped', _('Non-trailing spaces')),
+    ('sorted', _('Unordered')),
+    ('identical', _('Byte identical')),
+)
+
+
+class ProblemData(models.Model):
+    problem = models.OneToOneField('Problem', verbose_name=_('problem'), related_name='data_files')
+    zipfile = models.FileField(verbose_name=_('data zip file'), storage=problem_data_storage, null=True, blank=True,
+                               upload_to=problem_directory_file)
+    generator = models.FileField(verbose_name=_('generator file'), storage=problem_data_storage, null=True, blank=True,
+                                 upload_to=problem_directory_file)
+    output_prefix = models.IntegerField(verbose_name=_('output prefix length'), blank=True, null=True)
+    output_limit = models.IntegerField(verbose_name=_('output limit length'), blank=True, null=True)
+    feedback = models.TextField(verbose_name=_('init.yml generation feedback'), blank=True)
+    checker = models.CharField(max_length=10, verbose_name=_('checker'), choices=CHECKERS, blank=True)
+    checker_args = models.TextField(verbose_name=_('checker arguments'), blank=True,
+                                    help_text=_('checker arguments as a JSON object'))
+
+    __original_zipfile = None
+
+    def __init__(self, *args, **kwargs):
+        super(ProblemData, self).__init__(*args, **kwargs)
+        self.__original_zipfile = self.zipfile
+
+    def save(self, *args, **kwargs):
+        if self.zipfile != self.__original_zipfile:
+            self.__original_zipfile.delete(save=False)
+        return super(ProblemData, self).save(*args, **kwargs)
+
+    def has_yml(self):
+        return problem_data_storage.exists('%s/init.yml' % self.problem.code)
+
+
+class ProblemTestCase(models.Model):
+    dataset = models.ForeignKey('Problem', verbose_name=_('problem data set'), related_name='cases')
+    order = models.IntegerField(verbose_name=_('case position'))
+    type = models.CharField(max_length=1, verbose_name=_('case type'),
+                            choices=(('C', _('Normal case')),
+                                     ('S', _('Batch start')),
+                                     ('E', _('Batch end'))))
+    input_file = models.CharField(max_length=100, verbose_name=_('input file name'), blank=True)
+    output_file = models.CharField(max_length=100, verbose_name=_('output file name'), blank=True)
+    generator_args = models.TextField(verbose_name=_('generator arguments'), blank=True)
+    points = models.IntegerField(verbose_name=_('point value'), blank=True, null=True)
+    is_pretest = models.BooleanField(verbose_name=_('case is pretest?'))
+    output_prefix = models.IntegerField(verbose_name=_('output prefix length'), blank=True, null=True)
+    output_limit = models.IntegerField(verbose_name=_('output limit length'), blank=True, null=True)
+    checker = models.CharField(max_length=10, verbose_name=_('checker'), choices=CHECKERS, blank=True)
+    checker_args = models.TextField(verbose_name=_('checker arguments'), blank=True,
+                                    help_text=_('checker arguments as a JSON object'))
