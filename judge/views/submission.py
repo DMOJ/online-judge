76fb88826dead0aed34942b9e91a7af4b2f3e766
Tudor Brindus <tbrindus@gmail.com>
2016-12-16 19:18:07 -0500

    Copy-pasting is hard

--- judge/views/submission.py
+++ judge/views/submission.py
@@ -170,7 +170,7 @@ class SubmissionsListBase(DiggPaginatorMixin, TitleMixin, ListView):
         context['dynamic_update'] = False
         context['show_problem'] = self.show_problem
         context['completed_problem_ids'] = user_completed_ids(self.request.user.profile) if authenticated else []
-        context['authored_problem_ids'] = user_authored_ids(request.user.profile) if authenticated else []
+        context['authored_problem_ids'] = user_authored_ids(self.request.user.profile) if authenticated else []
         context['results'] = self.get_result_table()
         context['first_page_href'] = self.first_page_href or '.'
         context['my_submissions'] = self.get_my_submissions_page()
