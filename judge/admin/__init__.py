938f64b04f9bf879c6db268df9cd44fefc46b873
Quantum <quantum2048@gmail.com>
2016-12-20 18:10:11 -0500

    Split interface related model admin; #501

--- judge/admin/__init__.py
+++ judge/admin/__init__.py
@@ -1,134 +1,18 @@
 from django.contrib import admin
-from django.core.urlresolvers import reverse, reverse_lazy
-from django.db.models import TextField
-from django.forms import ModelForm
-from django.utils.html import format_html
-from django.utils.translation import ugettext_lazy as _, ugettext
-from mptt.admin import MPTTModelAdmin
-from reversion.admin import VersionAdmin
 
 from judge.admin.comments import CommentAdmin
 from judge.admin.contest import ContestTagAdmin, ContestAdmin, ContestParticipationAdmin
+from judge.admin.interface import NavigationBarAdmin, BlogPostAdmin, SolutionAdmin, LicenseAdmin
 from judge.admin.organization import OrganizationAdmin, OrganizationRequestAdmin
 from judge.admin.problem import ProblemAdmin
 from judge.admin.profile import ProfileAdmin
 from judge.admin.runtime import JudgeAdmin, LanguageAdmin
 from judge.admin.submission import SubmissionAdmin
-from judge.admin.taxon import LanguageAdmin, ProblemGroupAdmin, ProblemTypeAdmin
-from judge.dblock import LockModel
+from judge.admin.taxon import ProblemGroupAdmin, ProblemTypeAdmin
 from judge.models import Language, Profile, Problem, ProblemGroup, ProblemType, Submission, Comment, \
     MiscConfig, Judge, NavigationBar, Contest, ContestParticipation, Organization, BlogPost, \
     Solution, License, OrganizationRequest, \
     ContestTag
-from judge.widgets import MathJaxAdminPagedownWidget, \
-    HeavyPreviewAdminPageDownWidget, HeavySelect2Widget, HeavySelect2MultipleWidget
-
-# try:
-#    from suit.admin import SortableModelAdmin, SortableTabularInline
-# except ImportError:
-SortableModelAdmin = object
-SortableTabularInline = admin.TabularInline
-
-
-class NavigationBarAdmin(MPTTModelAdmin, SortableModelAdmin):
-    list_display = ('label', 'key', 'path')
-    fields = ('key', 'label', 'path', 'order', 'regex', 'parent')
-    list_editable = ()  # Bug in SortableModelAdmin: 500 without list_editable being set
-    mptt_level_indent = 20
-    sortable = 'order'
-
-    def __init__(self, *args, **kwargs):
-        super(NavigationBarAdmin, self).__init__(*args, **kwargs)
-        self.__save_model_calls = 0
-
-    def save_model(self, request, obj, form, change):
-        self.__save_model_calls += 1
-        return super(NavigationBarAdmin, self).save_model(request, obj, form, change)
-
-    def changelist_view(self, request, extra_context=None):
-        self.__save_model_calls = 0
-        with NavigationBar.objects.disable_mptt_updates():
-            result = super(NavigationBarAdmin, self).changelist_view(request, extra_context)
-        if self.__save_model_calls:
-            with LockModel(write=(NavigationBar,)):
-                NavigationBar.objects.rebuild()
-        return result
-
-
-class BlogPostForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(BlogPostForm, self).__init__(*args, **kwargs)
-        self.fields['authors'].widget.can_add_related = False
-
-    class Meta:
-        widgets = {
-            'authors': HeavySelect2MultipleWidget(data_view='profile_select2', attrs={'style': 'width: 100%'}),
-        }
-
-        if HeavyPreviewAdminPageDownWidget is not None:
-            widgets['content'] = HeavyPreviewAdminPageDownWidget(preview=reverse_lazy('blog_preview'))
-            widgets['summary'] = HeavyPreviewAdminPageDownWidget(preview=reverse_lazy('blog_preview'))
-
-
-class BlogPostAdmin(VersionAdmin):
-    fieldsets = (
-        (None, {'fields': ('title', 'slug', 'authors', 'visible', 'sticky', 'publish_on')}),
-        (_('Content'), {'fields': ('content', 'og_image',)}),
-        (_('Summary'), {'classes': ('collapse',), 'fields': ('summary',)}),
-    )
-    prepopulated_fields = {'slug': ('title',)}
-    list_display = ('id', 'title', 'visible', 'sticky', 'publish_on')
-    list_display_links = ('id', 'title')
-    ordering = ('-publish_on',)
-    form = BlogPostForm
-
-    def has_change_permission(self, request, obj=None):
-        return request.user.is_superuser or (request.user.has_perm('judge.see_hidden_post') and
-                (obj is None or obj.authors.filter(id=request.user.profile.id).exists()))
-
-
-class SolutionForm(ModelForm):
-    class Meta:
-        widgets = {
-            'problem': HeavySelect2Widget(data_view='problem_select2', attrs={'style': 'width: 250px'}),
-        }
-
-
-class SolutionAdmin(VersionAdmin):
-    fields = ('url', 'title', 'is_public', 'publish_on', 'problem', 'content')
-    list_display = ('title', 'url', 'problem_link', 'show_public')
-    search_fields = ('url', 'title')
-    form = SolutionForm
-
-    def show_public(self, obj):
-        format = '<a href="{0}" style="white-space:nowrap;">%s</a>' % ugettext('View on site')
-        return format_html(format, obj.get_absolute_url())
-
-    show_public.short_description = ''
-
-    def problem_link(self, obj):
-        if obj.problem is None:
-            return 'N/A'
-        return format_html(u'<a href="{}">{}</a>', reverse('admin:judge_problem_change', args=[obj.problem_id]),
-                           obj.problem.name)
-
-    problem_link.admin_order_field = 'problem__name'
-
-    if MathJaxAdminPagedownWidget is not None:
-        formfield_overrides = {
-            TextField: {'widget': MathJaxAdminPagedownWidget},
-        }
-
-
-class LicenseAdmin(admin.ModelAdmin):
-    fields = ('key', 'link', 'name', 'display', 'icon', 'text')
-    list_display = ('name', 'key')
-
-    if MathJaxAdminPagedownWidget is not None:
-        formfield_overrides = {
-            TextField: {'widget': MathJaxAdminPagedownWidget},
-        }
-
 
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(Comment, CommentAdmin)
---
+++ judge/admin/interface.py
@@ -0,0 +1,108 @@
+from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.forms import ModelForm
+from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _, ugettext
+from mptt.admin import MPTTModelAdmin
+from reversion.admin import VersionAdmin
+
+from judge.dblock import LockModel
+from judge.models import NavigationBar
+from judge.widgets import HeavySelect2MultipleWidget, HeavyPreviewAdminPageDownWidget, HeavySelect2Widget, \
+    MathJaxAdminPagedownWidget
+
+
+class NavigationBarAdmin(MPTTModelAdmin):
+    list_display = ('label', 'key', 'path')
+    fields = ('key', 'label', 'path', 'order', 'regex', 'parent')
+    list_editable = ()  # Bug in SortableModelAdmin: 500 without list_editable being set
+    mptt_level_indent = 20
+    sortable = 'order'
+
+    def __init__(self, *args, **kwargs):
+        super(NavigationBarAdmin, self).__init__(*args, **kwargs)
+        self.__save_model_calls = 0
+
+    def save_model(self, request, obj, form, change):
+        self.__save_model_calls += 1
+        return super(NavigationBarAdmin, self).save_model(request, obj, form, change)
+
+    def changelist_view(self, request, extra_context=None):
+        self.__save_model_calls = 0
+        with NavigationBar.objects.disable_mptt_updates():
+            result = super(NavigationBarAdmin, self).changelist_view(request, extra_context)
+        if self.__save_model_calls:
+            with LockModel(write=(NavigationBar,)):
+                NavigationBar.objects.rebuild()
+        return result
+
+
+class BlogPostForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(BlogPostForm, self).__init__(*args, **kwargs)
+        self.fields['authors'].widget.can_add_related = False
+
+    class Meta:
+        widgets = {
+            'authors': HeavySelect2MultipleWidget(data_view='profile_select2', attrs={'style': 'width: 100%'}),
+        }
+
+        if HeavyPreviewAdminPageDownWidget is not None:
+            widgets['content'] = HeavyPreviewAdminPageDownWidget(preview=reverse_lazy('blog_preview'))
+            widgets['summary'] = HeavyPreviewAdminPageDownWidget(preview=reverse_lazy('blog_preview'))
+
+
+class BlogPostAdmin(VersionAdmin):
+    fieldsets = (
+        (None, {'fields': ('title', 'slug', 'authors', 'visible', 'sticky', 'publish_on')}),
+        (_('Content'), {'fields': ('content', 'og_image',)}),
+        (_('Summary'), {'classes': ('collapse',), 'fields': ('summary',)}),
+    )
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('id', 'title', 'visible', 'sticky', 'publish_on')
+    list_display_links = ('id', 'title')
+    ordering = ('-publish_on',)
+    form = BlogPostForm
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser or (request.user.has_perm('judge.see_hidden_post') and
+               (obj is None or obj.authors.filter(id=request.user.profile.id).exists()))
+
+
+class SolutionForm(ModelForm):
+    class Meta:
+        widgets = {'problem': HeavySelect2Widget(data_view='problem_select2', attrs={'style': 'width: 250px'})}
+        if MathJaxAdminPagedownWidget is not None:
+            widgets['content'] = MathJaxAdminPagedownWidget
+
+
+class SolutionAdmin(VersionAdmin):
+    fields = ('url', 'title', 'is_public', 'publish_on', 'problem', 'content')
+    list_display = ('title', 'url', 'problem_link', 'show_public')
+    search_fields = ('url', 'title')
+    form = SolutionForm
+
+    def show_public(self, obj):
+        format = '<a href="{0}" style="white-space:nowrap;">%s</a>' % ugettext('View on site')
+        return format_html(format, obj.get_absolute_url())
+    show_public.short_description = ''
+
+    def problem_link(self, obj):
+        if obj.problem is None:
+            return 'N/A'
+        return format_html(u'<a href="{}">{}</a>', reverse('admin:judge_problem_change', args=[obj.problem_id]),
+                           obj.problem.name)
+    problem_link.short_description = ''
+    problem_link.admin_order_field = 'problem__name'
+
+
+class LicenseForm(ModelForm):
+    class Meta:
+        if MathJaxAdminPagedownWidget is not None:
+            widgets = {'text': MathJaxAdminPagedownWidget}
+
+
+class LicenseAdmin(admin.ModelAdmin):
+    fields = ('key', 'link', 'name', 'display', 'icon', 'text')
+    list_display = ('name', 'key')
+    form = LicenseForm
