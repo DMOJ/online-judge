245dc6e429ad9a1caf7f74b87bbb1c7a21d2c04d
Radu Pogonariu <WallE256@users.noreply.github.com>
2016-12-23 20:40:34 +0200

    A new migration appeared on the radar

--- judge/migrations/0055_add_performance_points.py
+++ judge/migrations/0055_add_performance_points.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('judge', '0053_opengraph_problems'),
+        ('judge', '0054_tickets'),
     ]
 
     operations = [
