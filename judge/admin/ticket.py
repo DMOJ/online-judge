598e72ed12ede9032d7cd20c2c3f01a8caa86dba
Quantum <quantum2048@gmail.com>
2016-12-23 00:17:27 -0500

    Is it because we are using non-admin widgets? #327

--- judge/admin/ticket.py
+++ judge/admin/ticket.py
@@ -3,7 +3,7 @@ from django.contrib.admin.options import InlineModelAdmin
 from django.forms import ModelForm
 
 from judge.models import TicketMessage
-from judge.widgets import HeavySelect2Widget, HeavySelect2MultipleWidget, PagedownWidget
+from judge.widgets import HeavySelect2Widget, HeavySelect2MultipleWidget, AdminPagedownWidget
 
 
 class TicketMessageForm(ModelForm):
@@ -11,8 +11,8 @@ class TicketMessageForm(ModelForm):
         widgets = {
             'user': HeavySelect2Widget(data_view='profile_select2', attrs={'style': 'width: 100%'}),
         }
-        if PagedownWidget is not None:
-            widgets['body'] = PagedownWidget()
+        if AdminPagedownWidget is not None:
+            widgets['body'] = AdminPagedownWidget()
 
 
 class TicketMessageInline(InlineModelAdmin):
