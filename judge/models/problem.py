4c36eb60b640dbf8ea51926ee171722e5d56e905
BinAccel <BinAccel@users.noreply.github.com>
2017-02-01 15:15:12 -0500

    Look nicer

--- judge/models/problem.py
+++ judge/models/problem.py
@@ -98,8 +98,8 @@ class Problem(models.Model):
     description = models.TextField(verbose_name=_('problem body'))
     authors = models.ManyToManyField(Profile, verbose_name=_('creators'), blank=True, related_name='authored_problems')
     curators = models.ManyToManyField(Profile, verbose_name=_('curators'), blank=True, related_name='curated_problems',
-                                     help_text=(
-                                         "These users will be able to edit a problem, but not be publicly shown as an author."))
+                                     help_text=("These users will be able to edit a problem,"
+                                                 + " but not be publicly shown as an author."))
     testers = models.ManyToManyField(Profile, verbose_name=_('testers'), blank=True, related_name='tested_problems',
                                      help_text=_(
                                          "These users will be able to view a private problem, but not edit it."))
