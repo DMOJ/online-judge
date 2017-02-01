a67a56359161fd3ebebec797c1954b46cd8bed11
BinAccel <BinAccel@users.noreply.github.com>
2017-02-01 16:35:41 -0500

    Quantum is a big hater of spaces.

--- judge/views/submission.py
+++ judge/views/submission.py
@@ -37,16 +37,12 @@ class SubmissionDetailBase(LoginRequiredMixin, TitleMixin, SubmissionMixin, Deta
     def get_object(self, queryset=None):
         submission = super(SubmissionDetailBase, self).get_object(queryset)
         profile = self.request.user.profile
-
         if self.request.user.has_perm('judge.view_all_submission'):
             return submission
-
         if submission.user_id == profile.id:
             return submission
-
         if submission.problem.is_editor(profile):
             return submission
-
         if submission.problem.is_public or submission.problem.testers.filter(id=user.profile.id).exists():
             if Submission.objects.filter(user_id=profile.id, result='AC', problem__code=submission.problem.code,
                                          points=F('problem__points')).exists():
