cd68c3960a7c1a7be67090bfd9c91a08a8dba824
BinAccel <rogerjfu@hotmail.com>
2017-02-13 20:14:10 -0500

    Life is cancerous

--- judge/views/user.py
+++ judge/views/user.py
@@ -219,7 +219,7 @@ class UserList(LoadSelect2Mixin, QueryStringSortMixin, DiggPaginatorMixin, Title
     default_sort = '-points'
 
     def get_queryset(self):
-        return (Profile.objects.exclude('is_unlisted').order_by(self.order, 'id').select_related('user')
+        return (Profile.objects.filter(is_unlisted=False).order_by(self.order, 'id').select_related('user')
                 .only('display_rank', 'user__username', 'name', 'points', 'rating', 'performance_points'))
 
     def get_context_data(self, **kwargs):
