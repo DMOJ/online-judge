8b2c1967fcebf3a4476bbf7c6c11b1e4ec6819b4
Tudor <xyene@dmoj.ca>
2017-02-15 12:47:09 -0500

    Actually return in default case; #536

--- judge/models/submission.py
+++ judge/models/submission.py
@@ -84,6 +84,7 @@ class Submission(models.Model):
             if case_points == case_total:
                 return 'AC'
             return '_AC'
+        return self.result
 
     @property
     def result_class(self):
