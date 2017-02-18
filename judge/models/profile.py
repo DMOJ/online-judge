6a5617a5d09590e4fdf3f288d82c4bb89482d87f
Quantum <quantum2048@gmail.com>
2017-02-18 13:58:03 -0500

    That space...

--- judge/models/profile.py
+++ judge/models/profile.py
@@ -107,7 +107,7 @@ class Profile(models.Model):
                 .annotate(max_points=Max('submission__points')).order_by('-max_points')
                 .values_list('max_points', flat=True).filter(max_points__gt=0))
         extradata = Problem.objects.filter(submission__user=self, submission__result='AC').values('id').distinct().count()
-        bonus_function =  getattr(settings, 'PP_BONUS_FUNCTION', lambda n: 300 * (1 - 0.997 ** n))
+        bonus_function = getattr(settings, 'PP_BONUS_FUNCTION', lambda n: 300 * (1 - 0.997 ** n))
         points = sum(data)
         problems = len(data)
         entries = min(len(data), len(table))
