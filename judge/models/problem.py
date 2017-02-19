40999df7eb2b06855e9435cc8717230b52e60ec1
Tudor <xyene@dmoj.ca>
2017-02-19 00:28:56 -0500

    PyCharm's error highlighting... #327

--- judge/models/problem.py
+++ judge/models/problem.py
@@ -141,7 +141,7 @@ class Problem(models.Model):
         return self.allowed_languages.values_list('common_name', flat=True).distinct().order_by('common_name')
 
     def is_editor(self, profile):
-        return (self.authors.filter(id=profile.id) | self.curators.filter(id=profile.id)).exists():
+        return (self.authors.filter(id=profile.id) | self.curators.filter(id=profile.id)).exists()
 
     def is_editable_by(self, user):
         if user.is_superuser:
