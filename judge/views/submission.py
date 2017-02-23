5b276e9194d787d76d582b66fbf3bf121fe9eba6
Radu Pogonariu <WallE256@users.noreply.github.com>
2017-02-24 01:29:00 +0200

    Contest problem ranking pages should not link to problem (#552)
    * First attempt at #379
    
    * Some fixes
    
    * You gotta know when something is just for me #379

--- judge/views/submission.py
+++ judge/views/submission.py
@@ -366,6 +366,9 @@ class ForceContestMixin(object):
             if self.contest.start_time is not None and self.contest.start_time > timezone.now():
                 raise Http404()
 
+    def get_problem_number(self, problem):
+        return self.contest.select_related('problem').get(problem=problem).order
+
     def get(self, request, *args, **kwargs):
         if 'contest' not in kwargs:
             raise ImproperlyConfigured(_('Must pass a contest'))
@@ -375,7 +378,9 @@ class ForceContestMixin(object):
 
 class UserContestSubmissions(ForceContestMixin, UserProblemSubmissions):
     def get_title(self):
-        return "%s's submissions for %s in %s" % (self.username, self.problem_name, self.contest.name)
+        if self.problem.is_accessible_by(self.request.user):
+            return "%s's submissions for %s in %s" % (self.username, self.problem_name, self.contest.name)
+        return "%s's submissions for problem %s in %s" % (self.username, self.get_problem_number(self.problem), self.contest.name)
 
     def access_check(self, request):
         super(UserContestSubmissions, self).access_check(request)
@@ -383,8 +388,14 @@ class UserContestSubmissions(ForceContestMixin, UserProblemSubmissions):
             raise Http404()
 
     def get_content_title(self):
+        if self.problem.is_accessible_by(self.request.user):
+            return format_html(_(u'<a href="{1}">{0}</a>\'s submissions for '
+                                u'<a href="{3}">{2}</a> in <a href="{5}">{4}</a>'),
+                                self.username, reverse('user_page', args=[self.username]),
+                                self.problem_name, reverse('problem_detail', args=[self.problem.code]),
+                                self.contest.name, reverse('contest_view', args=[self.contest.key]))
         return format_html(_(u'<a href="{1}">{0}</a>\'s submissions for '
-                             u'<a href="{3}">{2}</a> in <a href="{5}">{4}</a>'),
+                             u'problem {2} in <a href="{4}">{3}</a>'),
                            self.username, reverse('user_page', args=[self.username]),
-                           self.problem_name, reverse('problem_detail', args=[self.problem.code]),
+                           self.get_problem_number(self.problem),
                            self.contest.name, reverse('contest_view', args=[self.contest.key]))
