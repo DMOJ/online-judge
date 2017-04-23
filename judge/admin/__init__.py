820cd01d6b075661a4871306d3b14301aeb27136
Quantum <quantum2048@gmail.com>
2017-04-23 19:01:22 -0400

    Incomplete removal; #542

--- judge/admin/__init__.py
+++ judge/admin/__init__.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from judge.admin.comments import CommentAdmin
 from judge.admin.contest import ContestTagAdmin, ContestAdmin, ContestParticipationAdmin
-from judge.admin.interface import NavigationBarAdmin, BlogPostAdmin, SolutionAdmin, LicenseAdmin
+from judge.admin.interface import NavigationBarAdmin, BlogPostAdmin, LicenseAdmin
 from judge.admin.organization import OrganizationAdmin, OrganizationRequestAdmin
 from judge.admin.problem import ProblemAdmin
 from judge.admin.profile import ProfileAdmin
@@ -12,7 +12,7 @@ from judge.admin.taxon import ProblemGroupAdmin, ProblemTypeAdmin
 from judge.admin.ticket import TicketAdmin
 from judge.models import Language, Profile, Problem, ProblemGroup, ProblemType, Submission, Comment, \
     MiscConfig, Judge, NavigationBar, Contest, ContestParticipation, Organization, BlogPost, \
-    Solution, License, OrganizationRequest, ContestTag, Ticket
+    License, OrganizationRequest, ContestTag, Ticket
 
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(Comment, CommentAdmin)
@@ -29,7 +29,6 @@ admin.site.register(ContestTag, ContestTagAdmin)
 admin.site.register(ContestParticipation, ContestParticipationAdmin)
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(BlogPost, BlogPostAdmin)
-admin.site.register(Solution, SolutionAdmin)
 admin.site.register(License, LicenseAdmin)
 admin.site.register(OrganizationRequest, OrganizationRequestAdmin)
 admin.site.register(Ticket, TicketAdmin)
