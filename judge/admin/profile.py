3ba4b83e61e30e26f6593fa3bc766a4e9aadd20b
Quantum <quantum2048@gmail.com>
2017-04-23 20:48:56 -0400

    Do not guess ace mode names; #360

--- judge/admin/profile.py
+++ judge/admin/profile.py
@@ -91,5 +91,5 @@ class ProfileAdmin(VersionAdmin):
 
     def get_form(self, request, obj=None, **kwargs):
         form = super(ProfileAdmin, self).get_form(request, obj, **kwargs)
-        form.base_fields['user_script'].widget = AceWidget('js', request.user.profile.ace_theme)
+        form.base_fields['user_script'].widget = AceWidget('javascript', request.user.profile.ace_theme)
         return form
