19d062f4a63b05a3fa73ad2656ff3365a28fd21a
Quantum <quantum2048@gmail.com>
2017-07-07 22:04:25 -0400

    More preparation of checkboxes widget for #588

--- judge/widgets/checkbox.py
+++ judge/widgets/checkbox.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.core.exceptions import FieldError
-from django.template import Context
 from django.template.loader import get_template
 from django.utils.safestring import mark_safe
 
@@ -16,12 +15,13 @@ class CheckboxSelectMultipleWithSelectAll(forms.CheckboxSelectMultiple):
         select_all_name = name + '_all'
         original = super(CheckboxSelectMultipleWithSelectAll, self).render(name, value, attrs, original)
         template = get_template('widgets/select_all.jade')
-        context = Context({'original_widget': original,
-                           'select_all_id': select_all_id,
-                           'select_all_name': select_all_name,
-                           'all_selected': all(choice[0] in value for choice in self.choices),
-                           'empty': not self.choices})
-        return mark_safe(template.render(context))
+        return mark_safe(template.render({
+            'original_widget': original,
+            'select_all_id': select_all_id,
+            'select_all_name': select_all_name,
+            'all_selected': all(choice[0] in value for choice in self.choices),
+            'empty': not self.choices,
+        }))
 
     def value_from_datadict(self, *args, **kwargs):
         original = super(CheckboxSelectMultipleWithSelectAll, self).value_from_datadict(*args, **kwargs)
