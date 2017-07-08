ac5d6b0af35f54b1865edac3e2108bc6bb84a414
Quantum <quantum2048@gmail.com>
2017-07-07 22:02:55 -0400

    Prepare checkboxes widget for #588

--- judge/widgets/checkbox.py
+++ judge/widgets/checkbox.py
@@ -8,22 +8,19 @@ from django.utils.safestring import mark_safe
 class CheckboxSelectMultipleWithSelectAll(forms.CheckboxSelectMultiple):
     _all_selected = False
 
-    def render(self, *args, **kwargs):
-        empty = False
-        if not self.choices:
-            empty = True
-        has_id = kwargs and ('attrs' in kwargs) and ('id' in kwargs['attrs'])
-        if not has_id:
+    def render(self, name, value, attrs=None, original=None):
+        if 'id' not in attrs:
             raise FieldError('id required')
-        select_all_id = kwargs['attrs']['id'] + '_all'
-        select_all_name = args[0] + '_all'
-        renderer = super(CheckboxSelectMultipleWithSelectAll, self).get_renderer(*args, **kwargs)
+
+        select_all_id = attrs['id'] + '_all'
+        select_all_name = name + '_all'
+        original = super(CheckboxSelectMultipleWithSelectAll, self).render(name, value, attrs, original)
         template = get_template('widgets/select_all.jade')
-        context = Context({'original_widget': renderer.render(),
+        context = Context({'original_widget': original,
                            'select_all_id': select_all_id,
                            'select_all_name': select_all_name,
-                           'all_selected': all(choice[0] in renderer.value for choice in renderer.choices),
-                           'empty': empty})
+                           'all_selected': all(choice[0] in value for choice in self.choices),
+                           'empty': not self.choices})
         return mark_safe(template.render(context))
 
     def value_from_datadict(self, *args, **kwargs):
