cc0fb1b3c0172a687a6eb4f18862b9e974580e6a
Quantum <quantum2048@gmail.com>
2017-09-10 14:45:05 -0400

    Add gevent support.
    Use `wsgi_async` as WSGI module and add `gevent = <greenlet count>` in uwsgi.ini

--- dmoj/wsgi_async.py
+++ dmoj/wsgi_async.py
@@ -1,11 +1,11 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
 
-try:
-    import MySQLdb
-except ImportError:
-    import pymysql
-    pymysql.install_as_MySQLdb()
+import pymysql
+pymysql.install_as_MySQLdb()
+
+import gevent.monkey
+gevent.monkey.patch_all()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
