a4dd31cee238ee67b6cd960a2bd4cbe7e8d33000
Quantum <quantum2048@gmail.com>
2017-09-30 18:12:15 -0400

    Make CheckboxSelectMultipleWithSelectAll work on 1.11; #588

--- judge/widgets/checkbox.py
+++ judge/widgets/checkbox.py
@@ -5,29 +5,18 @@ from django.utils.safestring import mark_safe
 
 
 class CheckboxSelectMultipleWithSelectAll(forms.CheckboxSelectMultiple):
-    _all_selected = False
-
-    def render(self, name, value, attrs=None, original=None):
+    def render(self, name, value, attrs=None, renderer=None):
         if 'id' not in attrs:
             raise FieldError('id required')
 
         select_all_id = attrs['id'] + '_all'
         select_all_name = name + '_all'
-        original = super(CheckboxSelectMultipleWithSelectAll, self).render(name, value, attrs, original)
+        original = super(CheckboxSelectMultipleWithSelectAll, self).render(name, value, attrs, renderer)
         template = get_template('widgets/select_all.jade')
         return mark_safe(template.render({
             'original_widget': original,
             'select_all_id': select_all_id,
             'select_all_name': select_all_name,
-            'all_selected': all(choice[0] in value for choice in self.choices),
+            'all_selected': all(choice[0] in value for choice in self.choices) if value else False,
             'empty': not self.choices,
         }))
-
-    def value_from_datadict(self, *args, **kwargs):
-        original = super(CheckboxSelectMultipleWithSelectAll, self).value_from_datadict(*args, **kwargs)
-        select_all_name = args[2] + '_all'
-        if select_all_name in args[0]:
-            self._all_selected = True
-        else:
-            self._all_selected = False
-        return original
