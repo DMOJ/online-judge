ad00d6d8da6109a6de0656990017fae8971f1a99
Quantum <quantum2048@gmail.com>
2017-10-07 20:24:44 -0400

    Simplify evaluate tag; #619

--- judge/templatetags/evaluate.py
+++ judge/templatetags/evaluate.py
@@ -3,26 +3,10 @@ from django import template
 register = template.Library()
 
 
-@register.tag(name='evaluate')
-def do_evaluate(parser, token):
-    """
-    tag usage {% evaluate object.textfield %}
-    """
+@register.simple_tag(name='evaluate', takes_context=True)
+def evaluate_tag(context, template_code):
     try:
-        tag_name, variable = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError('%r tag requires a single argument' % token.contents.split()[0])
-    return EvaluateNode(variable)
-
-
-class EvaluateNode(template.Node):
-    def __init__(self, variable):
-        self.variable = template.Variable(variable)
-
-    def render(self, context):
-        try:
-            content = self.variable.resolve(context)
-            t = template.Template(content)
-            return t.render(context)
-        except template.VariableDoesNotExist, template.TemplateSyntaxError:
-            return 'Error rendering', self.variable
+        t = template.Template(template_code)
+        return t.render(context)
+    except (template.VariableDoesNotExist, template.TemplateSyntaxError):
+        return 'Error rendering: %r' % template_code
