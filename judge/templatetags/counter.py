097008eab8446cd453c78d1e828181790bec3058
Quantum <quantum2048@gmail.com>
2017-10-07 21:03:40 -0400

    Simplify get_counter template tag; #619

--- judge/templatetags/counter.py
+++ judge/templatetags/counter.py
@@ -5,19 +5,6 @@ from django import template
 register = template.Library()
 
 
-class GetCounterNode(template.Node):
-    def __init__(self, var_name, start=1):
-        self.var_name = var_name
-        self.start = start
-
-    def render(self, context):
-        context[self.var_name] = count(self.start).next
-        return ''
-
-
-@register.tag
-def get_counter(parser, token):
-    try:
-        return GetCounterNode(*token.contents.split()[1:])
-    except ValueError:
-        raise template.TemplateSyntaxError('%r tag requires arguments' % token.contents.split()[0])
+@register.simple_tag
+def get_counter(start=1):
+    return count(start).next
