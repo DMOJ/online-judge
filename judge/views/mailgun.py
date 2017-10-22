4e06d4ef94ce4f605946783efd796761b2b181a5
Quantum <quantum2048@gmail.com>
2017-10-22 19:52:21 -0400

    The problem with middlewares is that you don't know if they are loaded.

--- judge/views/mailgun.py
+++ judge/views/mailgun.py
@@ -5,6 +5,7 @@ from email.utils import parseaddr
 
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
@@ -49,7 +50,7 @@ class MailgunActivationView(View):
 
             key = registration.activation_key
             if key in params.get('body-plain', '') or key in params.get('body-html', ''):
-                if RegistrationProfile.objects.activate_user(key, request.site):
+                if RegistrationProfile.objects.activate_user(key, get_current_site(request)):
                     logger.info('Activated sender: %s: %s', sender, params.get('from'))
                     return HttpResponse('Activated', status=200)
                 logger.info('Failed to activate sender: %s: %s', sender, params.get('from'))
