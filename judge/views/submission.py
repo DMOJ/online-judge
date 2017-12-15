a066569d4993a7010891b39e9048f99e8fc3b060
Quantum <quantum2048@gmail.com>
2017-12-14 23:43:50 -0500

    We obviously have a problem id, why are we using the code? #641

--- judge/views/submission.py
+++ judge/views/submission.py
@@ -304,7 +304,7 @@ class ProblemSubmissionsBase(SubmissionsListBase):
     def get_queryset(self):
         if self.in_contest and not self.contest.contest_problems.filter(problem_id=self.problem.id).exists():
             raise Http404()
-        return super(ProblemSubmissionsBase, self)._get_queryset().filter(problem__code=self.problem.code)
+        return super(ProblemSubmissionsBase, self)._get_queryset().filter(problem_id=self.problem.id)
 
     def get_title(self):
         return _('All submissions for %s') % self.problem_name
