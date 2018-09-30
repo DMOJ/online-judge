d74b501de90d8b795c042e24f25a8086007d277c
Radu Pogonariu <radu.pogonariu@gmail.com>
2018-10-01 00:46:14 +0300

    Implement camo filter; #718

--- judge/jinja2/__init__.py
+++ judge/jinja2/__init__.py
@@ -8,8 +8,8 @@ from statici18n.templatetags.statici18n import inlinei18n
 
 from judge.highlight_code import highlight_code
 from judge.user_translations import ugettext
-from . import (datetime, filesize, gravatar, language, markdown, rating, reference, render, social, spaceless,
-               submission, timedelta)
+from . import (camo, datetime, filesize, gravatar, language, markdown, rating, reference, render, social,
+               spaceless, submission, timedelta)
 from . import registry
 
 registry.function('str', unicode)
