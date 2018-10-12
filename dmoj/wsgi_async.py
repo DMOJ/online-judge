84ae49310fc160156e71825022414b83557c876c
Quantum <quantum2048@gmail.com>
2018-10-12 18:23:39 -0400

    Automatically monkey patch MySQLdb when using wsgi_async.py

--- dmoj/wsgi_async.py
+++ dmoj/wsgi_async.py
@@ -1,11 +1,24 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
 
-import pymysql
-pymysql.install_as_MySQLdb()
-
 import gevent.monkey
 gevent.monkey.patch_all()
+
+try:
+    import MySQLdb
+except ImportError:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+else:
+    from functools import wraps, partial
+    import gevent.hub
+
+    def gevent_waiter(fd, hub=gevent.hub.get_hub()):
+        hub.wait(hub.loop.io(fd, 1))
+
+    MySQLdb.connect = MySQLdb.Connection = MySQLdb.Connect = wraps(MySQLdb.connect)(
+        partial(MySQLdb.connect, waiter=gevent_waiter)
+    )
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
