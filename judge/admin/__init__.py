44b358b7e76defb25d30a01e1cf4655ecfb1a0db
Radu Pogonariu <radu.pogonariu@gmail.com>
2018-10-13 12:14:21 +0300

    Add admin controls for comment locks; #555

--- judge/admin/__init__.py
+++ judge/admin/__init__.py
@@ -10,25 +10,26 @@ from judge.admin.runtime import JudgeAdmin, LanguageAdmin
 from judge.admin.submission import SubmissionAdmin
 from judge.admin.taxon import ProblemGroupAdmin, ProblemTypeAdmin
 from judge.admin.ticket import TicketAdmin
-from judge.models import Language, Profile, Problem, ProblemGroup, ProblemType, Submission, Comment, \
-    MiscConfig, Judge, NavigationBar, Contest, ContestParticipation, Organization, BlogPost, \
-    License, OrganizationRequest, ContestTag, Ticket
+from judge.models import BlogPost, Comment, CommentLock, Contest, ContestParticipation, \
+    ContestTag, Judge, Language, License, MiscConfig, NavigationBar, Organization, \
+    OrganizationRequest, Problem, ProblemGroup, ProblemType, Profile, Submission, Ticket
 
-admin.site.register(Language, LanguageAdmin)
+admin.site.register(BlogPost, BlogPostAdmin)
 admin.site.register(Comment, CommentAdmin)
-admin.site.register(Profile, ProfileAdmin)
+admin.site.register(CommentLock)
+admin.site.register(Contest, ContestAdmin)
+admin.site.register(ContestParticipation, ContestParticipationAdmin)
+admin.site.register(ContestTag, ContestTagAdmin)
+admin.site.register(Judge, JudgeAdmin)
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(License, LicenseAdmin)
+admin.site.register(MiscConfig)
+admin.site.register(NavigationBar, NavigationBarAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(OrganizationRequest, OrganizationRequestAdmin)
 admin.site.register(Problem, ProblemAdmin)
 admin.site.register(ProblemGroup, ProblemGroupAdmin)
 admin.site.register(ProblemType, ProblemTypeAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Submission, SubmissionAdmin)
-admin.site.register(MiscConfig)
-admin.site.register(NavigationBar, NavigationBarAdmin)
-admin.site.register(Judge, JudgeAdmin)
-admin.site.register(Contest, ContestAdmin)
-admin.site.register(ContestTag, ContestTagAdmin)
-admin.site.register(ContestParticipation, ContestParticipationAdmin)
-admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(BlogPost, BlogPostAdmin)
-admin.site.register(License, LicenseAdmin)
-admin.site.register(OrganizationRequest, OrganizationRequestAdmin)
 admin.site.register(Ticket, TicketAdmin)
