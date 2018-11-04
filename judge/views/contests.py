34cb17934032e97c98e2e804ae06785be1be4f3f
Quantum <quantum2048@gmail.com>
2018-11-03 20:27:19 -0400

    Do not run a query for every badge on contest list

--- judge/views/contests.py
+++ judge/views/contests.py
@@ -72,7 +72,7 @@ class ContestList(TitleMixin, ContestListMixin, ListView):
 
     def get_queryset(self):
         return super(ContestList, self).get_queryset() \
-            .order_by('-start_time', 'key').prefetch_related('tags')
+            .order_by('-start_time', 'key').prefetch_related('tags', 'organizations')
 
     def get_context_data(self, **kwargs):
         context = super(ContestList, self).get_context_data(**kwargs)
