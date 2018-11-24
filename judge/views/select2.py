6f417ba0c14518f8fe952d2d7602dea54eee614a
Roger Fu <kiritofeng@users.noreply.github.com>
2018-11-23 19:29:33 -0500

    Fix missing import of `django.http.Http404`

--- judge/views/select2.py
+++ judge/views/select2.py
@@ -1,5 +1,5 @@
 from django.db.models import Q, F
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.utils.encoding import smart_text
 from django.views.generic.list import BaseListView
