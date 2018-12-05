28877c499ad7e93bf700a362567bb98f6d76daa2
kiritofeng <rogerjfu@hotmail.com>
2018-12-05 14:48:31 +0000

    Update organization slug URL regex; #765

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -200,7 +200,7 @@ urlpatterns = [
 
     url(r'^organizations/$', organization.OrganizationList.as_view(), name='organization_list'),
     url(r'^organization/(?P<key>\w+)(?P<rest>/.*)?$', organization.fallback),
-    url(r'^organization/(?P<pk>\d+)-(?P<slug>[\w-]+)', include([
+    url(r'^organization/(?P<pk>\d+)-(?P<slug>[\w-]*)', include([
         url(r'^$', organization.OrganizationHome.as_view(), name='organization_home'),
         url(r'^/users$', organization.OrganizationUsers.as_view(), name='organization_users'),
         url(r'^/join$', organization.JoinOrganization.as_view(), name='join_organization'),
