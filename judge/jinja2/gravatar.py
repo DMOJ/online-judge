acd6d9409d48e975735bfa4e2d5f8538b892b6f1
Quantum <quantum2048@gmail.com>
2019-03-01 02:08:23 -0500

    Handle Unicode email addresses in Gravatar
    Fixes #893

--- judge/jinja2/gravatar.py
+++ judge/jinja2/gravatar.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.http import urlencode
 
 from judge.models import Profile
+from judge.utils.unicode import utf8bytes
 from . import registry
 
 
@@ -16,7 +17,7 @@ def gravatar(email, size=80, default=None):
     elif isinstance(email, AbstractUser):
         email = email.email
 
-    gravatar_url = '//www.gravatar.com/avatar/' + hashlib.md5(email.strip().lower()).hexdigest() + '?'
+    gravatar_url = '//www.gravatar.com/avatar/' + hashlib.md5(utf8bytes(email.strip().lower())).hexdigest() + '?'
     args = {'d': 'identicon', 's': str(size)}
     if default:
         args['f'] = 'y'
