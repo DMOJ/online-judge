8db5c1f3203ccf8f0f52b54452755aed86371c63
Quantum <quantum2048@gmail.com>
2019-03-11 23:41:45 -0400

    Add contest formats to admin site

--- judge/admin/contest.py
+++ judge/admin/contest.py
@@ -107,6 +107,7 @@ class ContestAdmin(VersionAdmin):
                            'hide_problem_tags', 'hide_scoreboard', 'run_pretests_only')}),
         (_('Scheduling'), {'fields': ('start_time', 'end_time', 'time_limit')}),
         (_('Details'), {'fields': ('description', 'og_image', 'logo_override_image','tags', 'summary')}),
+        (_('Format'), {'fields': ('format_name', 'format_config')}),
         (_('Rating'), {'fields': ('is_rated', 'rate_all', 'rate_exclude')}),
         (_('Organization'), {'fields': ('is_private', 'organizations', 'access_code')}),
         (_('Justice'), {'fields': ('banned_users',)}),
