69a0b015b439ce7360ebf7b2c42ca0901dc1b085
kiritofeng <rogerjfu@hotmail.com>
2019-03-15 00:22:14 +0000

    Add ECOO Contest Format

--- judge/contest_format/__init__.py
+++ judge/contest_format/__init__.py
@@ -1,4 +1,5 @@
 from judge.contest_format.atcoder import AtCoderContestFormat
 from judge.contest_format.default import DefaultContestFormat
+from judge.contest_format.ecoo import ECOOContestFormat
 from judge.contest_format.ioi import IOIContestFormat
 from judge.contest_format.registry import formats, choices
