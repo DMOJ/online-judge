9476b6036e33afc4fe562235edfc8d159a21a1cb
kiritofeng <rogerjfu@hotmail.com>
2019-04-03 01:04:49 +0000

    The PY3 Port: Part 5 of Infinity

--- judge/views/api/api_v2.py
+++ judge/views/api/api_v2.py
@@ -1,13 +1,13 @@
 from operator import attrgetter
 
-from django.db.models import Prefetch, F, Max
-from django.http import JsonResponse, Http404
+from django.db.models import Max
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
 from dmoj import settings
-from judge.models import Contest, Problem, Profile, Submission, ContestTag, ContestParticipation
+from judge.models import Problem, Profile, Submission, ContestParticipation
 from judge.utils.ranker import ranker
-from judge.views.contests import base_contest_ranking_list, contest_ranking_list
+from judge.views.contests import contest_ranking_list
 
 
 def error(message):
@@ -72,8 +72,11 @@ def api_v2_user_info(request):
                                   .order_by('-contest__end_time')):
         contest = participation.contest
 
-        problems = list(contest.contest_problems.select_related('problem').defer('problem__description').order_by('order'))
-        rank, result = filter(lambda data: data[1].user == profile.user, ranker(contest_ranking_list(contest, problems), key=attrgetter('points', 'cumtime')))[0]
+        problems = list(contest.contest_problems.select_related('problem').defer('problem__description')
+                               .order_by('order'))
+        rank, result = list(filter(lambda data: data[1].user == profile.user,
+                                   ranker(contest_ranking_list(contest, problems),
+                                          key=attrgetter('points', 'cumtime'))))[0]
 
         contest_history.append({
             'contest': {
