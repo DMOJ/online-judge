d3158ae0213313256c71eaeadae5ac4fcafd0136
Quantum <quantum2048@gmail.com>
2019-04-27 21:34:06 -0400

    Convert celery task failures into Django logging

--- dmoj/celery.py
+++ dmoj/celery.py
@@ -1,6 +1,9 @@
+import logging
 import os
+import socket
 
 from celery import Celery
+from celery.signals import task_failure
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
@@ -10,3 +13,13 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+# Logger to enable errors be reported.
+logger = logging.getLogger('judge.celery')
+
+
+@task_failure.connect()
+def celery_failure_log(sender, task_id, exception, traceback, *args, **kwargs):
+    logger.exception('Celery Task {task_name}: {task_id} on {hostname}'.format(
+        task_name=sender.name, task_id=task_id, hostname=socket.gethostname()
+    ), exc_info=(type(exception), exception, traceback))
