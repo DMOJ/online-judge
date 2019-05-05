77c35a2651240f1a28b7b86ecec7de65437b3157
Quantum <quantum2048@gmail.com>
2019-05-04 18:26:58 -0700

    Add rejudge all submissions task

--- judge/views/problem_manage.py
+++ judge/views/problem_manage.py
@@ -2,9 +2,12 @@ from django.http import Http404
 from django.urls import reverse
 from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
-from django.views.generic import DetailView
 from django.utils.translation import gettext as _
+from django.views.generic import DetailView
+from django.views.generic.detail import BaseDetailView
 
+from judge.tasks import rejudge_problem_all
+from judge.utils.celery import redirect_to_task_status
 from judge.utils.views import TitleMixin
 from judge.views.problem import ProblemMixin
 
@@ -16,6 +19,22 @@ class ManageProblemSubmissionMixin(ProblemMixin):
         if not problem.is_subs_manageable_by(user):
             raise Http404()
         return problem
+
+
+class ManageProblemSubmissionActionMixin(ManageProblemSubmissionMixin):
+    def perform_action(self):
+        raise NotImplementedError()
+
+    def get(self, request, *args, **kwargs):
+        raise Http404()
+
+    def post(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except Http404:
+            return self.no_such_problem()
+        else:
+            return self.perform_action()
 
 
 class ManageProblemSubmissionView(TitleMixin, ManageProblemSubmissionMixin, DetailView):
@@ -33,3 +52,9 @@ class ManageProblemSubmissionView(TitleMixin, ManageProblemSubmissionMixin, Deta
         context = super().get_context_data(**kwargs)
         context['submission_count'] = self.object.submission_set.count()
         return context
+
+
+class RejudgeAllSubmissionsView(ManageProblemSubmissionActionMixin, BaseDetailView):
+    def perform_action(self):
+        status = rejudge_problem_all.delay(self.object.id)
+        return redirect_to_task_status(status, _('Rejudging all submissions for %s...') % (self.object.name,))
