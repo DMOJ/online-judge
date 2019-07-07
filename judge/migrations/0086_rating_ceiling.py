5e21f3114f585bc7e3d2b9aecd0b9465fc329128
Evan <evanzhang1028@hotmail.com>
2019-07-07 16:57:55 -0400

    Fix migration dependencies

--- judge/migrations/0086_rating_ceiling.py
+++ judge/migrations/0086_rating_ceiling.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('judge', '0085_submission_source'),
+        ('judge', '0086_external_user'),
     ]
 
     operations = [
