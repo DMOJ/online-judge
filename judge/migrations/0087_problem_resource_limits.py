52d8b19696b14bd8787c6baa294a5b27c3457522
kiritofeng <rogerjfu@hotmail.com>
2019-09-02 16:11:12 +0000

    Maintain linkedlist property of migrations

--- judge/migrations/0087_problem_resource_limits.py
+++ judge/migrations/0087_problem_resource_limits.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('judge', '0085_submission_source'),
+        ('judge', '0086_rating_ceiling'),
     ]
 
     operations = [
