aa9e9a7415f07b0138b77f52ff6a43bd7225d5e7
Evan <evanzhang1028@hotmail.com>
2019-09-22 23:23:54 -0400

    Missed one renaming is_public -> is_visible

--- judge/admin/contest.py
+++ judge/admin/contest.py
@@ -110,7 +110,7 @@ class ContestForm(ModelForm):
 class ContestAdmin(VersionAdmin):
     fieldsets = (
         (None,              {'fields': ('key', 'name', 'organizers')}),
-        (_('Settings'),     {'fields': ('is_public', 'is_virtualable', 'use_clarifications', 'hide_problem_tags',
+        (_('Settings'),     {'fields': ('is_visible', 'is_virtualable', 'use_clarifications', 'hide_problem_tags',
                                         'freeze_submissions', 'hide_scoreboard', 'run_pretests_only', 'access_code')}),
         (_('Scheduling'),   {'fields': ('start_time', 'end_time', 'time_limit')}),
         (_('Details'),      {'fields': ('description', 'og_image', 'logo_override_image', 'tags', 'summary')}),
