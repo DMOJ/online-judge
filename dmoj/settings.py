DMOJ/online-judge

commit f479d87f1626722ed231e85b1afae0b3ec7552c0

Moves constants to 'settings.py'; #993

dmoj/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import tempfile
 
 from django.utils.translation import ugettext_lazy as _
 from django_jinja.builtins import DEFAULT_EXTENSIONS
@@ -31,8 +32,89 @@ ALLOWED_HOSTS = []
 SITE_ID = 1
 SITE_NAME = 'DMOJ'
 SITE_LONG_NAME = 'DMOJ: Modern Online Judge'
+SITE_ADMIN_EMAIL = False
+
+
+# Set to 1 to use HTTPS if request was made to https://
+# Set to 2 to always use HTTPS for links
+# Set to 0 to always use HTTP for links
+DMOJ_SSL = 0
+
+# Refer to dmoj.ca/post/103-point-system-rework
+DMOJ_PP_STEP = 0.95
+DMOJ_PP_ENTRIES = 100
+DMOJ_PP_BONUS_FUNCTION = lambda n: 300 * (1 - 0.997 ** n)  # noqa: E731
+
+NODEJS = '/usr/bin/node'
+EXIFTOOL = '/usr/bin/exiftool'
+ACE_URL = '//cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/ext-split.js'
+SELECT2_JS_URL = '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js'
+DEFAULT_SELECT2_CSS = '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css'
+
+DMOJ_CAMO_URL = None
+DMOJ_CAMO_KEY = None
+DMOJ_CAMO_HTTPS = False
+DMOJ_CAMO_EXCLUDE = ()
+DMOJ_PROBLEM_DATA_ROOT = None
+DMOJ_RATING_COLORS = False
+DMOJ_EMAIL_THROTTLING = (10, 60)
+DMOJ_STATS_LANGUAGE_THRESHOLD = 10
+DMOJ_SUBMISSIONS_REJUDGE_LIMIT = 10
+DMOJ_BLOG_NEW_PROBLEM_COUNT = 7
+DMOJ_BLOG_RECENTLY_ATTEMPTED_PROBLEMS_COUNT = 7
+DMOJ_TOTP_TOLERANCE_HALF_MINUTES = 1
+DMOJ_USER_MAX_ORGANIZATION_COUNT = 3
+DMOJ_COMMENT_VOTE_HIDE_THRESHOLD = -5
+DMOJ_PDF_PROBLEM_CACHE = ''
+DMOJ_PDF_PROBLEM_TEMP_DIR = tempfile.gettempdir()
+
+MARKDOWN_STYLES = {}
+MARKDOWN_DEFAULT_STYLE = {}
+
+MATHOID_URL = False
+MATHOID_GZIP = False
+MATHOID_MML_CACHE = None
+MATHOID_CSS_CACHE = 'default'
+MATHOID_DEFAULT_TYPE = 'auto'
+MATHOID_MML_CACHE_TTL = 86400
+MATHOID_CACHE_ROOT = ''
+MATHOID_CACHE_URL = False
+
+TEXOID_GZIP = False
+TEXOID_META_CACHE = 'default'
+TEXOID_META_CACHE_TTL = 86400
+DMOJ_NEWSLETTER_ID_ON_REGISTER = None
+
+BAD_MAIL_PROVIDERS = ()
+BAD_MAIL_PROVIDER_REGEX = ()
+NOFOLLOW_EXCLUDED = set()
+
+TIMEZONE_BG = None
+TIMEZONE_MAP = None
+TIMEZONE_DETECT_BACKEND = None
+
+TERMS_OF_SERVICE_URL = None
+DEFAULT_USER_LANGUAGE = 'PY2'
+
+PHANTOMJS = ''
+PHANTOMJS_PDF_ZOOM = 0.75
+PHANDOMJS_PDF_TIMEOUT = 5.0
+PHANTOMJS_PAPER_SIZE = 'Letter'
+
+SLIMERJS = ''
+SLIMERJS_PDF_ZOOM = 0.75
+SLIMERJS_FIREFOX_PATH = ''
+SLIDERJS_PAPER_SIZE = 'Letter'
+
+PUPPETEER_MODULE = '/usr/lib/node_modules/puppeteer'
+PUPPETEER_PAPER_SIZE = 'Letter'
 
 PYGMENT_THEME = 'pygment-github.css'
+INLINE_JQUERY = True
+INLINE_FONTAWESOME = True
+JQUERY_JS = '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'
+FONTAWESOME_CSS = '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
+DMOJ_CANONICAL = ''
 
 # Application definition
 
@@ -312,6 +394,7 @@ ENABLE_FTS = False
 
 # Bridged configuration
 BRIDGED_JUDGE_ADDRESS = [('localhost', 9999)]
+BRIDGED_JUDGE_PROXIES = None
 BRIDGED_DJANGO_ADDRESS = [('localhost', 9998)]
 BRIDGED_DJANGO_CONNECT = None
 
@@ -321,6 +404,7 @@ EVENT_DAEMON_POST = 'ws://localhost:9997/'
 EVENT_DAEMON_GET = 'ws://localhost:9996/'
 EVENT_DAEMON_POLL = '/channels/'
 EVENT_DAEMON_KEY = None
+EVENT_DAEMON_AMQP_EXCHANGE = 'dmoj-events'
 EVENT_DAEMON_SUBMISSION_KEY = '6Sdmkx^%pk@GsifDfXcwX*Y7LRF%RGT8vmFpSxFBT$fwS7trc8raWfN#CSfQuKApx&$B#Gh2L7p%W!Ww'
 
 # Internationalization

django_ace/widgets.py
@@ -9,8 +9,6 @@ from django.conf import settings
 from django.forms.utils import flatatt
 from django.utils.safestring import mark_safe
 
-ACE_URL = getattr(settings, 'ACE_URL', '//cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/ext-split.js')
-
 
 class AceWidget(forms.Textarea):
     def __init__(self, mode=None, theme=None, wordwrap=False, width='100%', height='300px',
@@ -25,7 +23,7 @@ class AceWidget(forms.Textarea):
 
     @property
     def media(self):
-        js = [urljoin(ACE_URL, 'ace.js')] if self.ace_media else []
+        js = [urljoin(settings.ACE_URL, 'ace.js')] if self.ace_media else []
         js.append('django_ace/widget.js')
         css = {
             'screen': ['django_ace/widget.css'],

dmoj/throttle_mail.py
@@ -8,7 +8,7 @@ DEFAULT_THROTTLE = (10, 60)
 
 
 def new_email():
-    cache.add('error_email_throttle', 0, getattr(settings, 'DMOJ_EMAIL_THROTTLING', DEFAULT_THROTTLE)[1])
+    cache.add('error_email_throttle', 0, settings.DMOJ_EMAIL_THROTTLING[1])
     return cache.incr('error_email_throttle')
 
 
@@ -16,7 +16,7 @@ class ThrottledEmailHandler(AdminEmailHandler):
     def __init__(self, *args, **kwargs):
         super(ThrottledEmailHandler, self).__init__(*args, **kwargs)
 
-        self.throttle = getattr(settings, 'DMOJ_EMAIL_THROTTLING', DEFAULT_THROTTLE)[0]
+        self.throttle = settings.DMOJ_EMAIL_THROTTLING[0]
 
     def emit(self, record):
         try:

judge/admin/contest.py
@@ -182,7 +182,7 @@ class ContestAdmin(VersionAdmin):
 
         queryset = ContestSubmission.objects.filter(problem_id=problem_id).select_related('submission')
         if not request.user.has_perm('judge.rejudge_submission_lot') and \
-                len(queryset) > getattr(settings, 'DMOJ_SUBMISSIONS_REJUDGE_LIMIT', 10):
+                len(queryset) > settings.DMOJ_SUBMISSIONS_REJUDGE_LIMIT:
             self.message_user(request, ugettext('You do not have the permission to rejudge THAT many submissions.'),
                               level=messages.ERROR)
             return

judge/admin/submission.py
@@ -151,7 +151,7 @@ class SubmissionAdmin(admin.ModelAdmin):
             return
         queryset = queryset.order_by('id')
         if not request.user.has_perm('judge.rejudge_submission_lot') and \
-                queryset.count() > getattr(settings, 'DMOJ_SUBMISSIONS_REJUDGE_LIMIT', 10):
+                queryset.count() > settings.DMOJ_SUBMISSIONS_REJUDGE_LIMIT:
             self.message_user(request, gettext('You do not have the permission to rejudge THAT many submissions.'),
                               level=messages.ERROR)
             return

judge/comments.py
@@ -117,6 +117,6 @@ class CommentedDetailView(TemplateResponseMixin, SingleObjectMixin, View):
             context['is_new_user'] = (not self.request.user.is_staff and
                                       not profile.submission_set.filter(points=F('problem__points')).exists())
         context['comment_list'] = queryset
-        context['vote_hide_threshold'] = getattr(settings, 'DMOJ_COMMENT_VOTE_HIDE_THRESHOLD', -5)
+        context['vote_hide_threshold'] = settings.DMOJ_COMMENT_VOTE_HIDE_THRESHOLD
 
         return context

judge/event_poster.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 __all__ = ['last', 'post']
 
-if not getattr(settings, 'EVENT_DAEMON_USE', False):
+if not settings.EVENT_DAEMON_USE:
     real = False
 
     def post(channel, message):

judge/event_poster_amqp.py
@@ -12,7 +12,7 @@ __all__ = ['EventPoster', 'post', 'last']
 class EventPoster(object):
     def __init__(self):
         self._connect()
-        self._exchange = getattr(settings, 'EVENT_DAEMON_AMQP_EXCHANGE', 'dmoj-events')
+        self._exchange = settings.EVENT_DAEMON_AMQP_EXCHANGE
 
     def _connect(self):
         self._conn = pika.BlockingConnection(pika.URLParameters(settings.EVENT_DAEMON_AMQP))

commit b16e9beed4b389c8e23d83df067dca2b9898e1e5

Fix SlimerJS typo introduced in f479d87

dmoj/settings.py
@@ -104,7 +104,7 @@ PHANTOMJS_PAPER_SIZE = 'Letter'
 SLIMERJS = ''
 SLIMERJS_PDF_ZOOM = 0.75
 SLIMERJS_FIREFOX_PATH = ''
-SLIDERJS_PAPER_SIZE = 'Letter'
+SLIMERJS_PAPER_SIZE = 'Letter'
 
 PUPPETEER_MODULE = '/usr/lib/node_modules/puppeteer'
 PUPPETEER_PAPER_SIZE = 'Letter'