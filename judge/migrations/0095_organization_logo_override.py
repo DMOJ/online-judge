9e2d1a841e2554425624dea746d046bc548bd6ae
Evan <evanzhang1028@hotmail.com>
2019-10-26 11:14:24 -0400

    Rebase migration

--- judge/migrations/0095_organization_logo_override.py
+++ judge/migrations/0095_organization_logo_override.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('judge', '0093_contest_moss'),
+        ('judge', '0094_submissiontestcase_unique_together'),
     ]
 
     operations = [
