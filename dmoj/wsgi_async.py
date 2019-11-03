82c91996dd71af905f54e244741c11738d52771b
Quantum <quantum2048@gmail.com>
2019-11-02 20:03:06 -0400

    Make PyMySQL look like mysqlclient 1.3.13; #1112

--- dmoj/wsgi_async.py
+++ dmoj/wsgi_async.py
@@ -1,25 +1,12 @@
 import os
 
-import gevent.monkey
+import gevent.monkey  # noqa: I100, gevent must be imported here
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
 gevent.monkey.patch_all()
 
-try:
-    import MySQLdb  # noqa: F401, imported for side effect
-except ImportError:
-    import pymysql
-    pymysql.install_as_MySQLdb()
-else:
-    from functools import wraps, partial
-    import gevent.hub
+# noinspection PyUnresolvedReferences
+import dmoj_install_pymysql  # noqa: F401, I100, I202, imported for side effect
 
-    def gevent_waiter(fd, hub=gevent.hub.get_hub()):
-        hub.wait(hub.loop.io(fd, 1))
-
-    MySQLdb.connect = MySQLdb.Connection = MySQLdb.Connect = wraps(MySQLdb.connect)(
-        partial(MySQLdb.connect, waiter=gevent_waiter),
-    )
-
-from django.core.wsgi import get_wsgi_application  # noqa: E402, I202, django must be imported here
+from django.core.wsgi import get_wsgi_application  # noqa: E402, I100, I202, django must be imported here
 application = get_wsgi_application()
