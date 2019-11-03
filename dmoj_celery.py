08d8ec9f4653b28fbaa887aa24de020a3943fe11
Quantum <quantum2048@gmail.com>
2019-11-02 21:12:19 -0400

    Patch Django before Celery loads; #1112

--- dmoj_celery.py
+++ dmoj_celery.py
@@ -1,7 +1,15 @@
+import os
+
 try:
     import MySQLdb  # noqa: F401, imported for side effect
 except ImportError:
     import dmoj_install_pymysql  # noqa: F401, imported for side effect
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
+
+# noinspection PyUnresolvedReferences
+import django_2_2_pymysql_patch  # noqa: I100, F401, I202, imported for side effect
 
 # noinspection PyUnresolvedReferences
 from dmoj.celery import app  # noqa: F401, imported for side effect
