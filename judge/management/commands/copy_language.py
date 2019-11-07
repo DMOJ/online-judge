ddd171b555594e161fbc01b385472ea497ed562a
kiritofeng <rogerjfu@hotmail.com>
2019-11-07 05:33:12 +0000

    Copy custom runtime limits when copying languages; closes #1165

--- judge/management/commands/copy_language.py
+++ judge/management/commands/copy_language.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from judge.models import Language
+from judge.models import Language, LanguageLimit
 
 
 class Command(BaseCommand):
@@ -22,3 +22,6 @@ class Command(BaseCommand):
             raise CommandError('Invalid target language: %s' % options['target'])
 
         target.problem_set.set(source.problem_set.all())
+        LanguageLimit.objects.bulk_create(LanguageLimit(problem=ll.problem, language=target, time_limit=ll.time_limit,
+                                                        memory_limit=ll.memory_limit)
+                                          for ll in LanguageLimit.objects.filter(language=source))
