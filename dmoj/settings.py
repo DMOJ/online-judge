ffe0918fab6f913c47a2702666f7372ede43963f
Outvi V <19144373+outloudvi@users.noreply.github.com>
2020-01-09 07:44:39 +0000

    dmoj/settings.py: Bump jquery to 3.4.1

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -127,7 +127,7 @@ PUPPETEER_PAPER_SIZE = 'Letter'
 PYGMENT_THEME = 'pygment-github.css'
 INLINE_JQUERY = True
 INLINE_FONTAWESOME = True
-JQUERY_JS = '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'
+JQUERY_JS = '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'
 FONTAWESOME_CSS = '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
 DMOJ_CANONICAL = ''
 
