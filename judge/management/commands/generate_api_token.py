ee9a6d6b7ebc3f2c2884633dc7eeaf0214d5825d
TheAvidDev <AvidDev@protonmail.com>
2020-03-18 20:41:59 -0400

    Create manage.py command to generate API token

---
+++ judge/management/commands/generate_api_token.py
@@ -0,0 +1,21 @@
+import pyotp
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+from judge.models import Profile
+
+class Command(BaseCommand):
+    help = 'generates or regenerates API token for user'
+
+    def add_arguments(self, parser):
+        parser.add_argument('name', help='username')
+
+    def handle(self, *args, **options):
+        usr = User.objects.get(username=options['name'])
+
+        # Set new API token
+        profile = Profile.objects.get(user=usr)
+        profile.api_token = pyotp.random_base32(length=32).lower()
+        profile.save()
