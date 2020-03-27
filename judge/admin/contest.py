DMOJ/online-judge

commit 25213ed68a0395cbabdb4899af99e58241ecbb67

Add permission to make contest public; closes #1166

judge/admin/contest.py
@@ -124,7 +124,6 @@ class ContestAdmin(VersionAdmin):
         (_('Justice'), {'fields': ('banned_users',)}),
     )
     list_display = ('key', 'name', 'is_visible', 'is_rated', 'start_time', 'end_time', 'time_limit', 'user_count')
-    actions = ['make_visible', 'make_hidden']
     inlines = [ContestProblemInline]
     actions_on_top = True
     actions_on_bottom = True
@@ -132,6 +131,16 @@ class ContestAdmin(VersionAdmin):
     change_list_template = 'admin/judge/contest/change_list.html'
     filter_horizontal = ['rate_exclude']
     date_hierarchy = 'start_time'
+
+    def get_actions(self, request):
+        actions = super(ContestAdmin, self).get_actions(request)
+
+        if request.user.has_perm('judge.change_contest_visibility') or \
+                request.user.has_perm('judge.create_private_contest'):
+            for action in ('make_visible', 'make_hidden'):
+                actions[action] = self.get_action(action)
+
+        return actions
 
     def get_queryset(self, request):
         queryset = Contest.objects.all()
@@ -148,9 +157,21 @@ class ContestAdmin(VersionAdmin):
             readonly += ['access_code']
         if not request.user.has_perm('judge.create_private_contest'):
             readonly += ['is_private', 'private_contestants', 'is_organization_private', 'organizations']
+            if not request.user.had_perm('judge.change_contest_visibility'):
+                readonly += ['is_visible']
         if not request.user.has_perm('judge.contest_problem_label'):
             readonly += ['problem_label_script']
         return readonly
+
+    def save_model(self, request, obj, form, change):
+        if form.cleaned_data['is_visible']:
+            if not request.user.has_perm('judge.change_contest_visibility'):
+                if not form.cleaned_data['is_private'] and not form.cleaned_data['is_organization_private']:
+                    raise PermissionDenied
+                if not request.user.has_perm('judge.create_private_contest'):
+                    raise PermissionDenied
+
+        super().save_model(request, obj, form, change)
 
     def has_change_permission(self, request, obj=None):
         if not request.user.has_perm('judge.edit_own_contest'):
@@ -160,6 +181,8 @@ class ContestAdmin(VersionAdmin):
         return obj.organizers.filter(id=request.profile.id).exists()
 
     def make_visible(self, request, queryset):
+        if not request.user.has_perm('judge.change_contest_visibility'):
+            queryset = queryset.filter(Q(is_private=True) | Q(is_organization_private=True))
         count = queryset.update(is_visible=True)
         self.message_user(request, ungettext('%d contest successfully marked as visible.',
                                              '%d contests successfully marked as visible.',
@@ -167,7 +190,9 @@ class ContestAdmin(VersionAdmin):
     make_visible.short_description = _('Mark contests as visible')
 
     def make_hidden(self, request, queryset):
-        count = queryset.update(is_visible=False)
+        if not request.user.has_perm('judge.change_contest_visibility'):
+            queryset = queryset.filter(Q(is_private=True) | Q(is_organization_private=True))
+        count = queryset.update(is_visible=True)
         self.message_user(request, ungettext('%d contest successfully marked as hidden.',
                                              '%d contests successfully marked as hidden.',
                                              count) % count)

judge/models/contest.py
@@ -303,6 +303,7 @@ class Contest(models.Model):
             ('contest_rating', _('Rate contests')),
             ('contest_access_code', _('Contest access codes')),
             ('create_private_contest', _('Create private contests')),
+            ('change_contest_visibility', _('Change contest visibility')),
             ('contest_problem_label', _('Edit contest problem label script')),
         )
         verbose_name = _('contest')

judge/migrations/0100_contest_visiblity_permission.py
@@ -1,0 +1,17 @@
+# Generated by Django 2.2.6 on 2019-11-08 02:15
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('judge', '0099_contest_problem_label'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='contest',
+            options={'permissions': (('see_private_contest', 'See private contests'), ('edit_own_contest', 'Edit own contests'), ('edit_all_contest', 'Edit all contests'), ('clone_contest', 'Clone contest'), ('moss_contest', 'MOSS contest'), ('contest_rating', 'Rate contests'), ('contest_access_code', 'Contest access codes'), ('create_private_contest', 'Create private contests'), ('contest_problem_label', 'Edit contest problem label script'), ('change_contest_visibility', 'Change contest visibility')), 'verbose_name': 'contest', 'verbose_name_plural': 'contests'},
+        ),
+    ]

commit d8e8ae17a6c7b9059950249582613f932027df46

Fix `KeyError: 'is_visible'` bug introduced in 25213ed68a0395cbabdb4899af99e58241ecbb67

judge/admin/contest.py
@@ -164,12 +164,12 @@ class ContestAdmin(VersionAdmin):
         return readonly
 
     def save_model(self, request, obj, form, change):
-        if form.cleaned_data['is_visible']:
-            if not request.user.has_perm('judge.change_contest_visibility'):
-                if not form.cleaned_data['is_private'] and not form.cleaned_data['is_organization_private']:
-                    raise PermissionDenied
-                if not request.user.has_perm('judge.create_private_contest'):
-                    raise PermissionDenied
+        # `is_visible` will not appear in `cleaned_data` if user cannot edit it
+        if form.cleaned_data.get('is_visible') and not request.user.has_perm('judge.change_contest_visibility'):
+            if not form.cleaned_data['is_private'] and not form.cleaned_data['is_organization_private']:
+                raise PermissionDenied
+            if not request.user.has_perm('judge.create_private_contest'):
+                raise PermissionDenied
 
         super().save_model(request, obj, form, change)
 