6e2eafe768967f0a4c7c44c46e13fdb9599d298b
Evan <evanzhang1028@hotmail.com>
2020-03-31 19:46:22 -0400

    404 contests that the user should not have access to in the API

--- judge/views/api/api_v1.py
+++ judge/views/api/api_v1.py
@@ -32,6 +32,9 @@ def api_v1_contest_list(request):
 def api_v1_contest_detail(request, contest):
     contest = get_object_or_404(Contest, key=contest)
 
+    if not contest.is_accessible_by(request.user):
+        raise Http404()
+
     in_contest = contest.is_in_contest(request.user)
     can_see_rankings = contest.can_see_scoreboard(request.user)
     if contest.hide_scoreboard and in_contest:
