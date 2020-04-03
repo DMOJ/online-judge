c3f59289d37d72b25d26306a6f8210203fbefe2c
Evan <evanzhang1028@hotmail.com>
2020-04-02 23:25:09 -0400

    participation_number -> virtual_participation_number

--- judge/views/api/api_v2.py
+++ judge/views/api/api_v2.py
@@ -251,7 +251,7 @@ class APIContestParticipationList(APIListView):
         ('contest', 'contest__key'),
         ('user', 'user__user__username'),
         ('is_disqualified', 'is_disqualified'),
-        ('participation_number', 'virtual'),
+        ('virtual_participation_number', 'virtual'),
     )
 
     def get_unfiltered_queryset(self):
@@ -286,7 +286,7 @@ class APIContestParticipationList(APIListView):
             'score': participation.score,
             'cumulative_time': participation.cumtime,
             'is_disqualified': participation.is_disqualified,
-            'participation_number': participation.virtual,
+            'virtual_participation_number': participation.virtual,
         }
 
 
