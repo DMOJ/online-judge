1739f15584399685e4d2aa29b11711489a7fa610
Evan <evanzhang1028@hotmail.com>
2020-04-09 13:39:35 -0400

    Only `distinct()` when required for problem filtering

--- judge/views/select2.py
+++ judge/views/select2.py
@@ -55,7 +55,7 @@ class OrganizationSelect2View(Select2View):
 class ProblemSelect2View(Select2View):
     def get_queryset(self):
         return Problem.get_visible_problems(self.request.user) \
-                      .filter(Q(code__icontains=self.term) | Q(name__icontains=self.term))
+                      .filter(Q(code__icontains=self.term) | Q(name__icontains=self.term)).distinct()
 
 
 class ContestSelect2View(Select2View):
