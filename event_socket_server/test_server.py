fd162d0c0e4db96c5de2f8496c5e8afde9289963
Quantum <quantum2048@gmail.com>
2020-04-10 16:36:06 -0400

    Please flake8

--- event_socket_server/test_server.py
+++ event_socket_server/test_server.py
@@ -1,5 +1,5 @@
-from .server import Server
 from .helpers import ProxyProtocolMixin, ZlibPacketHandler
+from .server import Server
 
 
 class EchoPacketHandler(ProxyProtocolMixin, ZlibPacketHandler):
