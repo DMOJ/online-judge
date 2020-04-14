b21879921a9f9bf492631731d5622abce4e6651a
Evan <evanzhang1028@hotmail.com>
2020-04-14 14:49:08 -0400

    Expose hidden scoreboard field for contests

--- judge/views/api/api_v2.py
+++ judge/views/api/api_v2.py
@@ -213,6 +213,7 @@ class APIContestDetail(APIDetailView):
             'has_rating': contest.ratings.exists(),
             'rating_floor': contest.rating_floor,
             'rating_ceiling': contest.rating_ceiling,
+            'hidden_scoreboard': contest.hide_scoreboard,
             'is_organization_private': contest.is_organization_private,
             'organizations': list(contest.organizations.values_list('id', flat=True)),
             'is_private': contest.is_private,
