14824a01767f63a21237ddf7a46b729cee7009ce
Quantum <quantum2048@gmail.com>
2020-04-17 23:24:28 -0400

    Use faster estimate for home page submission count
    Also we were using lazy completely wrong...

--- judge/views/blog.py
+++ judge/views/blog.py
@@ -51,10 +51,10 @@ class PostList(ListView):
                 context['has_clarifications'] = clarifications.count() > 0
                 context['clarifications'] = clarifications.order_by('-date')
 
-        context['user_count'] = lazy(Profile.objects.count, int, int)
-        context['problem_count'] = lazy(Problem.get_public_problems().count, int, int)
-        context['submission_count'] = lazy(Submission.objects.count, int, int)
-        context['language_count'] = lazy(Language.objects.count, int, int)
+        context['user_count'] = Profile.objects.count
+        context['problem_count'] = Problem.get_public_problems().count
+        context['submission_count'] = lambda: Submission.objects.aggregate(max_id=Max('id'))['max_id']
+        context['language_count'] = Language.objects.count
 
         context['post_comment_counts'] = {
             int(page[2:]): count for page, count in
