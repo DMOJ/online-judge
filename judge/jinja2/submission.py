ed876eba434952862a942c71629d0c32a61bc819
Evan Zhang <evanzhang1028@hotmail.com>
2020-05-08 16:39:32 +0000

    Reorder `submission_layout` checks to be more efficient

--- judge/jinja2/submission.py
+++ judge/jinja2/submission.py
@@ -7,17 +7,14 @@ def submission_layout(submission, profile_id, user, completed_problem_ids, edita
     can_view = False
     can_edit = False
 
-    if user.has_perm('judge.view_all_submission'):
-        can_view = True
-
-    if profile_id == submission.user_id:
-        can_view = True
-
     if user.has_perm('judge.edit_all_problem') or problem_id in editable_problem_ids:
         can_view = True
         can_edit = True
-
-    if submission.problem_id in completed_problem_ids:
-        can_view |= submission.problem.is_public or submission.problem_id in tester_problem_ids
+    elif user.has_perm('judge.view_all_submission'):
+        can_view = True
+    elif profile_id == submission.user_id:
+        can_view = True
+    elif submission.problem_id in completed_problem_ids:
+        can_view = submission.problem.is_public or submission.problem_id in tester_problem_ids
 
     return can_view, can_edit
