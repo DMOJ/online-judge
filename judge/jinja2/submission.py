f116cc00aab5ebcde5db1b019cdcd150badd4ce7
Evan Zhang <evanzhang1028@hotmail.com>
2020-05-08 15:11:10 +0000

    Only show submission list admin controls if user can edit the problem

--- judge/jinja2/submission.py
+++ judge/jinja2/submission.py
@@ -5,6 +5,7 @@ from . import registry
 def submission_layout(submission, profile_id, user, completed_problem_ids, editable_problem_ids, tester_problem_ids):
     problem_id = submission.problem_id
     can_view = False
+    can_edit = False
 
     if user.has_perm('judge.view_all_submission'):
         can_view = True
@@ -12,10 +13,11 @@ def submission_layout(submission, profile_id, user, completed_problem_ids, edita
     if profile_id == submission.user_id:
         can_view = True
 
-    if problem_id in editable_problem_ids:
+    if user.has_perm('judge.edit_all_problem') or problem_id in editable_problem_ids:
         can_view = True
+        can_edit = True
 
     if submission.problem_id in completed_problem_ids:
         can_view |= submission.problem.is_public or submission.problem_id in tester_problem_ids
 
-    return can_view
+    return can_view, can_edit
