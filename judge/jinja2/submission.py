b1e9b1ecac13e0c1bbfb5493165c1e5088c07ba6
Evan <evanzhang1028@hotmail.com>
2020-05-09 11:30:07 -0400

    Fix incorrect `can_view` in `submission_layout`

--- judge/jinja2/submission.py
+++ judge/jinja2/submission.py
@@ -7,7 +7,8 @@ def submission_layout(submission, profile_id, user, completed_problem_ids, edita
     can_view = False
     can_edit = False
 
-    if user.has_perm('judge.edit_all_problem') or problem_id in editable_problem_ids:
+    if (user.has_perm('judge.edit_all_problem') and user.has_perm('judge.see_restricted_problem')) \
+            or problem_id in editable_problem_ids:
         can_view = True
         can_edit = True
     elif profile_id == submission.user_id:
