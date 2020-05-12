2818f85118a829dedd99c9d63c4cc5f961a65ff0
Guanzhong Chen <quantum2048@gmail.com>
2020-05-11 23:12:06 -0400

    Make code lint correctly with new flake8 (#1388)

--- dmoj/wsgi_async.py
+++ dmoj/wsgi_async.py
@@ -6,9 +6,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
 gevent.monkey.patch_all()
 
 # noinspection PyUnresolvedReferences
-import dmoj_install_pymysql  # noqa: F401, I100, I202, imported for side effect
+import dmoj_install_pymysql  # noqa: E402, F401, I100, I202, imported for side effect
 
 from django.core.wsgi import get_wsgi_application  # noqa: E402, I100, I202, django must be imported here
 # noinspection PyUnresolvedReferences
-import django_2_2_pymysql_patch  # noqa: I100, F401, I202, imported for side effect
+import django_2_2_pymysql_patch  # noqa: E402, I100, F401, I202, imported for side effect
 application = get_wsgi_application()
--- dmoj_bridge_async.py
+++ dmoj_bridge_async.py
@@ -6,13 +6,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
 gevent.monkey.patch_all()
 
 # noinspection PyUnresolvedReferences
-import dmoj_install_pymysql  # noqa: F401, I100, I202, imported for side effect
+import dmoj_install_pymysql  # noqa: E402, F401, I100, I202, imported for side effect
 
-import django  # noqa: F401, I100, I202, django must be imported here
+import django  # noqa: E402, F401, I100, I202, django must be imported here
 django.setup()
 
 # noinspection PyUnresolvedReferences
-import django_2_2_pymysql_patch  # noqa: I100, F401, I202, imported for side effect
+import django_2_2_pymysql_patch  # noqa: E402, I100, F401, I202, imported for side effect
 
 from judge.bridge.daemon import judge_daemon  # noqa: E402, I100, I202, django code must be imported here
 
