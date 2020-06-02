227b8b37f4ed4265a117ec4b198c4703862b45fb
TheAvidDev <AvidDev@protonmail.com>
2020-06-02 02:00:36 -0400

    Clean up camo tests

--- judge/utils/tests/test_camo.py
+++ judge/utils/tests/test_camo.py
@@ -11,28 +11,32 @@ from judge.utils.unicode import utf8bytes
 #       settings just don't get set so client is always None.
 class CamoClientTestCase(SimpleTestCase):
     def setUp(self):
-        self.http_camo_camo_blank_key = CamoClient('http://example.com/', '')
-        self.http_camo = CamoClient('http://example.com/', 'key', excluded=('https://github.com/',))
-        self.https_camo = CamoClient('https://example.com', 'key', https=True)
-
+        self.example_url = '//example.com'
+        self.example_url_http = 'http:%s' % self.example_url
+        self.example_url_https = 'https:%s' % self.example_url
         self.test_url = '//test.test/img.png/'
         self.test_url_http = 'http:%s' % self.test_url
         self.test_url_https = 'https:%s' % self.test_url
+
+        self.http_camo = CamoClient(self.example_url_http, 'key', excluded=('https://github.com/',))
+        self.https_camo = CamoClient(self.example_url_https, 'key', https=True)
+
+        # These urls are generated the same way camo does
         self.camo_blank_url = '%s/%s/' % (
             self.http_camo.server,
-            hmac.new(utf8bytes(''), utf8bytes(''), sha1).hexdigest()
+            hmac.new(utf8bytes(''), utf8bytes(''), sha1).hexdigest(),
         )
         self.camo_image_url = '%s/%s/%s' % (
             self.http_camo.server,
             hmac.new(utf8bytes('key'), utf8bytes(self.test_url_http), sha1).hexdigest(),
-            utf8bytes(self.test_url_http).hex()
+            utf8bytes(self.test_url_http).hex(),
         )
 
     def test_removing_trailing_slash(self):
-        self.assertEqual(self.http_camo.server, 'http://example.com')
+        self.assertEqual(self.http_camo.server, self.example_url_http)
 
     def test_image_url(self):
-        self.assertEqual(self.http_camo_camo_blank_key.image_url(''), self.camo_blank_url)
+        self.assertEqual(CamoClient(self.example_url_http, '').image_url(''), self.camo_blank_url)
         self.assertEqual(self.http_camo.image_url(self.test_url_http), self.camo_image_url)
 
     def test_rewrite(self):
@@ -40,8 +44,8 @@ class CamoClientTestCase(SimpleTestCase):
         self.assertEqual(self.http_camo.rewrite_url(self.http_camo.excluded[0]), self.http_camo.excluded[0])
         self.assertEqual(self.http_camo.rewrite_url(self.test_url_http), self.camo_image_url)
         self.assertEqual(self.http_camo.rewrite_url(self.test_url_https), self.http_camo.image_url(self.test_url_https))
-        self.assertEqual(self.http_camo.rewrite_url('//example.com'), self.http_camo.server)
-        self.assertEqual(self.https_camo.rewrite_url('//example.com'), self.https_camo.server)
+        self.assertEqual(self.http_camo.rewrite_url(self.example_url), self.example_url_http)
+        self.assertEqual(self.https_camo.rewrite_url(self.example_url), self.example_url_https)
         self.assertEqual(self.http_camo.rewrite_url(self.test_url), self.camo_image_url)
         self.assertEqual(self.https_camo.rewrite_url(self.test_url), self.https_camo.image_url(self.test_url_https))
         self.assertEqual(self.http_camo.rewrite_url('wss://other.test'), 'wss://other.test')
