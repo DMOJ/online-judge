aa282672712b0154767b340b4edcae4c6be9bf8d
TheAvidDev <AvidDev@protonmail.com>
2020-06-01 23:42:09 -0400

    Test proper url encoding in two_factor

--- judge/utils/tests/test_two_factor.py
+++ judge/utils/tests/test_two_factor.py
@@ -8,7 +8,7 @@ class TwoFactorTestCase(SimpleTestCase):
         self.assertEqual(webauthn_decode(''), b'')
         self.assertEqual(webauthn_decode('AA'), bytes(range(1)))
         self.assertEqual(webauthn_decode('AAE'), bytes(range(2)))
-        self.assertEqual(webauthn_decode('AAEC'), bytes(range(3)))
+        self.assertEqual(webauthn_decode('f39_'), b'\x7f\x7f\x7f')
         self.assertEqual(webauthn_decode(
             'AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0-Pw'), bytes(range(64)))
 
@@ -16,6 +16,6 @@ class TwoFactorTestCase(SimpleTestCase):
         self.assertEqual(webauthn_encode(b''), '')
         self.assertEqual(webauthn_encode(bytes(range(1))), 'AA')
         self.assertEqual(webauthn_encode(bytes(range(2))), 'AAE')
-        self.assertEqual(webauthn_encode(bytes(range(3))), 'AAEC')
+        self.assertEqual(webauthn_encode(b'\x7f\x7f\x7f'), 'f39_')
         self.assertEqual(webauthn_encode(bytes(range(64))),
                          'AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0-Pw')
