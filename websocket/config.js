a8fea331afc198e36f4b0a9decb30dc6b1dd65cd
TheAvidDev <AvidDev@protonmail.com>
2020-07-29 12:32:00 -0400

    Add config.js websocket to repo

---
+++ websocket/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    get_host: 'websocket',
+    get_port: 15100,
+    post_host: 'websocket',
+    post_port: 15101,
+    http_host: 'websocket',
+    http_port: 15102,
+    long_poll_timeout: 29000,
+};
