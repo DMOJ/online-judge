afc4f95a58da21ab9475121f6f9c27caa746dace
munhyunsu <munhyunsu@gmail.com>
2021-01-11 23:15:11 +0900

    Added option DMOJ_PRIVATE_SUBMISSION to hide other people\'s source code even for users who solved the problem

--- judge/jinja2/submission.py
+++ judge/jinja2/submission.py
@@ -2,7 +2,7 @@ from . import registry
 
 
 @registry.function
-def submission_layout(submission, profile_id, user, completed_problem_ids, editable_problem_ids, tester_problem_ids):
+def submission_layout(submission, profile_id, user, completed_problem_ids, editable_problem_ids, tester_problem_ids, private_submission):
     problem_id = submission.problem_id
     can_view = False
     can_edit = False
@@ -15,6 +15,8 @@ def submission_layout(submission, profile_id, user, completed_problem_ids, edita
     elif profile_id == submission.user_id:
         can_view = True
     elif submission.problem_id in completed_problem_ids:
-        can_view = submission.problem.is_public or submission.problem_id in tester_problem_ids
+        can_view = submission.problem_id in tester_problem_ids
+        if not private_submission:
+            can_view = can_view or submission.problem.is_public
 
     return can_view, can_edit
