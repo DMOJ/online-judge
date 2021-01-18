bb13f4f83d45249c40d57ef4ff7b3cced0a0a2b8
kevinjycui <kevincui1230@gmail.com>
2021-01-18 15:45:07 -0500

    Remove share to Google Plus

--- judge/jinja2/social.py
+++ judge/jinja2/social.py
@@ -1,13 +1,14 @@
 from django.template.loader import get_template
 from django.utils.safestring import mark_safe
-from django_social_share.templatetags.social_share import post_to_facebook_url, post_to_gplus_url, post_to_twitter_url
+from django_social_share.templatetags.social_share import post_to_facebook_url, post_to_twitter_url
 
 from . import registry
 
 SHARES = [
     ('post_to_twitter', 'django_social_share/templatetags/post_to_twitter.html', post_to_twitter_url),
     ('post_to_facebook', 'django_social_share/templatetags/post_to_facebook.html', post_to_facebook_url),
-    ('post_to_gplus', 'django_social_share/templatetags/post_to_gplus.html', post_to_gplus_url),
+    # Deprecated:
+    # ('post_to_gplus', 'django_social_share/templatetags/post_to_gplus.html', post_to_gplus_url),
     # For future versions:
     # ('post_to_linkedin', 'django_social_share/templatetags/post_to_linkedin.html', post_to_linkedin_url),
     # ('post_to_reddit', 'django_social_share/templatetags/post_to_reddit.html', post_to_reddit_url),
