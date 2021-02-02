fed289da5f4a061f3a9db55bcbb3a1f134b4be7e
munhyunsu <munhyunsu@gmail.com>
2021-02-02 13:40:34 +0900

    Remove unnecessary code

--- judge/jinja2/submission.py
+++ judge/jinja2/submission.py
@@ -16,11 +16,11 @@ def submission_layout(submission, profile_id, user, completed_problem_ids, edita
         can_view = True
     elif profile_id == submission.user_id:
         can_view = True
+    elif settings.DMOJ_SUBMISSION_SOURCE_VISIBILITY == 'all':
+        can_view = True
     elif submission.problem_id in completed_problem_ids:
         can_view = submission.problem_id in tester_problem_ids
         if settings.DMOJ_SUBMISSION_SOURCE_VISIBILITY == 'all-solved':
             can_view = can_view or submission.problem.is_public
-        elif settings.DMOJ_SUBMISSION_SOURCE_VISIBILITY == 'only-own':
-            can_view = can_view
 
     return can_view, can_edit
