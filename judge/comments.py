31d43f60fd84f81c0794de7f2128d10cfdbdb7af
Quantum <quantum2048@gmail.com>
2021-04-08 19:51:46 -0400

    Require a solved problem to update profile about
    This prevents spam bots from posting sketchy links.

--- judge/comments.py
+++ judge/comments.py
@@ -45,8 +45,7 @@ class CommentForm(ModelForm):
             profile = self.request.profile
             if profile.mute:
                 raise ValidationError(_('Your part is silent, little toad.'))
-            elif (not self.request.user.is_staff and
-                  not profile.submission_set.filter(points=F('problem__points')).exists()):
+            elif not self.request.user.is_staff and not profile.has_any_solves:
                 raise ValidationError(_('You need to have solved at least one problem '
                                         'before your voice can be heard.'))
         return super(CommentForm, self).clean()
