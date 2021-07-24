4a2fefa7dc52a54068827414c6619f3cb78e5529
Quantum <quantum2048@gmail.com>
2021-07-24 02:19:39 -0400

    Add rate limit to password reset view

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -56,7 +56,7 @@ register_patterns = [
     url(r'^password/change/done/$', auth_views.PasswordChangeDoneView.as_view(
         template_name='registration/password_change_done.html',
     ), name='password_change_done'),
-    url(r'^password/reset/$', auth_views.PasswordResetView.as_view(
+    url(r'^password/reset/$', user.CustomPasswordResetView.as_view(
         template_name='registration/password_reset.html',
         html_email_template_name='registration/password_reset_email.html',
         email_template_name='registration/password_reset_email.txt',
