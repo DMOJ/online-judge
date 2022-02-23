DMOJ/online-judge

commit f95a9fed6601854d4f7bfe9f907ef542d1eedfba

Infer private booleans; fixes #1829

judge/admin/contest.py
@@ -122,8 +122,8 @@ class ContestAdmin(NoBatchDeleteMixin, VersionAdmin):
         (_('Details'), {'fields': ('description', 'og_image', 'logo_override_image', 'tags', 'summary')}),
         (_('Format'), {'fields': ('format_name', 'format_config', 'problem_label_script')}),
         (_('Rating'), {'fields': ('is_rated', 'rate_all', 'rating_floor', 'rating_ceiling', 'rate_exclude')}),
-        (_('Access'), {'fields': ('access_code', 'is_private', 'private_contestants', 'is_organization_private',
-                                  'organizations', 'classes', 'view_contest_scoreboard')}),
+        (_('Access'), {'fields': ('access_code', 'private_contestants', 'organizations', 'classes',
+                                  'view_contest_scoreboard')}),
         (_('Justice'), {'fields': ('banned_users',)}),
     )
     list_display = ('key', 'name', 'is_visible', 'is_rated', 'locked_after', 'start_time', 'end_time', 'time_limit',
@@ -167,7 +167,7 @@ class ContestAdmin(NoBatchDeleteMixin, VersionAdmin):
         if not request.user.has_perm('judge.contest_access_code'):
             readonly += ['access_code']
         if not request.user.has_perm('judge.create_private_contest'):
-            readonly += ['is_private', 'private_contestants', 'is_organization_private', 'organizations']
+            readonly += ['private_contestants', 'organizations']
             if not request.user.has_perm('judge.change_contest_visibility'):
                 readonly += ['is_visible']
         if not request.user.has_perm('judge.contest_problem_label'):
@@ -182,6 +182,8 @@ class ContestAdmin(NoBatchDeleteMixin, VersionAdmin):
             if not request.user.has_perm('judge.create_private_contest'):
                 raise PermissionDenied
 
+        obj.is_private = bool(form.cleaned_data['private_contestants'])
+        obj.is_organization_private = bool(form.cleaned_data['organizations'])
         super().save_model(request, obj, form, change)
         # We need this flag because `save_related` deals with the inlines, but does not know if we have already rescored
         self._rescored = False

judge/admin/problem.py
@@ -123,7 +123,7 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
         (None, {
             'fields': (
                 'code', 'name', 'is_public', 'is_manually_managed', 'date', 'authors', 'curators', 'testers',
-                'is_organization_private', 'organizations', 'submission_source_visibility_mode', 'is_full_markup',
+                'organizations', 'submission_source_visibility_mode', 'is_full_markup',
                 'description', 'license',
             ),
         }),
@@ -234,10 +234,11 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
         return form
 
     def save_model(self, request, obj, form, change):
+        obj.is_organization_private = bool(form.cleaned_data['organizations'])
         super(ProblemAdmin, self).save_model(request, obj, form, change)
         if (
             form.changed_data and
-            any(f in form.changed_data for f in ('is_public', 'is_organization_private', 'points', 'partial'))
+            any(f in form.changed_data for f in ('is_public', 'organizations', 'points', 'partial'))
         ):
             self._rescore(request, obj.id)
 

judge/migrations/0126_infer_private_bools.py
@@ -1,0 +1,39 @@
+# Generated by hand on 2021-12-15
+
+from django.db import migrations
+
+
+def sync_private_booleans(apps, schema_editor):
+    Problem = apps.get_model('judge', 'Problem')
+    Contest = apps.get_model('judge', 'Contest')
+    db_alias = schema_editor.connection.alias
+
+    for problem in Problem.objects.using(db_alias).all():
+        if not problem.is_organization_private:
+            problem.organizations.clear()
+        elif not problem.organizations.exists():
+            problem.is_organization_private = False
+            problem.save()
+
+    for contest in Contest.objects.using(db_alias).all():
+        if not contest.is_private:
+            contest.private_contestants.clear()
+        elif not contest.private_contestants.exists():
+            contest.is_private = False
+            contest.save()
+        if not contest.is_organization_private:
+            contest.organizations.clear()
+        elif not contest.organizations.exists():
+            contest.is_organization_private = False
+            contest.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('judge', '0125_organization_classes'),
+    ]
+
+    operations = [
+        migrations.RunPython(sync_private_booleans, migrations.RunPython.noop),
+    ]

commit 326809fe31c2205b0f06ba19ce7e604b8dbca3eb

Fix errors while inferring private booleans; #1829
Fixes errors introduced in f95a9fed6601854d4f7bfe9f907ef542d1eedfba.

judge/admin/contest.py
@@ -182,8 +182,12 @@ class ContestAdmin(NoBatchDeleteMixin, VersionAdmin):
             if not request.user.has_perm('judge.create_private_contest'):
                 raise PermissionDenied
 
-        obj.is_private = bool(form.cleaned_data['private_contestants'])
-        obj.is_organization_private = bool(form.cleaned_data['organizations'])
+        # `private_contestants` and `organizations` will not appear in `cleaned_data` if user cannot edit it
+        if form.changed_data:
+            if 'private_contestants' in form.changed_data:
+                obj.is_private = bool(form.cleaned_data['private_contestants'])
+            if 'organizations' in form.changed_data:
+                obj.is_organization_private = bool(form.cleaned_data['organizations'])
         super().save_model(request, obj, form, change)
         # We need this flag because `save_related` deals with the inlines, but does not know if we have already rescored
         self._rescored = False

judge/admin/problem.py
@@ -234,7 +234,9 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
         return form
 
     def save_model(self, request, obj, form, change):
-        obj.is_organization_private = bool(form.cleaned_data['organizations'])
+        # `organizations` will not appear in `cleaned_data` if user cannot edit it
+        if form.changed_data and 'organizations' in form.changed_data:
+            obj.is_organization_private = bool(form.cleaned_data['organizations'])
         super(ProblemAdmin, self).save_model(request, obj, form, change)
         if (
             form.changed_data and
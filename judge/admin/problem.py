326809fe31c2205b0f06ba19ce7e604b8dbca3eb
Roger Fu <me@kirito.ca>
2022-02-22 19:04:51 -0500

    Fix errors while inferring private booleans; #1829
    Fixes errors introduced in f95a9fed6601854d4f7bfe9f907ef542d1eedfba.

--- judge/admin/problem.py
+++ judge/admin/problem.py
@@ -234,7 +234,9 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
         return form
 
     def save_model(self, request, obj, form, change):
-        obj.is_organization_private = bool(form.cleaned_data['organizations'])
+        # `organizations` will not appear in `cleaned_data` if user cannot edit it
+        if form.changed_data and 'organizations' in form.changed_data:
+            obj.is_organization_private = bool(form.cleaned_data['organizations'])
         super(ProblemAdmin, self).save_model(request, obj, form, change)
         if (
             form.changed_data and
