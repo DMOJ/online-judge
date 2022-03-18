4a2d12175793561b7d57e1b00bcdbbd2394c21c2
int-y1 <jason_yuen2007@hotmail.com>
2022-03-17 22:27:03 -0400

    Fix i18n messages for pwned passwords

--- judge/utils/pwned.py
+++ judge/utils/pwned.py
@@ -40,7 +40,7 @@ import requests
 from django.conf import settings
 from django.contrib.auth.password_validation import CommonPasswordValidator
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext as _, ngettext
+from django.utils.translation import gettext as _
 
 from judge.utils.unicode import utf8bytes
 
@@ -97,22 +97,6 @@ class PwnedPasswordsValidator(object):
     """
     Password validator which checks the Pwned Passwords database.
     """
-    DEFAULT_HELP_MESSAGE = _("Your password can't be a commonly used password.")
-    DEFAULT_PWNED_MESSAGE = _('This password is too common.')
-
-    def __init__(self, error_message=None, help_message=None):
-        self.help_message = help_message or self.DEFAULT_HELP_MESSAGE
-        error_message = error_message or self.DEFAULT_PWNED_MESSAGE
-
-        # If there is no plural, use the same message for both forms.
-        if isinstance(error_message, str):
-            singular, plural = error_message, error_message
-        else:
-            singular, plural = error_message
-        self.error_message = {
-            'singular': singular,
-            'plural': plural,
-        }
 
     def validate(self, password, user=None):
         amount = pwned_password(password)
@@ -123,14 +107,9 @@ class PwnedPasswordsValidator(object):
             CommonPasswordValidator().validate(password, user)
         elif amount:
             raise ValidationError(
-                ngettext(
-                    self.error_message['singular'],
-                    self.error_message['plural'],
-                    amount,
-                ),
-                params={'amount': amount},
+                _('This password is too common.'),
                 code='pwned_password',
             )
 
     def get_help_text(self):
-        return self.help_message
+        return _("Your password can't be a commonly used password.")
