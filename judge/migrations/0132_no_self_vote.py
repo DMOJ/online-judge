1f27c61ff6c4939e0624006462006e0104e0379f
int-y1 <jason_yuen2007@hotmail.com>
2022-04-23 02:54:00 -0400

    Add migration

---
+++ judge/migrations/0132_no_self_vote.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+from django.db.models import F, OuterRef, Subquery, Sum
+from django.db.models.functions import Coalesce
+
+
+def delete_self_votes(apps, schema_editor):
+    Comment = apps.get_model('judge', 'Comment')
+    CommentVote = apps.get_model('judge', 'CommentVote')
+
+    CommentVote.objects.filter(voter=F('comment__author')).delete()
+    Comment.objects.update(
+        score=Subquery(
+            Comment.objects.filter(
+                id=OuterRef('id')
+            ).annotate(
+                foo=Coalesce(Sum('votes__score'), 0)
+            ).values('foo')[:1]
+        )
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('judge', '0131_spectate_contests'),
+    ]
+
+    operations = [
+        migrations.RunPython(delete_self_votes, migrations.RunPython.noop),
+    ]
