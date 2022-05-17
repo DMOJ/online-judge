2cfd66c3c4f005c2a5877b742dd0287e13a30aae
Evan <me@evanzhang.ca>
2022-05-17 12:10:34 -0400

    Fix TypeError with new version of Pygments; #1935

--- judge/highlight_code.py
+++ judge/highlight_code.py
@@ -7,31 +7,21 @@ def _make_pre_code(code):
     return mark_safe('<pre>' + escape(code) + '</pre>')
 
 
-def _wrap_code(inner):
-    yield 0, '<code>'
-    for tup in inner:
-        yield tup
-    yield 0, '</code>'
-
-
 try:
     import pygments
     import pygments.lexers
-    import pygments.formatters.html
+    import pygments.formatters
     import pygments.util
 except ImportError:
     def highlight_code(code, language, cssclass=None):
         return _make_pre_code(code)
 else:
-    class HtmlCodeFormatter(pygments.formatters.HtmlFormatter):
-        def wrap(self, source, outfile):
-            return self._wrap_div(self._wrap_pre(_wrap_code(source)))
-
     def highlight_code(code, language, cssclass='codehilite'):
         try:
             lexer = pygments.lexers.get_lexer_by_name(language)
         except pygments.util.ClassNotFound:
             return _make_pre_code(code)
 
-        # return mark_safe(pygments.highlight(code, lexer, HtmlCodeFormatter(cssclass=cssclass, linenos='table')))
-        return mark_safe(pygments.highlight(code, lexer, HtmlCodeFormatter(cssclass=cssclass)))
+        return mark_safe(
+            pygments.highlight(code, lexer, pygments.formatters.HtmlFormatter(cssclass=cssclass, wrapcode=True)),
+        )
