69d34a55f56008ba0394751e311ca3fee1fc77a1
Evan <me@evanzhang.ca>
2022-08-26 22:14:35 -0400

    Fix pwned passwords throwing `ValueError`
    Relevant upstream issue: https://github.com/ubernostrum/pwned-passwords-django/issues/35

--- judge/utils/pwned.py
+++ judge/utils/pwned.py
@@ -73,7 +73,7 @@ def _get_pwned(prefix):
     results = {}
     for line in response.text.splitlines():
         line_suffix, _, times = line.partition(':')
-        results[line_suffix] = int(times)
+        results[line_suffix] = int(times.replace(',', ''))
 
     return results
 
