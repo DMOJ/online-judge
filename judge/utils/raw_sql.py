0dc7f026de3e695afbf25859ed12f3bbb3f1d5b7
Quantum <quantum2048@gmail.com>
2022-10-31 00:48:28 -0400

    Remove unique_together_left_join helper

--- judge/utils/raw_sql.py
+++ judge/utils/raw_sql.py
@@ -1,5 +1,3 @@
-from copy import copy
-
 from django.db import connections
 from django.db.models import Field
 from django.db.models.expressions import RawSQL
@@ -7,24 +5,6 @@ from django.db.models.sql.constants import INNER, LOUTER
 from django.db.models.sql.datastructures import Join
 
 from judge.utils.cachedict import CacheDict
-
-
-def unique_together_left_join(queryset, model, link_field_name, filter_field_name, filter_value, parent_model=None):
-    link_field = copy(model._meta.get_field(link_field_name).remote_field)
-    filter_field = model._meta.get_field(filter_field_name)
-
-    def restrictions(where_class, alias, related_alias):
-        cond = where_class()
-        cond.add(filter_field.get_lookup('exact')(filter_field.get_col(alias), filter_value), 'AND')
-        return cond
-
-    link_field.get_extra_restriction = restrictions
-
-    if parent_model is not None:
-        parent_alias = parent_model._meta.db_table
-    else:
-        parent_alias = queryset.query.get_initial_alias()
-    return queryset.query.join(Join(model._meta.db_table, parent_alias, None, LOUTER, link_field, True))
 
 
 class RawSQLJoin(Join):
