f2dfa60cc33983e5efd398b3bfc6401cbee2c5c7
int-y1 <jason_yuen2007@hotmail.com>
2022-12-20 20:16:53 -0500

    Fix small error in pp calculation

--- judge/models/profile.py
+++ judge/models/profile.py
@@ -235,14 +235,15 @@ class Profile(models.Model):
                            .annotate(max_points=Max('submission__points')).order_by('-max_points')
                            .values_list('max_points', flat=True).filter(max_points__gt=0)
         )
-        extradata = (
-            public_problems.filter(submission__user=self, submission__result='AC').values('id').distinct().count()
-        )
         bonus_function = settings.DMOJ_PP_BONUS_FUNCTION
         points = sum(data)
-        problems = len(data)
         entries = min(len(data), len(table))
-        pp = sum(map(mul, table[:entries], data[:entries])) + bonus_function(extradata)
+        problems = (
+            public_problems.filter(submission__user=self, submission__result='AC',
+                                   submission__case_points__gte=F('submission__case_total'))
+            .values('id').distinct().count()
+        )
+        pp = sum(map(mul, table[:entries], data[:entries])) + bonus_function(problems)
         if self.points != points or problems != self.problem_count or self.performance_points != pp:
             self.points = points
             self.problem_count = problems
