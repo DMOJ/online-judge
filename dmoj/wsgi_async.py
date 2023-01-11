b0589844b270f1240406e71989995e3ade8bf303
int-y1 <jason_yuen2007@hotmail.com>
2023-01-11 03:15:45 -0500

    Remove Django 2.2 file

--- dmoj/wsgi_async.py
+++ dmoj/wsgi_async.py
@@ -9,6 +9,4 @@ gevent.monkey.patch_all()
 import dmoj_install_pymysql  # noqa: E402, F401, I100, I202, imported for side effect
 
 from django.core.wsgi import get_wsgi_application  # noqa: E402, I100, I202, django must be imported here
-# noinspection PyUnresolvedReferences
-import django_2_2_pymysql_patch  # noqa: E402, I100, F401, I202, imported for side effect
 application = get_wsgi_application()
