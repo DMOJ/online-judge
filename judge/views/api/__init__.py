c91b725d596335628d085af88aa5ed93ee58b4f9
Quantum <quantum2048@gmail.com>
2023-01-23 02:08:04 -0500

    Delete old, deprecated API v1

--- judge/views/api/__init__.py
+++ judge/views/api/__init__.py
@@ -1,4 +1,3 @@
-from .api_v1 import *
 from .api_v2 import (
     APIContestDetail, APIContestList, APIContestParticipationList, APIOrganizationList, APIProblemDetail,
     APIProblemList, APISubmissionDetail, APISubmissionList, APIUserDetail, APIUserList,
