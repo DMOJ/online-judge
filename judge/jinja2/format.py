e6c320a4f4cf5e307ec40615772b8b003f7d1d93
int-y1 <jason_yuen2007@hotmail.com>
2024-05-05 01:15:20 -0400

    Fix i18n in js strings

--- judge/jinja2/format.py
+++ judge/jinja2/format.py
@@ -1,4 +1,6 @@
-from django.utils.html import format_html
+import html
+
+from django.utils.html import escapejs, format_html
 
 from . import registry
 
@@ -6,3 +8,8 @@ from . import registry
 @registry.function
 def bold(text):
     return format_html('<b>{0}</b>', text)
+
+
+@registry.filter
+def htmltojs(text):
+    return format_html("'{0}'", escapejs(html.unescape(text)))
