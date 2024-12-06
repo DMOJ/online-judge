7a9d95e003fe7c7df620483a303feafe6cad46d6
kiritofeng <me@kirito.ca>
2024-12-06 00:51:58 -0500

    admin: Add `create_private_problem` perm

--- judge/admin/problem.py
+++ judge/admin/problem.py
@@ -2,6 +2,7 @@ from operator import attrgetter
 
 from django import forms
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
 from django.db import transaction
 from django.forms import ModelForm
 from django.urls import reverse, reverse_lazy
@@ -150,7 +151,8 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
     def get_actions(self, request):
         actions = super(ProblemAdmin, self).get_actions(request)
 
-        if request.user.has_perm('judge.change_public_visibility'):
+        if request.user.has_perm('judge.change_public_visibility') or \
+                request.user.has_perm('judge.create_private_problem'):
             func, name, desc = self.get_action('make_public')
             actions[name] = (func, name, desc)
 
@@ -164,8 +166,10 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         fields = self.readonly_fields
-        if not request.user.has_perm('judge.change_public_visibility'):
-            fields += ('is_public',)
+        if not request.user.has_perm('judge.create_private_problem'):
+            fields += ('organizations',)
+            if not request.user.has_perm('judge.change_public_visibility'):
+                fields += ('is_public',)
         if not request.user.has_perm('judge.change_manually_managed'):
             fields += ('is_manually_managed',)
         if not request.user.has_perm('judge.problem_full_markup'):
@@ -195,6 +199,8 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
 
     @admin.display(description=_('Mark problems as public'))
     def make_public(self, request, queryset):
+        if not request.user.has_perm('judge.change_public_visibility'):
+            queryset = queryset.filter(is_organization_private=True)
         count = queryset.update(is_public=True)
         for problem_id in queryset.values_list('id', flat=True):
             self._rescore(request, problem_id)
@@ -204,6 +210,8 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
 
     @admin.display(description=_('Mark problems as private'))
     def make_private(self, request, queryset):
+        if not request.user.has_perm('judge.change_public_visibility'):
+            queryset = queryset.filter(is_organization_private=True)
         count = queryset.update(is_public=False)
         for problem_id in queryset.values_list('id', flat=True):
             self._rescore(request, problem_id)
@@ -233,6 +241,13 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
         # `organizations` will not appear in `cleaned_data` if user cannot edit it
         if form.changed_data and 'organizations' in form.changed_data:
             obj.is_organization_private = bool(form.cleaned_data['organizations'])
+
+        if form.cleaned_data.get('is_public') and not request.user.has_perm('judge.change_public_visibility'):
+            if not obj.is_organization_private:
+                raise PermissionDenied
+            if not request.user.has_perm('judge.create_private_problem'):
+                raise PermissionDenied
+
         super(ProblemAdmin, self).save_model(request, obj, form, change)
         if (
             form.changed_data and
