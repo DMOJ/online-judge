DMOJ/online-judge

commit dd8f5f06793a7f22e52414d043a6c94528c4e45c

admin: Add `create_private_problem` perm

judge/admin/problem.py
@@ -2,6 +2,7 @@ from operator import attrgetter
 
 from django import forms
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
 from django.db import transaction
 from django.forms import ModelForm
 from django.urls import reverse, reverse_lazy
@@ -150,7 +151,8 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
     def get_actions(self, request):
         actions = super(ProblemAdmin, self).get_actions(request)
 
-        if request.user.has_perm('judge.change_public_visibility'):
+        if request.user.has_perm('judge.change_public_visibility') or \
+                request.user.has_perm('judge.create_private_problem'):
             func, name, desc = self.get_action('make_public')
             actions[name] = (func, name, desc)
 
@@ -164,8 +166,10 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         fields = self.readonly_fields
-        if not request.user.has_perm('judge.change_public_visibility'):
-            fields += ('is_public',)
+        if not request.user.has_perm('judge.create_private_problem'):
+            fields += ('organizations',)
+            if not request.user.has_perm('judge.change_public_visibility'):
+                fields += ('is_public',)
         if not request.user.has_perm('judge.change_manually_managed'):
             fields += ('is_manually_managed',)
         if not request.user.has_perm('judge.problem_full_markup'):
@@ -195,6 +199,8 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
 
     @admin.display(description=_('Mark problems as public'))
     def make_public(self, request, queryset):
+        if not request.user.has_perm('judge.change_public_visibility'):
+            queryset = queryset.filter(is_organization_private=True)
         count = queryset.update(is_public=True)
         for problem_id in queryset.values_list('id', flat=True):
             self._rescore(request, problem_id)
@@ -204,6 +210,8 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
 
     @admin.display(description=_('Mark problems as private'))
     def make_private(self, request, queryset):
+        if not request.user.has_perm('judge.change_public_visibility'):
+            queryset = queryset.filter(is_organization_private=True)
         count = queryset.update(is_public=False)
         for problem_id in queryset.values_list('id', flat=True):
             self._rescore(request, problem_id)
@@ -233,6 +241,13 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
         # `organizations` will not appear in `cleaned_data` if user cannot edit it
         if form.changed_data and 'organizations' in form.changed_data:
             obj.is_organization_private = bool(form.cleaned_data['organizations'])
+
+        if form.cleaned_data.get('is_public') and not request.user.has_perm('judge.change_public_visibility'):
+            if not obj.is_organization_private:
+                raise PermissionDenied
+            if not request.user.has_perm('judge.create_private_problem'):
+                raise PermissionDenied
+
         super(ProblemAdmin, self).save_model(request, obj, form, change)
         if (
             form.changed_data and

judge/models/problem.py
@@ -501,6 +501,7 @@ class Problem(models.Model):
             ('change_public_visibility', _('Change is_public field')),
             ('change_manually_managed', _('Change is_manually_managed field')),
             ('see_organization_problem', _('See organization-private problems')),
+            ('create_private_problem', _('Create private problems')),
         )
         verbose_name = _('problem')
         verbose_name_plural = _('problems')

judge/migrations/0149_add_organization_private_problems_permission.py
@@ -1,0 +1,17 @@
+# Generated by Django 3.2.25 on 2024-12-17 03:55
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('judge', '0148_clarify_rate_all_desc'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='problem',
+            options={'permissions': (('see_private_problem', 'See hidden problems'), ('edit_own_problem', 'Edit own problems'), ('edit_all_problem', 'Edit all problems'), ('edit_public_problem', 'Edit all public problems'), ('problem_full_markup', 'Edit problems with full markup'), ('clone_problem', 'Clone problem'), ('change_public_visibility', 'Change is_public field'), ('change_manually_managed', 'Change is_manually_managed field'), ('see_organization_problem', 'See organization-private problems'), ('create_private_problem', 'Create private problems')), 'verbose_name': 'problem', 'verbose_name_plural': 'problems'},
+        ),
+    ]

commit 92bc9ff3a5cd381f2a442417edabc7d44bc05f3a

admin: fix `save_model` checks on `Problem` and `Contest`
A regression was introduced in dd8f5f06793a7f22e52414d043a6c94528c4e45c that made
it so unless you had the `judge.change_public_visiblity` perm,
you could no longer edit your own public problems.

For parity, we also allow for visible contests to be edited by those without
`judge.change_contest_visibility`, as long as they do not change the visibility
of the contest.

judge/admin/contest.py
@@ -180,21 +180,33 @@ class ContestAdmin(NoBatchDeleteMixin, SortableAdminBase, VersionAdmin):
         return readonly
 
     def save_model(self, request, obj, form, change):
-        # `private_contestants` and `organizations` will not appear in `cleaned_data` if user cannot edit it
+        # Conditions for save:
+        #   - visibility (is_public and private_contestants and organizations) is unchanged
+        #   - otherwise
+        #       - can change visibility (`judge.change_contest_visibility`)
+        #       - not is_visible contest
+        #       - otherwise
+        #           - (is_private or is_organization_private) and `judge.create_private_contest`
         if form.changed_data:
+            visibility_changed = 'is_visible' in form.changed_data
+
+            # `private_contestants`, `organizations`, etc will not appear in `cleaned_data` if user cannot edit it
             if 'private_contestants' in form.changed_data:
                 obj.is_private = bool(form.cleaned_data['private_contestants'])
+                visibility_changed = True
             if 'organizations' in form.changed_data or 'classes' in form.changed_data:
                 obj.is_organization_private = bool(form.cleaned_data['organizations'] or form.cleaned_data['classes'])
+                visibility_changed = True
             if 'join_organizations' in form.cleaned_data:
                 obj.limit_join_organizations = bool(form.cleaned_data['join_organizations'])
 
-        # `is_visible` will not appear in `cleaned_data` if user cannot edit it
-        if form.cleaned_data.get('is_visible') and not request.user.has_perm('judge.change_contest_visibility'):
-            if not obj.is_private and not obj.is_organization_private:
-                raise PermissionDenied
-            if not request.user.has_perm('judge.create_private_contest'):
-                raise PermissionDenied
+            if visibility_changed:
+                # `is_visible` will not appear in `cleaned_data` if user cannot edit it
+                if form.cleaned_data.get('is_visible') and not request.user.has_perm('judge.change_contest_visibility'):
+                    if not obj.is_private and not obj.is_organization_private:
+                        raise PermissionDenied
+                    if not request.user.has_perm('judge.create_private_contest'):
+                        raise PermissionDenied
 
         super().save_model(request, obj, form, change)
         # We need this flag because `save_related` deals with the inlines, but does not know if we have already rescored

judge/admin/problem.py
@@ -238,15 +238,24 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
         return form
 
     def save_model(self, request, obj, form, change):
-        # `organizations` will not appear in `cleaned_data` if user cannot edit it
-        if form.changed_data and 'organizations' in form.changed_data:
-            obj.is_organization_private = bool(form.cleaned_data['organizations'])
+        # Conditions for save:
+        #   - visibility (is_public and organizations) is unchanged
+        #   - otherwise
+        #       - can change visibility (`judge.change_public_visibility`)
+        #       - not is_public problem
+        #       - otherwise
+        #           - is_organization_private and `judge.create_private_problem`
+        if form.changed_data:
+            # `organizations` will not appear in `cleaned_data` if user cannot edit it
+            if 'organizations' in form.changed_data:
+                obj.is_organization_private = bool(form.cleaned_data['organizations'])
 
-        if form.cleaned_data.get('is_public') and not request.user.has_perm('judge.change_public_visibility'):
-            if not obj.is_organization_private:
-                raise PermissionDenied
-            if not request.user.has_perm('judge.create_private_problem'):
-                raise PermissionDenied
+            if 'is_public' in form.changed_data or 'organizations' in form.changed.data:
+                if form.cleaned_data.get('is_public') and not request.user.has_perm('judge.change_public_visibility'):
+                    if not obj.is_organization_private:
+                        raise PermissionDenied
+                    if not request.user.has_perm('judge.create_private_problem'):
+                        raise PermissionDenied
 
         super(ProblemAdmin, self).save_model(request, obj, form, change)
         if (